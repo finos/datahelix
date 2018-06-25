b10d1438be46542490ecf35ee5e7e995ebc6cbee
Dillon Diep <dillonkeithdiep@outlook.com>
2018-06-25 10:39:59 +0100

    Add spark wrapper component

--- src/main/scala/com/scottlogic/deg/App.scala
+++ src/main/scala/com/scottlogic/deg/App.scala
@@ -1,18 +1,11 @@
 package com.scottlogic.deg
 
-import org.apache.spark.sql.SparkSession
-import org.apache.log4j.Logger
-import org.apache.log4j.Level
-
+import com.scottlogic.deg.spark.SparkWrapper
 
 object App {
     def main(args : Array[String]) {
-        Logger.getLogger("org").setLevel(Level.ERROR)
-        Logger.getLogger("akka").setLevel(Level.ERROR)
-        val spark = SparkSession.builder
-            .appName("Data Engineering Generator")
-            .config("spark.master", "local")
-            .getOrCreate()
+        lazy val spark = new SparkWrapper()
+
         spark.stop()
     }
 }
