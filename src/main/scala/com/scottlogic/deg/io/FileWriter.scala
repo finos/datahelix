3f1ecbcb2908e572af22b7680dbdb3c5e6b324ad
Dillon Diep <dillonkeithdiep@outlook.com>
2018-06-27 11:10:05 +0100

    Add basic profile writer (json)

--- src/main/scala/com/scottlogic/deg/io/FileWriter.scala
+++ src/main/scala/com/scottlogic/deg/io/FileWriter.scala
@@ -1,10 +1,27 @@
 package com.scottlogic.deg.io
 
+import java.io
+import java.io.{BufferedWriter, File}
+
 import org.apache.spark.sql.{DataFrame, SaveMode, SparkSession}
 
 class FileWriter(spark: SparkSession) {
     def writeJson(name: String, df: DataFrame): Boolean = {
-        df.write.json("output/" + name)
+        df.write.mode(SaveMode.Overwrite).json("output/" + name)
         return true
+    }
+
+    def writeProfile(name: String, dfs: Array[(String, DataFrame)]): Unit = {
+        val columnDistributions: String = dfs.map( columnData => {
+            "{ \"columnName\": \"" + columnData._1 + "\", \"distribution\": " + columnData._2.toJSON.collect.mkString(", ") + "}"
+        }).mkString(", ")
+
+        val jsonString = "{ \"profileName\": \"testProfile\", " +
+            "\"columnDistributions\": [" + columnDistributions + "]" +
+            "}"
+        val file = new File("test-output.json")
+        val bw = new BufferedWriter(new io.FileWriter(file))
+        bw.write(jsonString)
+        bw.close()
     }
 }
--- src/main/scala/com/scottlogic/deg/profiler/Profiler.scala
+++ src/main/scala/com/scottlogic/deg/profiler/Profiler.scala
@@ -2,11 +2,15 @@ package com.scottlogic.deg.profiler
 
 import com.scottlogic.deg.io.{FileReader, FileWriter}
 import org.apache.spark.sql.DataFrame
+import org.apache.spark.sql.functions.{min, max, mean, stddev_pop, stddev_samp, variance}
 
 class Profiler(args: Array[String], fileReader: FileReader, fileWriter: FileWriter) {
     def profile() {
         val df: DataFrame = fileReader.readCSV(args(0))
-        df.show()
+        df.printSchema()
+        val dfs: Array[(String, DataFrame)] = df.columns zip
+            df.columns.map(col => df.agg(min(col), max(col), mean(col), stddev_pop(col), stddev_samp(col), variance(col)))
         fileWriter.writeJson("profile", df)
+        fileWriter.writeProfile("profileString", dfs)
     }
 }
