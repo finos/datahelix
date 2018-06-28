dd63308945be2a7a28f3172119b7c27aa1bb9c0d
Dillon Diep <dillonkeithdiep@outlook.com>
2018-06-28 15:52:19 +0100

    Refactor write profile json: split into smaller functions

--- src/main/scala/com/scottlogic/deg/io/FileWriter.scala
+++ src/main/scala/com/scottlogic/deg/io/FileWriter.scala
@@ -13,28 +13,34 @@ class FileWriter(spark: SparkSession) {
     }
 
     def writeProfile(name: String, dfs: Array[(String, DataFrame)]): Unit = {
-        val columnDistribution = dfs.foldLeft(JsArray()) {
-            (array, next: (String, DataFrame)) => {
-                val row = next._2.first()
-                val values: Map[String, JsValue] = row.getValuesMap(row.schema.fieldNames)
-                    .map(pair => (pair._1, JsString(pair._2)))
-                    .asInstanceOf[Map[String, JsValue]]
-                array.append(JsObject(
-                    Seq(next._1 -> JsObject(values))
-                ))
-            }
-        }
-
-        val json: JsValue = Json.obj(
-            "profileName" -> "testProfile",
-            "columnDistributions" -> columnDistribution
-        )
+        val json = toProfile(toColumnDistribution(dfs))
 
         val file = new File("test-output.json")
         val bw = new BufferedWriter(new io.FileWriter(file))
         bw.write(Json.stringify(json))
         bw.close()
+    }
 
+    def toColumnDistribution(dfs: Array[(String, DataFrame)]) = {
+        dfs.foldLeft(JsArray()) {
+            distributionJsCombinator
+        }
+    }
 
+    def distributionJsCombinator(array: JsArray, next: (String, DataFrame)) = {
+        val row = next._2.first()
+        val values: Map[String, JsValue] = row.getValuesMap(row.schema.fieldNames)
+            .map(pair => (pair._1, JsString(pair._2)))
+            .asInstanceOf[Map[String, JsValue]]
+        array.append(JsObject(
+            Seq(next._1 -> JsObject(values))
+        ))
+    }
+
+    def toProfile(columnDistribution: JsValue): JsValue = {
+        Json.obj(
+            "profileName" -> "testProfile",
+            "columnDistributions" -> columnDistribution
+        )
     }
 }
