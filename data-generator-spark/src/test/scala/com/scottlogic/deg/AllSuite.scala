a1c62d6d4ab90d0ccb05048eebe952ce42727dd2
Alex <scottlogic-alex@users.noreply.github.com>
2018-07-06 15:44:27 +0100

    go back to explicit class collection, to avoid accidentally running retired classes left in output folder from previous builds

--- data-generator-spark/src/test/scala/com/scottlogic/deg/AllSuite.scala
+++ data-generator-spark/src/test/scala/com/scottlogic/deg/AllSuite.scala
@@ -1,11 +1,15 @@
 package com.scottlogic.deg
 
 import org.junit.platform.runner.JUnitPlatform
-import org.junit.platform.suite.api.SelectPackages
+import org.junit.platform.suite.api.{SelectClasses, SelectPackages}
 import org.junit.runner.RunWith
 
 @RunWith(classOf[JUnitPlatform])
-@SelectPackages(Array("com.scottlogic.deg"))
+@SelectClasses(Array(
+  classOf[FileReaderInt],
+  classOf[JsonTest],
+  classOf[SemanticInferenceTest]
+))
 class AllSuite {
 
 }
