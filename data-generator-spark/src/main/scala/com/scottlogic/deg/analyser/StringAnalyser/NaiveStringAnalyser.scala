abccb7b99f0e214e61c4b045a88648c74239802f
Dillon Diep <dillonkeithdiep@outlook.com>
2018-07-06 16:28:49 +0100

    Add computing char set in naive string analyser

--- data-generator-spark/src/main/scala/com/scottlogic/deg/analyser/StringAnalyser/NaiveStringAnalyser.scala
+++ data-generator-spark/src/main/scala/com/scottlogic/deg/analyser/StringAnalyser/NaiveStringAnalyser.scala
@@ -1,20 +1,32 @@
 package com.scottlogic.deg.analyser.StringAnalyser
 
-import com.scottlogic.deg.SparkSessionBuilder
 import org.apache.spark.sql.{DataFrame, SparkSession}
 import org.apache.spark.sql.functions._
 
 class NaiveStringAnalyser(spark: SparkSession) extends StringAnalyser {
-    def analyse(df: DataFrame, columnName: String): Unit = {
+    import spark.implicits._
+    def analyse(df: DataFrame, columnName: String): (DataFrame, DataFrame) = {
         val col = df(columnName)
-        if (col != null) {
-            val dfString = df.agg(
-                min(length(col)).as("len_min"),
-                max(length(col)).as("len_max"),
-                avg(length(col)).as("len_avg"),
-                stddev(length(col)).as("len_stddev")
-            ).as(columnName)
-            dfString.show()
+
+        if (col == null) {
+            throw new NullPointerException("Column does not exist")
         }
+
+        // distribute map transformation load among workers rather collecting to driver
+        val dfCharSet = df.select(columnName)
+            .map(r => r.getString(0)).collect.toList
+            .mkString.toLowerCase
+            .toSeq.map(char => char.toString)
+            .toDF("character")
+            .distinct()
+
+        val dfLengthAnalysis = df.agg(
+            min(length(col)).as("len_min"),
+            max(length(col)).as("len_max"),
+            avg(length(col)).as("len_avg"),
+            stddev(length(col)).as("len_stddev")
+        ).as(columnName)
+
+        (dfCharSet, dfLengthAnalysis)
     }
 }
