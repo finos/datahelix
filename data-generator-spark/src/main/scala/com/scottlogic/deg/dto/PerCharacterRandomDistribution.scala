a431931f50e4e0db62ecdf61aa75509ff277931a
dillonkeithdiep <dillonkeithdiep@outlook.com>
2018-07-10 09:57:54 +0100

    Update naive string analyser to use computed distribution over previous mock data

--- data-generator-spark/src/main/scala/com/scottlogic/deg/dto/PerCharacterRandomDistribution.scala
+++ data-generator-spark/src/main/scala/com/scottlogic/deg/dto/PerCharacterRandomDistribution.scala
@@ -3,5 +3,6 @@ package com.scottlogic.deg.dto
 case class PerCharacterRandomDistribution (
   lengthMin: Int,
   lengthMax: Int,
+  lengthAvg: Double,
   alphabet: String
 ) extends AbstractDistribution
