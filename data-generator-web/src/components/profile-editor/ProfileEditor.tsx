27496a2a2ca258b3663793813aad182752087532
Mark <>
2018-07-10 14:18:54 +0100

    Web: Add text to clarify interface when no fields are defined yet

--- data-generator-web/src/components/profile-editor/ProfileEditor.tsx
+++ data-generator-web/src/components/profile-editor/ProfileEditor.tsx
@@ -14,6 +14,15 @@ const ProfileEditor = ({children}: IProps) => (
 
 		<Form fluid="true">
 			<Grid columns={3} divided="vertically">
+				{ children.length === 0 &&
+					<Grid.Row>
+						<Grid.Column width={16}>
+							<span style={{ fontSize: "larger", color: "darkgrey", fontStyle: "italic" }}>
+								No fields defined yet
+							</span>
+						</Grid.Column>
+					</Grid.Row>}
+
 				{ children }
 
 				<Grid.Row>
