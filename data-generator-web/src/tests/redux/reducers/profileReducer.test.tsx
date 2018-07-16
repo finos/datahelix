41b69498dbeabd2a9ec5fc5041b344e4e1c55f21
Alejandro Manuel Baronetti <abaronetti@scottlogic.co.uk>
2018-07-16 12:47:26 +0100

    Cleanup and first action test

--- data-generator-web/src/tests/redux/reducers/profileReducer.test.tsx
+++ data-generator-web/src/tests/redux/reducers/profileReducer.test.tsx
@@ -1,12 +1,19 @@
 import {Action} from "redux";
 import {Reducer} from 'redux-testkit';
-import {IProfileState} from "../../../redux/state/IAppState";
+import {IProfileState, IFieldState, FieldKinds} from "../../../redux/state/IAppState";
+import Actions from "../../../redux/actions";
 import profileReducer from '../../../redux/reducers/profileReducer';
 
 describe('Profile reducer', () => {
     let emptyState : IProfileState = { fields: [] };
+    let genericFieldState : IFieldState = { id: 'generic', name : '', nullPrevalence: 0, restrictions : { kind: FieldKinds.Unclassified } };
+    
     it('Should handle undefined state', () => {
         let mockAction : Action = { type : 'MOCK_ACTION' };
         Reducer(profileReducer).withState(undefined).expect(mockAction).toReturnState(emptyState);
     });
+    it('Should handle CLEAR_CURRENT_PROFILE action', () => {
+        let cleanProfile = Actions.Profiles.ClearCurrentProfile.create({});
+        Reducer(profileReducer).withState(genericFieldState).expect(cleanProfile).toReturnState(emptyState);
+    })
 });
