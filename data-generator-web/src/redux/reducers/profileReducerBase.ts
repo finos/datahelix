abadf5b5e89f2cfde81985ad3e7b3bbafe55e12d
Alejandro Manuel Baronetti <abaronetti@scottlogic.co.uk>
2018-07-17 11:33:36 +0100

    Added undefine to function signature

--- data-generator-web/src/redux/reducers/profileReducerBase.ts
+++ data-generator-web/src/redux/reducers/profileReducerBase.ts
@@ -5,7 +5,7 @@ import { IFieldState, IProfileState } from "../state/IAppState";
 export default function profileReducerBase(
 	oldState: IProfileState | undefined,
 	action: Action,
-	fallbackReducer: (previousState : IFieldState[], action : Action<any>) => any )
+	fallbackReducer: (previousState : IFieldState[] | undefined, action : Action<any>) => any )
 	: IProfileState {
 	if (!oldState)
 		return { fields: [] };
