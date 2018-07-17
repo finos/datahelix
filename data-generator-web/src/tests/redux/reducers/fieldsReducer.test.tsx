cb0d29f8723d55e811f153ce3a38a2346a3c3dca
Alejandro Manuel Baronetti <abaronetti@scottlogic.co.uk>
2018-07-17 14:30:12 +0100

    removed redux-testkit due to a dependency conflict.

--- data-generator-web/src/tests/redux/reducers/fieldsReducer.test.tsx
+++ data-generator-web/src/tests/redux/reducers/fieldsReducer.test.tsx
@@ -1,5 +1,4 @@
 import { Action } from "redux";
-import { Reducer } from 'redux-testkit';
 import Actions from "../../../redux/actions";
 import fieldsReducer from '../../../redux/reducers/fieldsReducer';
 import { FieldKinds } from "../../../redux/state/IAppState";
@@ -15,24 +14,49 @@ describe('Fields reducer', () => {
     const allFieldStates = [genericFieldState, enumFieldState, numericFiledState, stringFieldState, temporalFieldState];
 
     it('Should handle undefined state', () => {
+        // Arrange
         const mockAction: Action = { type: 'MOCK_ACTION' };
-        Reducer(fieldsReducer).withState(undefined).expect(mockAction).toReturnState([]);
+
+        // Act
+        const result = fieldsReducer(undefined, mockAction);
+
+        // Assert
+        expect(result).toEqual([]);
     });
+
     it('Should handle ADD_BLANK_FIELD action', () => {
+        // Arrange
         const addBlankFieldAction = Actions.Fields.AddBlankField.create({});
         const newBlankState: IFieldState = { id: "0", name: "", nullPrevalence: 0, restrictions: { kind: FieldKinds.Unclassified } };
         const expectedResult = [genericFieldState, newBlankState];
-        Reducer(fieldsReducer).withState([genericFieldState]).expect(addBlankFieldAction).toReturnState(expectedResult);
+
+        //Act
+        const result = fieldsReducer([genericFieldState], addBlankFieldAction);
+
+        //Assert
+        expect(result).toEqual(expectedResult);
     });
     it('Should handle DELETE_FIELD action', () => {
+        // Arrange
         const deleteFieldAction = Actions.Fields.DeleteField.create({ fieldId: genericFieldState.id });
-        Reducer(fieldsReducer).withState([genericFieldState]).expect(deleteFieldAction).toReturnState([]);
+
+        //Act
+        const result = fieldsReducer([genericFieldState], deleteFieldAction);
+
+        //Assert
+        expect(result).toEqual([]);
     });
     it('Should handle UPDATE_FIELD ation', () => {
+        // Arrange
         const modifiedStateFields = { name: 'Updated name', nullPrevalence: 0.5 };
         const updateFieldAction = Actions.Fields.UpdateField.create({ fieldId: genericFieldState.id, newValues: modifiedStateFields });
         const expectedState = [{ ...genericFieldState, name: modifiedStateFields.name, nullPrevalence: modifiedStateFields.nullPrevalence }];
-        Reducer(fieldsReducer).withState([genericFieldState]).expect(updateFieldAction).toReturnState(expectedState);
+
+        //Act
+        const result = fieldsReducer([genericFieldState], updateFieldAction);
+
+        //Assert
+        expect(result).toEqual(expectedState);
     });
 
     allFieldStates.forEach(originFieldState => {
@@ -40,8 +64,13 @@ describe('Fields reducer', () => {
         allKinds.forEach(destinationKind => {
             const destinationKindName = FieldKinds[destinationKind];
             it(`Should handle CHANGE_FIELD_KIND action from ${originalKindName} to ${destinationKindName}`, () => {
+                // Arrange
                 const changeFieldKindAction = Actions.Fields.ChangeFieldKind.create({ fieldId: genericFieldState.id, newKind: destinationKind });
+
+                //Act
                 const result = fieldsReducer([originFieldState], changeFieldKindAction);
+
+                //Assert
                 expect(result.length).toBe(1);
                 expect(result[0].restrictions.kind).toEqual(destinationKind);
             });
--- data-generator-web/src/tests/redux/reducers/profileReducer.test.tsx
+++ data-generator-web/src/tests/redux/reducers/profileReducer.test.tsx
@@ -1,5 +1,4 @@
 import { Action } from "redux";
-import { Reducer } from 'redux-testkit';
 import Actions from "../../../redux/actions";
 import profileReducer from '../../../redux/reducers/profileReducer';
 import profileReducerBase from '../../../redux/reducers/profileReducerBase';
@@ -9,25 +8,47 @@ describe('Profile reducer', () => {
     const emptyState: IProfileState = { fields: [] };
     const genericFieldState: IFieldState = { id: 'generic', name: '', nullPrevalence: 0, restrictions: { kind: FieldKinds.Unclassified } };
     const mockAction: Action = { type: 'MOCK_ACTION' };
+    const genericStateProfile: IProfileState = { fields: [genericFieldState] };
 
     it('Should handle undefined state', () => {
-        Reducer(profileReducer).withState(undefined).expect(mockAction).toReturnState(emptyState);
+        // Act
+        const result = profileReducer(undefined, mockAction);
+
+        // Assert
+        expect(result).toEqual(emptyState);
     });
     it('Should handle CLEAR_CURRENT_PROFILE action', () => {
+        // Arrange
         // We are not creating a new Action because we prefer the test to not understand anything about tha action structure, etc.
         const cleanProfile = Actions.Profiles.ClearCurrentProfile.create({});
-        Reducer(profileReducer).withState(genericFieldState).expect(cleanProfile).toReturnState(emptyState);
+
+        // Act
+        const result = profileReducer(genericStateProfile, cleanProfile);
+
+        // Assert
+        expect(result).toEqual(emptyState);
     });
     it('Should handle SET_CURRENT_PROFILE action', () => {
+        // Arrange
         const filledInFieldState: IFieldState = { id: '1', name: 'SomeField', nullPrevalence: 0.5, restrictions: { kind: FieldKinds.String, allowableCharacters: "test", minimumLength: 1, maximumLength: 10 } };
         const filledInProfileState: IProfileState = { fields: [filledInFieldState] };
         const filledInState = { newProfile: filledInProfileState };
         const setProfile = Actions.Profiles.SetCurrentProfile.create(filledInState);
-        Reducer(profileReducer).withState(filledInState).expect(setProfile).toReturnState(filledInProfileState);
+
+        // Act
+        const result = profileReducer(genericStateProfile, setProfile);
+
+        // Assert
+        expect(result).toEqual(filledInProfileState);
     });
     it('Should handle other actions by calling a fallback function', () => {
+        // Arrange
         const mockFunction = jest.fn((state: IProfileState, action: Action<any>): IProfileState => state);
+
+        // Act
         const result = profileReducerBase(emptyState, mockAction, mockFunction);
+
+        // Assert
         expect(mockFunction.mock.calls.length).toBe(1);
         expect(result).toEqual(emptyState);
     });
