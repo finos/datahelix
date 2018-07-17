74d1ef01fae616d066e5a5bf0d802b528338a8eb
Alejandro Manuel Baronetti <abaronetti@scottlogic.co.uk>
2018-07-17 13:47:36 +0100

    Added selector tests

--- data-generator-web/src/tests/redux/reducers/fieldsReducer.test.tsx
+++ data-generator-web/src/tests/redux/reducers/fieldsReducer.test.tsx
@@ -8,10 +8,10 @@ import { IFieldState } from "../../../redux/state/IAppState";
 describe('Fields reducer', () => {
     const allKinds = [FieldKinds.Enum, FieldKinds.Numeric, FieldKinds.String, FieldKinds.Temporal, FieldKinds.Unclassified];
     const genericFieldState: IFieldState = { id: 'generic', name: '', nullPrevalence: 0, restrictions: { kind: FieldKinds.Unclassified } };
-    const enumFieldState : IFieldState = { id: 'generic', name: '', nullPrevalence: 0, restrictions: { kind: FieldKinds.Enum, members : [] } };
-    const numericFiledState : IFieldState = { id: 'generic', name: '', nullPrevalence: 0, restrictions: { kind: FieldKinds.Numeric, minimumValue : null, maximumValue : null, stdDev: null, meanAvg : null } };
-    const stringFieldState : IFieldState = { id: 'generic', name: '', nullPrevalence: 0, restrictions: { kind: FieldKinds.String, minimumLength : null, maximumLength : null, allowableCharacters : null } };
-    const temporalFieldState : IFieldState = { id: 'generic', name: '', nullPrevalence: 0, restrictions: { kind: FieldKinds.Temporal, minimum : null, maximum : null } };
+    const enumFieldState: IFieldState = { id: 'generic', name: '', nullPrevalence: 0, restrictions: { kind: FieldKinds.Enum, members: [] } };
+    const numericFiledState: IFieldState = { id: 'generic', name: '', nullPrevalence: 0, restrictions: { kind: FieldKinds.Numeric, minimumValue: null, maximumValue: null, stdDev: null, meanAvg: null } };
+    const stringFieldState: IFieldState = { id: 'generic', name: '', nullPrevalence: 0, restrictions: { kind: FieldKinds.String, minimumLength: null, maximumLength: null, allowableCharacters: null } };
+    const temporalFieldState: IFieldState = { id: 'generic', name: '', nullPrevalence: 0, restrictions: { kind: FieldKinds.Temporal, minimum: null, maximum: null } };
     const allFieldStates = [genericFieldState, enumFieldState, numericFiledState, stringFieldState, temporalFieldState];
 
     it('Should handle undefined state', () => {
@@ -34,13 +34,13 @@ describe('Fields reducer', () => {
         const expectedState = [{ ...genericFieldState, name: modifiedStateFields.name, nullPrevalence: modifiedStateFields.nullPrevalence }];
         Reducer(fieldsReducer).withState([genericFieldState]).expect(updateFieldAction).toReturnState(expectedState);
     });
-    
+
     allFieldStates.forEach(originFieldState => {
         const originalKindName = FieldKinds[originFieldState.restrictions.kind];
         allKinds.forEach(destinationKind => {
             const destinationKindName = FieldKinds[destinationKind];
             it(`Should handle CHANGE_FIELD_KIND action from ${originalKindName} to ${destinationKindName}`, () => {
-                const changeFieldKindAction = Actions.Fields.ChangeFieldKind.create({fieldId: genericFieldState.id, newKind: destinationKind});
+                const changeFieldKindAction = Actions.Fields.ChangeFieldKind.create({ fieldId: genericFieldState.id, newKind: destinationKind });
                 const result = fieldsReducer([originFieldState], changeFieldKindAction);
                 expect(result.length).toBe(1);
                 expect(result[0].restrictions.kind).toEqual(destinationKind);
