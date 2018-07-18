9c46c837e808a3628d98dc0ce8233edf811309b3
Alejandro Manuel Baronetti <abaronetti@scottlogic.co.uk>
2018-07-18 09:29:10 +0100

    Use tabs

--- data-generator-web/src/tests/redux/reducers/appReducer.test.tsx
+++ data-generator-web/src/tests/redux/reducers/appReducer.test.tsx
@@ -2,16 +2,16 @@ import {Action} from "redux";
 import appReducer from "../../../redux/reducers/appReducer";
 
 it('Should return state', () => {
-    // Arrange
-    const mockAction: Action = { type: 'MOCK_ACTION' };
-    const emptyAppState = {
-        currentProfile: { fields : [] },
+	// Arrange
+	const mockAction: Action = { type: 'MOCK_ACTION' };
+	const emptyAppState = {
+		currentProfile: { fields : [] },
 		currentModal: undefined
-    };
+	};
 
-    // Act
-    const result = appReducer(undefined, mockAction);
+	// Act
+	const result = appReducer(undefined, mockAction);
 
-    // Assert
-    expect(result).toEqual(emptyAppState);
+	// Assert
+	expect(result).toEqual(emptyAppState);
 });
--- data-generator-web/src/tests/redux/reducers/modalReducer.test.tsx
+++ data-generator-web/src/tests/redux/reducers/modalReducer.test.tsx
@@ -3,28 +3,28 @@ import modalReducer from "../../../redux/reducers/modalReducer";
 import {ModalId} from "../../../redux/state/IAppState";
 
 describe('Modal reducer', () => {
-    it('Should handle OPEN_MODAL by returning the modal id', () => {
-        // Arrange
-        const oldState : ModalId = ModalId.StartProfilingFromFile;
-        const openModalAction = Actions.Modals.OpenModal.create({modalId : oldState});
-        const expectedResult = ModalId.StartProfilingFromFile;
+	it('Should handle OPEN_MODAL by returning the modal id', () => {
+		// Arrange
+		const oldState : ModalId = ModalId.StartProfilingFromFile;
+		const openModalAction = Actions.Modals.OpenModal.create({modalId : oldState});
+		const expectedResult = ModalId.StartProfilingFromFile;
 
-        // Act
-        const result = modalReducer(oldState, openModalAction);
+		// Act
+		const result = modalReducer(oldState, openModalAction);
 
-        // Assert
-        expect(result).toEqual(expectedResult);
-    });
+		// Assert
+		expect(result).toEqual(expectedResult);
+	});
 
-    it('Should handle CLOSE_MODAL by returning undefined', () => {
-       // Arrange
-       const oldState : ModalId = ModalId.StartProfilingFromFile;
-       const closeModalAction = Actions.Modals.CloseModal.create({});
+	it('Should handle CLOSE_MODAL by returning undefined', () => {
+		// Arrange
+		const oldState : ModalId = ModalId.StartProfilingFromFile;
+		const closeModalAction = Actions.Modals.CloseModal.create({});
 
-       // Act
-       const result = modalReducer(oldState, closeModalAction);
+		// Act
+		const result = modalReducer(oldState, closeModalAction);
 
-       // Assert
-       expect(result).toBe(undefined);
-    });
+		// Assert
+		expect(result).toBe(undefined);
+	});
 });
