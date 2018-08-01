12bfa4816bc5ff708b0063e682704ae7be74db4a
Alex <scottlogic-alex@users.noreply.github.com>
2018-08-01 10:30:04 +0100

    implement more field spec factory methods

--- scratch-reducer/src/main/java/com/scottlogic/deg/restriction/FieldSpecFactory.java
+++ scratch-reducer/src/main/java/com/scottlogic/deg/restriction/FieldSpecFactory.java
@@ -1,45 +1,51 @@
 package com.scottlogic.deg.restriction;
 
-import com.scottlogic.deg.generator.constraints.IConstraint;
-import com.scottlogic.deg.generator.constraints.IsInSetConstraint;
-import com.scottlogic.deg.generator.constraints.NotConstraint;
+import com.scottlogic.deg.generator.constraints.*;
+
+import java.util.Collections;
 
 public class FieldSpecFactory {
     public FieldSpec construct(String name, IConstraint constraint) {
         final FieldSpec fieldSpec = new FieldSpec(name);
-        apply(fieldSpec, constraint);
+        apply(fieldSpec, constraint, false);
         return fieldSpec;
     }
 
-    private void apply(FieldSpec fieldSpec, IConstraint constraint) {
+    private void apply(FieldSpec fieldSpec, IConstraint constraint, boolean negate) {
         if (constraint instanceof NotConstraint) {
-            applyNot(fieldSpec, ((NotConstraint) constraint).negatedConstraint);
+            apply(fieldSpec, ((NotConstraint) constraint).negatedConstraint, !negate);
         } else if (constraint instanceof IsInSetConstraint) {
-            apply(fieldSpec, (IsInSetConstraint) constraint);
+            apply(fieldSpec, (IsInSetConstraint) constraint, negate);
+        } else if (constraint instanceof IsEqualToConstantConstraint) {
+            final IsEqualToConstantConstraint castConstraint = (IsEqualToConstantConstraint) constraint;
+            apply(
+                    fieldSpec,
+                    new IsInSetConstraint(
+                            castConstraint.field,
+                            Collections.singleton(castConstraint.requiredValue)
+                    ),
+                    negate
+            );
+        } else if (constraint instanceof IsNullConstraint) {
+            apply(fieldSpec, (IsNullConstraint) constraint, negate);
         } else {
             throw new UnsupportedOperationException();
-
         }
     }
 
-    private void applyNot(FieldSpec fieldSpec, IConstraint constraint) {
-        if (constraint instanceof NotConstraint) {
-//            apply(fieldSpec, ((NotConstraint) constraint).negatedConstraint);
-            throw new IllegalStateException();
-        } else if (constraint instanceof IsInSetConstraint) {
-            applyNot(fieldSpec, (IsInSetConstraint) constraint);
+    private void apply(FieldSpec fieldSpec, IsInSetConstraint constraint, boolean negate) {
+        final SetRestrictions setRestrictions = fieldSpec.getSetRestrictions();
+        if (negate) {
+            setRestrictions.blacklist = constraint.legalValues;
         } else {
-            throw new UnsupportedOperationException();
+            setRestrictions.whitelist = constraint.legalValues;
         }
     }
 
-    private void apply(FieldSpec fieldSpec, IsInSetConstraint constraint) {
-        final SetRestrictions setRestrictions = fieldSpec.getSetRestrictions();
-        setRestrictions.whitelist = constraint.legalValues;
-    }
-
-    private void applyNot(FieldSpec fieldSpec, IsInSetConstraint constraint) {
-        final SetRestrictions setRestrictions = fieldSpec.getSetRestrictions();
-        setRestrictions.blacklist = constraint.legalValues;
+    private void apply(FieldSpec fieldSpec, IsNullConstraint constraint, boolean negate) {
+        final NullRestrictions nullRestrictions = fieldSpec.getNullRestrictions();
+        nullRestrictions.nullness = negate
+                ? NullRestrictions.Nullness.MustNotBeNull
+                : NullRestrictions.Nullness.MustBeNull;
     }
 }
