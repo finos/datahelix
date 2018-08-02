113671d48f9fe6af4a0238f96af550861df7404a
Alejandro Manuel Baronetti <abaronetti@scottlogic.co.uk>
2018-08-02 14:03:50 +0100

    Refactored profiler

--- profiler/src/main/scala/com/scottlogic/deg/profiler/Profiler.scala
+++ profiler/src/main/scala/com/scottlogic/deg/profiler/Profiler.scala
@@ -4,22 +4,29 @@ import com.scottlogic.deg.analyser.field_analyser.GenericFieldAnalyser
 import com.scottlogic.deg.analyser.field_analyser.numeric_analyser.NaiveNumericAnalyser
 import com.scottlogic.deg.analyser.field_analyser.string_analyser.NaiveStringAnalyser
 import com.scottlogic.deg.analyser.field_analyser.timestamp_analyser.NaiveTimestampAnalyser
-import com.scottlogic.deg.schemas.v3.{RuleDTO, V3ProfileDTO}
+import com.scottlogic.deg.schemas.v3.{FieldDTO, RuleDTO, V3ProfileDTO}
 import org.apache.spark.sql.DataFrame
 import org.apache.spark.sql.types._
+import collection.JavaConverters.asJavaCollectionConverter
 
 case class Location(lat: Double, lon: Double)
 
 class Profiler(df: DataFrame) {
-    def profile(): V3ProfileDTO = {
-        new V3ProfileDTO{
-            def fields = df.schema.fields.map(field => field.name).toSeq;
-            def rules = df.schema.fields.map(field => (field.dataType match {
-                case DoubleType | LongType | IntegerType => new NaiveNumericAnalyser(df, field)
-                case TimestampType => new NaiveTimestampAnalyser(df, field)
-                case StringType => new NaiveStringAnalyser(df, field)
-                case _ => new GenericFieldAnalyser(df, field)
-            }).constructDTOField()).toSeq;
-        }
+  def profile(): V3ProfileDTO = {
+    val fieldArray = df.schema.fields.map(field => new FieldDTO {
+      def name = field.name.toString()
+    });
+    val ruleArray = df.schema.fields.map(field => (field.dataType match {
+      case DoubleType | LongType | IntegerType => new NaiveNumericAnalyser(df, field)
+      case TimestampType => new NaiveTimestampAnalyser(df, field)
+      case StringType => new NaiveStringAnalyser(df, field)
+      case _ => new GenericFieldAnalyser(df, field)
+    }).constructDTOField());
+        
+    new V3ProfileDTO {
+      def fields = asJavaCollectionConverter[FieldDTO](fieldArray).asJavaCollection;
+
+      def rules = asJavaCollectionConverter[RuleDTO](ruleArray).asJavaCollection;
     }
+  }
 }
