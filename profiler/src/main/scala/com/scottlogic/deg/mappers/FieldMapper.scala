adf7fdee7c17953c4a7f35728f2b9205c991b02b
Alejandro Manuel Baronetti <abaronetti@scottlogic.co.uk>
2018-08-03 16:55:48 +0100

    Removed mappers we will probably never use

--- profiler/src/main/scala/com/scottlogic/deg/mappers/FieldMapper.scala
+++
@@ -1,8 +0,0 @@
-package com.scottlogic.deg.mappers
-
-import com.scottlogic.deg.schemas.v3.FieldDTO
-import com.scottlogic.deg.models.Field
-
-object FieldMapper extends IMapper[FieldDTO,Field] {
-  override def Map(original: FieldDTO): Field = new Field(original.name)
-}
