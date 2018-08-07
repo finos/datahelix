b2f3b88af3a66dfbd7f600cc252f4334c1e78f74
Mark <>
2018-08-07 09:31:58 +0100

    Added DTO conversion for new HasDecimalPlaces constraint. Accepted some IDE suggestions and streamlined type-checking code

--- profiler/src/main/scala/com/scottlogic/deg/mappers/ConstraintDTOMapper.scala
+++ profiler/src/main/scala/com/scottlogic/deg/mappers/ConstraintDTOMapper.scala
@@ -6,43 +6,39 @@ import com.scottlogic.deg.schemas.v3.{ConstraintDTO, ConstraintDTOBuilder}
 object ConstraintDTOMapper extends IMapper[IConstraint,ConstraintDTO] {
   override def Map(original: IConstraint): ConstraintDTO = {
     if(original == null){
-      return null;
+      throw new IllegalArgumentException("constraint argument was null")
     }
 
     val builder = ConstraintDTOBuilder.instance;
 
-    if(original.isInstanceOf[IsOfTypeConstraint]){
-      val instance = original.asInstanceOf[IsOfTypeConstraint];
-      return builder.appendField(instance.FieldName)
+    original match {
+      case instance: IsOfTypeConstraint =>
+        builder.appendField(instance.FieldName)
           .appendIs("ofType")
           .appendValue(instance.Value)
           .Build;
+      case instance: IsGreaterThanOrEqualToConstantConstraint =>
+        builder.appendField(instance.FieldName)
+          .appendIs("greaterThanOrEqual")
+          .appendValue(instance.Value)
+          .Build;
+      case instance: IsLowerThanConstraint =>
+        builder.appendField(instance.FieldName)
+          .appendIs("lowerThan")
+          .appendValue(instance.Value)
+          .Build;
+      case instance: MatchesRegexConstraint =>
+        builder.appendField(instance.FieldName)
+          .appendIs("matchesRegex")
+          .appendValue(instance.Value)
+          .Build;
+      case instance: HasDecimalPlacesConstraint =>
+        builder.appendField(instance.FieldName)
+          .appendIs("hasDecimalPlaces")
+          .appendValue(instance.NumberOfDecimalPlaces)
+          .Build;
+      case _ =>
+        throw new IllegalArgumentException("Can't convert constraint of supplied type")
     }
-
-    if(original.isInstanceOf[IsGreaterThanOrEqualToConstantConstraint]){
-      val instance = original.asInstanceOf[IsGreaterThanOrEqualToConstantConstraint];
-      return builder.appendField(instance.FieldName)
-        .appendIs("greaterThanOrEqual")
-        .appendValue(instance.Value)
-        .Build;
-    }
-
-    if(original.isInstanceOf[IsLowerThanConstraint]){
-      val instance = original.asInstanceOf[IsLowerThanConstraint];
-      return builder.appendField(instance.FieldName)
-        .appendIs("lowerThan")
-        .appendValue(instance.Value)
-        .Build;
-    }
-
-    if(original.isInstanceOf[MatchesRegexConstraint]){
-      val instance = original.asInstanceOf[MatchesRegexConstraint];
-      return builder.appendField(instance.FieldName)
-        .appendIs("matchesRegex")
-        .appendValue(instance.Value)
-        .Build;
-    }
-
-    return builder.Build;
   }
 }
