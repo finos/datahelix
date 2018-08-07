c87d33d06946af0569b9134f7c9a5f8aa32fca0f
Alex <scottlogic-alex@users.noreply.github.com>
2018-08-07 14:03:58 +0100

    first stab at tree walker

--- generator/src/main/java/com/scottlogic/deg/generator/walker/DecisionTreeWalker.java
+++ generator/src/main/java/com/scottlogic/deg/generator/walker/DecisionTreeWalker.java
@@ -1,12 +1,70 @@
 package com.scottlogic.deg.generator.walker;
 
+import com.scottlogic.deg.generator.ProfileFields;
 import com.scottlogic.deg.generator.decisiontree.IDecisionTreeProfile;
+import com.scottlogic.deg.generator.decisiontree.IRuleDecision;
+import com.scottlogic.deg.generator.decisiontree.IRuleDecisionTree;
+import com.scottlogic.deg.generator.decisiontree.IRuleOption;
+import com.scottlogic.deg.generator.reducer.ConstraintReducer;
+import com.scottlogic.deg.generator.restrictions.FieldSpecMerger;
 import com.scottlogic.deg.generator.restrictions.RowSpec;
 
-public class DecisionTreeWalker {
+import java.util.Collection;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
-    public Iterable<RowSpec> walk(IDecisionTreeProfile decisionTreeProfile) {
-        decisionTreeProfile.getDecisionTrees().iterator().next().
+public class DecisionTreeWalker {
+    private final ConstraintReducer constraintReducer;
+    private final FieldSpecMerger fieldSpecMerger;
+
+    public DecisionTreeWalker(
+            ConstraintReducer constraintReducer,
+            FieldSpecMerger fieldSpecMerger
+    ) {
+        this.constraintReducer = constraintReducer;
+        this.fieldSpecMerger = fieldSpecMerger;
     }
 
+    public Iterable<RowSpec> walk(IDecisionTreeProfile decisionTreeProfile) {
+        final DecisionTreeWalkerHelper helper = new DecisionTreeWalkerHelper(decisionTreeProfile.getFields());
+        return helper.walk(decisionTreeProfile.getDecisionTrees())
+                .collect(Collectors.toList());
+    }
+
+    private class DecisionTreeWalkerHelper {
+        private final ProfileFields profileFields;
+
+        private DecisionTreeWalkerHelper(ProfileFields profileFields) {
+            this.profileFields = profileFields;
+        }
+
+        private Stream<RowSpec> walk(Collection<IRuleDecisionTree> ruleDecisionTrees) {
+            return ruleDecisionTrees.stream()
+                    .flatMap(this::walk);
+        }
+
+        private Stream<RowSpec> walk(IRuleDecisionTree ruleDecisionTree) {
+            return walk(ruleDecisionTree.getRootOption());
+        }
+
+        private Stream<RowSpec> walk(IRuleOption ruleOption) {
+            final RowSpec nominalRowSpec = constraintReducer.reduceConstraintsToRowSpec(
+                    profileFields,
+                    ruleOption.getAtomicConstraints()
+            );
+
+            return ruleOption
+                    .getDecisions()
+                    .stream()
+                    .flatMap(x -> walk(x, nominalRowSpec));
+        }
+
+        private Stream<RowSpec> walk(IRuleDecision decision, RowSpec accumulatedSpec) {
+            return decision
+                    .getOptions()
+                    .stream()
+                    .flatMap(this::walk)
+                    .map(x -> RowSpec.merge(fieldSpecMerger, accumulatedSpec, x));
+        }
+    }
 }
--- generator/src/test/java/com/scottlogic/deg/generator/decisiontree/DecisionTreeToRowSpecsTests.java
+++ generator/src/test/java/com/scottlogic/deg/generator/decisiontree/DecisionTreeToRowSpecsTests.java
@@ -6,6 +6,8 @@ import com.scottlogic.deg.generator.Rule;
 import com.scottlogic.deg.generator.constraints.ConditionalConstraint;
 import com.scottlogic.deg.generator.constraints.IsEqualToConstantConstraint;
 import com.scottlogic.deg.generator.constraints.IsInSetConstraint;
+import com.scottlogic.deg.generator.reducer.ConstraintReducer;
+import com.scottlogic.deg.generator.restrictions.FieldSpecMerger;
 import com.scottlogic.deg.generator.restrictions.RowSpec;
 import com.scottlogic.deg.generator.walker.DecisionTreeWalker;
 import org.junit.jupiter.api.Test;
@@ -15,8 +17,14 @@ import java.util.Collections;
 import java.util.HashSet;
 
 public class DecisionTreeToRowSpecsTests {
-    final DecisionTreeWalker dTreeWalker = new DecisionTreeWalker();
+    final DecisionTreeWalker dTreeWalker = new DecisionTreeWalker(new ConstraintReducer(), new FieldSpecMerger());
     final DecisionTreeGenerator dTreeGenerator = new DecisionTreeGenerator();
+
+    @Test
+    public void test() {
+        final IDecisionTreeProfile dTree = dTreeGenerator.analyse(makeProfile());
+        final Iterable<RowSpec> rowSpecs = dTreeWalker.walk(dTree);
+    }
 
     private Profile makeProfile() {
         final Field country = new Field("country");
@@ -26,7 +34,7 @@ public class DecisionTreeToRowSpecsTests {
                 Arrays.asList(country, currency, city),
                 Arrays.asList(
                         new Rule(
-                                "country constrains city",
+                                "US country constrains city",
                                 Collections.singletonList(
                                         new ConditionalConstraint(
                                                 new IsEqualToConstantConstraint(
@@ -39,14 +47,53 @@ public class DecisionTreeToRowSpecsTests {
                                                 )
                                         )
                                 )
+                        ),
+                        new Rule(
+                                "GB country constrains city",
+                                Collections.singletonList(
+                                        new ConditionalConstraint(
+                                                new IsEqualToConstantConstraint(
+                                                        country,
+                                                        "GB"
+                                                ),
+                                                new IsInSetConstraint(
+                                                        city,
+                                                        new HashSet<>(Arrays.asList("Bristol", "London"))
+                                                )
+                                        )
+                                )
+                        ),
+                        new Rule(
+                                "US country constrains currency",
+                                Collections.singletonList(
+                                        new ConditionalConstraint(
+                                                new IsEqualToConstantConstraint(
+                                                        country,
+                                                        "US"
+                                                ),
+                                                new IsEqualToConstantConstraint(
+                                                        currency,
+                                                        "USD"
+                                                )
+                                        )
+                                )
+                        ),
+                        new Rule(
+                                "GB country constrains currency",
+                                Collections.singletonList(
+                                        new ConditionalConstraint(
+                                                new IsEqualToConstantConstraint(
+                                                        country,
+                                                        "GB"
+                                                ),
+                                                new IsEqualToConstantConstraint(
+                                                        currency,
+                                                        "GBP"
+                                                )
+                                        )
+                                )
                         )
                 )
         );
-    }
-
-    @Test
-    public void test() {
-        final IDecisionTreeProfile dTree = dTreeGenerator.analyse(makeProfile());
-        final Iterable<RowSpec> rowSpecs = dTreeWalker.walk(dTree);
     }
 }
