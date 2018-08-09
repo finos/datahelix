530251759f8998b6c2a8765d6f347eaaef755ad2
Alejandro Manuel Baronetti <abaronetti@scottlogic.co.uk>
2018-08-09 17:37:17 +0100

    More logic for classifiers

--- profiler/src/main/scala/com/scottlogic/deg/classifier/simple_classifier/SEDOLClassifier.scala
+++ profiler/src/main/scala/com/scottlogic/deg/classifier/simple_classifier/SEDOLClassifier.scala
@@ -1,13 +1,23 @@
 package com.scottlogic.deg.classifier.simple_classifier
 
-import com.scottlogic.deg.classifier.{Classifier, SemanticType}
+import com.scottlogic.deg.classifier.{Classifier, SEDOLType, SemanticType}
 
 object SEDOLClassifier extends Classifier {
+  private val sedolRegex = "[B-Db-dF-Hf-hJ-Nj-nP-Tp-tV-Xv-xYyZz\\d]{6}\\d";
+
   override def classify(input: String): Seq[SemanticType] = {
-    // TODO: Classifier logic
     if(input == null || input == ""){
       return List[SemanticType]()
     }
+
+    if(input.length != 7){
+      return List[SemanticType]()
+    }
+
+    if(input.matches(sedolRegex)){
+      return List[SemanticType](SEDOLType)
+    }
+
     return Seq[SemanticType]()
   }
 }
