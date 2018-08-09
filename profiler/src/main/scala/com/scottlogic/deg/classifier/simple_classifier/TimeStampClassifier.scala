8d5990a3785d1d7f8a011ba64030950cc54f2231
Alejandro Manuel Baronetti <abaronetti@scottlogic.co.uk>
2018-08-09 18:07:44 +0100

    More testing

--- profiler/src/main/scala/com/scottlogic/deg/classifier/simple_classifier/TimeStampClassifier.scala
+++ profiler/src/main/scala/com/scottlogic/deg/classifier/simple_classifier/TimeStampClassifier.scala
@@ -3,7 +3,7 @@ package com.scottlogic.deg.classifier.simple_classifier
 import com.scottlogic.deg.classifier.{Classifier, SemanticType, TimeStampType}
 
 object TimeStampClassifier extends Classifier {
-  private val dateFormats : List[String] = List[String]("\\d{4}[-/]\\d{2}[-/]d{2}", "\\d{2}[-/]\\d{2}[-/]d\\{4}")
+  private val dateFormats : List[String] = List[String]("\\d{4}[-/]\\d{2}[-/]\\d{2}", "\\d{2}[-/]\\d{2}[-/]\\d{4}")
 
   override def classify(input: String): Seq[SemanticType] = {
     if(input == null || input == ""){
