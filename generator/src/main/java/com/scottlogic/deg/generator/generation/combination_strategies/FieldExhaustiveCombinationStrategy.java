69bc0a97f048cdad139f032cb93d2421358b493d
Mark <>
2018-08-09 18:08:51 +0100

    FieldExhaustiveCombinationStrategy: Fixed issues raised in code review

--- generator/src/main/java/com/scottlogic/deg/generator/generation/combination_strategies/FieldExhaustiveCombinationStrategy.java
+++ generator/src/main/java/com/scottlogic/deg/generator/generation/combination_strategies/FieldExhaustiveCombinationStrategy.java
@@ -2,11 +2,12 @@ package com.scottlogic.deg.generator.generation.combination_strategies;
 
 import com.scottlogic.deg.generator.generation.databags.DataBag;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
-import java.util.stream.Stream;
 
 public class FieldExhaustiveCombinationStrategy implements ICombinationStrategy {
 
@@ -24,52 +25,46 @@ public class FieldExhaustiveCombinationStrategy implements ICombinationStrategy 
 
         @Override
         public Iterator<DataBag> iterator() {
-            return new FieldExhaustiveCombinationStrategy.InternalIterator(dataRowSequences);
+            List<Iterator<DataBag>> sequenceIterators = dataRowSequences.stream()
+                .map(Iterable::iterator)
+                .collect(Collectors.toList());
+
+            List<DataBag> baselines = new ArrayList<>(dataRowSequences.size());
+            for (int i = 0; i < sequenceIterators.size(); i++) {
+                if (!sequenceIterators.get(i).hasNext()) {
+                    return Collections.emptyIterator();
+                }
+
+                baselines.set(i, sequenceIterators.get(i).next());
+            }
+
+            return new InternalIterator(sequenceIterators, baselines);
         }
     }
 
     class InternalIterator implements Iterator<DataBag> {
+        private final List<DataBag> baselinesForInputSequences;
+        private final List<Iterator<DataBag>> inputSequencesIterators;
+
         private Integer indexOfSequenceToVary;
 
-        private final DataBag[] baselines;
-        private final List<Iterable<DataBag>> dataRowSequences;
-        private final List<Iterator<DataBag>> sequenceIterators;
+        public InternalIterator(
+            List<Iterator<DataBag>> inputSequencesIterators,
+            List<DataBag> baselinesForInputSequences) {
 
-        private final boolean isEmpty;
-
-        InternalIterator(List<Iterable<DataBag>> dataRowSequences) {
-            this.baselines = new DataBag[dataRowSequences.size()];
-
-            this.sequenceIterators = dataRowSequences.stream()
-                .map(Iterable::iterator)
-                .collect(Collectors.toList());
+            this.baselinesForInputSequences = baselinesForInputSequences;
+            this.inputSequencesIterators = inputSequencesIterators;
 
             this.indexOfSequenceToVary = null;
-
-            this.dataRowSequences = dataRowSequences;
-
-            for (int i = 0; i < sequenceIterators.size(); i++) {
-                if (!sequenceIterators.get(i).hasNext()) {
-                    this.isEmpty = true;
-                    return;
-                }
-
-                this.baselines[i] = sequenceIterators.get(i).next();
-            }
-
-            this.isEmpty = false;
         }
 
         @Override
         public boolean hasNext() {
-            if (this.isEmpty)
-                return false;
-
             if (this.indexOfSequenceToVary == null)
                 return true; // because this means we haven't output a baselines row yet. I know this code is awful, I'll definitely fix it -MH
 
             // kind of inefficient
-            return this.sequenceIterators.stream().anyMatch(Iterator::hasNext);
+            return this.inputSequencesIterators.stream().anyMatch(Iterator::hasNext);
         }
 
         @Override
@@ -77,21 +72,21 @@ public class FieldExhaustiveCombinationStrategy implements ICombinationStrategy 
             if (this.indexOfSequenceToVary == null) {
                 this.indexOfSequenceToVary = 0;
 
-                return Stream.of(this.baselines)
+                return this.baselinesForInputSequences.stream()
                     .reduce(new DataBag(), (db1, db2) -> DataBag.merge(db1, db2));
             }
 
-            return IntStream.range(0, this.dataRowSequences.size())
+            return IntStream.range(0, this.inputSequencesIterators.size())
                 .mapToObj(seqIndex -> {
                     if (seqIndex != this.indexOfSequenceToVary) {
-                        return this.baselines[seqIndex];
+                        return this.baselinesForInputSequences.get(seqIndex);
                     }
                     else {
-                        if (!this.sequenceIterators.get(seqIndex).hasNext()) {
+                        if (!this.inputSequencesIterators.get(seqIndex).hasNext()) {
                             this.indexOfSequenceToVary++;
-                            return this.baselines[seqIndex];
+                            return this.baselinesForInputSequences.get(seqIndex);
                         }
-                        return this.sequenceIterators.get(seqIndex).next();
+                        return this.inputSequencesIterators.get(seqIndex).next();
                     }
                 })
                 .reduce(new DataBag(), (db1, db2) -> DataBag.merge(db1, db2));
