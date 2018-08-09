9b8b0dbb1331d507fd81dbc75f27406f7b034492
Mark <>
2018-08-09 14:46:39 +0100

    Pruned some code in preparation for making numeric and string restrictions mutually exclusive

--- generator/src/main/java/com/scottlogic/deg/generator/generation/FieldSpecFulfiller.java
+++ generator/src/main/java/com/scottlogic/deg/generator/generation/FieldSpecFulfiller.java
@@ -62,15 +62,6 @@ public class FieldSpecFulfiller implements IDataPointSource {
             return simplifyStringIterator(stringIterator);
         }
 
-        if (spec.getStringRestrictions() != null && spec.getStringRestrictions().automaton != null &&
-                spec.getNumericRestrictions() != null &&
-                (spec.getNumericRestrictions().min != null || spec.getNumericRestrictions().max != null)) {
-            Automaton combinedAutomaton = spec.getStringRestrictions().automaton
-                    .intersection(getNumericAutomaton(spec.getNumericRestrictions()));
-            StringIterator iterator = new StringIterator(combinedAutomaton, getBlacklist());
-            return simplifyStringIterator(iterator);
-        }
-
         // no restrictions, just output some random bits of data
         return new SpecificDataPointsIterator(null, "string", 123, true);
     }
@@ -85,47 +76,5 @@ public class FieldSpecFulfiller implements IDataPointSource {
         if (stringIterator.hasNext())
             return new SingleObjectIterator(stringIterator.next());
         return new UnfulfillableIterator();
-    }
-
-    private Automaton getNumericAutomaton(NumericRestrictions restrictions) {
-        if (restrictions.min != null && restrictions.max != null) {
-            return getNumericRangeAutomaton(restrictions);
-        }
-        if (restrictions.max != null) {
-            return getNumericLessThanAutomaton(restrictions.max);
-        }
-        if (restrictions.min != null) {
-            return getNumericGreaterThanAutomaton(restrictions.min);
-        }
-        return getNumericUnboundedAutomaton();
-    }
-
-    private Automaton getNumericDecimalAutomaton() {
-        // matches either the empty string, or dot followed by one or more digits.
-        return new RegExp("()|\\.[0-9]+").toAutomaton();
-    }
-
-    private Automaton getNumericUnboundedAutomaton() {
-        return new RegExp("[1-9][0-9]*").toAutomaton().concatenate(getNumericDecimalAutomaton());
-    }
-
-    private Automaton getNumericLessThanAutomaton(NumericRestrictions.NumericLimit limit) {
-        BigDecimal roundedLimit = limit.getLimit().setScale(0, RoundingMode.FLOOR);
-        return Automaton.makeMaxInteger(Integer.toString(roundedLimit.intValue()))
-                .concatenate(getNumericDecimalAutomaton());
-    }
-
-    private Automaton getNumericGreaterThanAutomaton(NumericRestrictions.NumericLimit limit) {
-        BigDecimal roundedLimit = limit.getLimit().setScale(0, RoundingMode.CEILING);
-        Automaton automaton = Automaton.makeMinInteger(Integer.toString(roundedLimit.intValue()))
-                .concatenate(getNumericDecimalAutomaton());
-        return automaton;
-    }
-
-    private Automaton getNumericRangeAutomaton(NumericRestrictions restrictions) {
-        BigDecimal roundedMin = restrictions.min.getLimit().setScale(0, RoundingMode.CEILING);
-        BigDecimal roundedMax = restrictions.max.getLimit().setScale(0, RoundingMode.FLOOR);
-        return Automaton.makeInterval(roundedMin.intValue(), roundedMax.intValue(), 0)
-                .concatenate(getNumericDecimalAutomaton());
     }
 }
