a5f2df4e0f5df8023a6130f7c26e6db08299d90b
Alejandro Manuel Baronetti <abaronetti@scottlogic.co.uk>
2018-08-09 17:55:06 +0100

    Negative numbers

--- profiler/src/main/scala/com/scottlogic/deg/classifier/simple_classifier/IntegerClassifier.scala
+++ profiler/src/main/scala/com/scottlogic/deg/classifier/simple_classifier/IntegerClassifier.scala
@@ -3,7 +3,7 @@ package com.scottlogic.deg.classifier.simple_classifier
 import com.scottlogic.deg.classifier.{Classifier, IntegerType, SemanticType}
 
 object IntegerClassifier extends Classifier {
-  private val integerRegex : String = "^\\d+$";
+  private val integerRegex : String = "^[-]?\\d+$";
   override def classify(input: String): Seq[SemanticType] = {
     if(input == null || input == ""){
       return List[SemanticType]()
