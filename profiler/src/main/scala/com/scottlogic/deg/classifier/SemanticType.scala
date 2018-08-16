5e16660e0064f4a54f0b48c0269b033a5654a574
Joel Matthews <jmatthews@scottlogic.co.uk>
2018-08-16 10:55:09 +0100

    Add a rank to each SemanticType

--- profiler/src/main/scala/com/scottlogic/deg/classifier/SemanticType.scala
+++ profiler/src/main/scala/com/scottlogic/deg/classifier/SemanticType.scala
@@ -1,17 +1,17 @@
 package com.scottlogic.deg.classifier
 
-sealed trait SemanticType
-case object NullType extends SemanticType
-case object StringType extends SemanticType
-case object EmailType extends SemanticType
-case object NameType extends SemanticType
-case object CountryCodeType extends SemanticType
-case object CurrencyType extends SemanticType
-case object IntegerType extends SemanticType
-case object DoubleType extends SemanticType
-case object FloatType extends SemanticType
-case object TimeStampType extends SemanticType
-case object RICType extends SemanticType
-case object SEDOLType extends SemanticType
-case object ISINType extends SemanticType
-case object EnumType extends SemanticType
+sealed trait SemanticType {def rank: Int}
+case object NullType extends SemanticType {val rank = 0}
+case object StringType extends SemanticType {val rank = 1}
+case object IntegerType extends SemanticType {val rank = 2}
+case object DoubleType extends SemanticType {val rank = 3}
+case object FloatType extends SemanticType {val rank = 4}
+case object EmailType extends SemanticType {val rank = 5}
+case object NameType extends SemanticType {val rank = 6}
+case object CountryCodeType extends SemanticType {val rank = 7}
+case object CurrencyType extends SemanticType {val rank = 8}
+case object TimeStampType extends SemanticType {val rank = 9}
+case object RICType extends SemanticType {val rank = 10}
+case object SEDOLType extends SemanticType {val rank = 11}
+case object ISINType extends SemanticType {val rank = 12}
+case object EnumType extends SemanticType {val rank = 13}
