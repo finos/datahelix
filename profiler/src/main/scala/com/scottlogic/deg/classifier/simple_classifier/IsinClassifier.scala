faa41b30526482c83d45c85a0d983ace5b1c1d21
Yui-Wah (Clement) Lee <clee@scottlogic.co.uk>
2018-09-28 12:32:39 +0100

    Compute isinRegex only once

--- profiler/src/main/scala/com/scottlogic/deg/classifier/simple_classifier/IsinClassifier.scala
+++ profiler/src/main/scala/com/scottlogic/deg/classifier/simple_classifier/IsinClassifier.scala
@@ -3,16 +3,15 @@ package com.scottlogic.deg.classifier.simple_classifier
 import com.scottlogic.deg.classifier.{IsinType, SemanticType}
 
 object IsinClassifier extends Classifier {
+  /*
+   * ISIN: https://www.iso.org/standard/44811.html
+   * 
+   * "XS" is for codes obtained via an international central depository
+   * c.f. https://www.isin.net/xs-isin/
+   */
+  private val isinRegex : String = "^(XS|"+
+    CountryCodeClassifier.Iso2CountryCodes.mkString("|")+
+    ")([0-9A-Z]{9})([0-9]{1})$"
   override val semanticType: SemanticType = IsinType
-  override def matches(input: String): Boolean = {
-    val countryCodes = CountryCodeClassifier.Iso2CountryCodes.mkString("|")
-    /*
-     * ISIN: https://www.iso.org/standard/44811.html
-     * 
-     * "XS" is for codes obtained via an international central depository
-     * c.f. https://www.isin.net/xs-isin/
-     */
-    val isinRegex : String = "^(XS|"+countryCodes+")([0-9A-Z]{9})([0-9]{1})$";
-    input.matches(isinRegex)
-  }
+  override def matches(input: String): Boolean = input.matches(isinRegex)
 }
