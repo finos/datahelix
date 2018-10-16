5a3c08ba13f07e40d4097073d4b0a9c3e62c7f0e
Simon Laing <slaing@scottlogic.com>
2018-10-16 13:33:34 +0100

    Improved debugger visualisation of generator constraints
    Introduced string representations of the following constraints:
    - NotConstraint
    - IsInSetConstraint
    - IsNullConstraint

--- generator/src/main/java/com/scottlogic/deg/generator/constraints/IsNullConstraint.java
+++ generator/src/main/java/com/scottlogic/deg/generator/constraints/IsNullConstraint.java
@@ -1,6 +1,7 @@
 package com.scottlogic.deg.generator.constraints;
 
 import com.scottlogic.deg.generator.Field;
+import com.scottlogic.deg.generator.restrictions.NullRestrictions;
 
 public class IsNullConstraint implements IConstraint
 {
@@ -13,5 +14,12 @@ public class IsNullConstraint implements IConstraint
     @Override
     public String toDotLabel() {
         return String.format("%s is null", field.name);
+    }
+
+    public String toString(){
+        return String.format(
+                "%s: %s",
+                NullRestrictions.Nullness.MustBeNull,
+                field.toString());
     }
 }
