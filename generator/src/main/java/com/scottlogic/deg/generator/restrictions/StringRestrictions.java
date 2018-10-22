23d57df0f3ac1e0fe4cff1fbc7327556f00497e9
Simon Laing <slaing@scottlogic.com>
2018-10-22 14:01:30 +0100

    Introduced string representations of Regex constraints

--- generator/src/main/java/com/scottlogic/deg/generator/restrictions/StringRestrictions.java
+++ generator/src/main/java/com/scottlogic/deg/generator/restrictions/StringRestrictions.java
@@ -1,6 +1,9 @@
 package com.scottlogic.deg.generator.restrictions;
 
 import com.scottlogic.deg.generator.generation.IStringGenerator;
+import com.scottlogic.deg.generator.generation.IsinStringGenerator;
+import com.scottlogic.deg.generator.generation.RegexStringGenerator;
+import com.scottlogic.deg.generator.generation.SedolStringGenerator;
 
 /**
  * https://github.com/ScottLogic/data-engineering-generator/blob/ws/experimental-data-constraint-solver/data-constraint-poc/src/main/java/com/scottlogic/deg/constrainer/util/RegexProcessor.java
@@ -8,6 +11,10 @@ import com.scottlogic.deg.generator.generation.IStringGenerator;
  */
 public class StringRestrictions {
     public IStringGenerator stringGenerator;
+
+    public StringRestrictions() {
+
+    }
 
     public static boolean isString(Object o) {
         return o instanceof String;
@@ -21,5 +28,19 @@ public class StringRestrictions {
         String s = (String) o;
         return stringGenerator.match(s);
 
+    }
+
+    @Override
+    public String toString() {
+        if (stringGenerator instanceof RegexStringGenerator)
+            return String.format("Regex: `%s`", stringGenerator.toString());
+
+        if (stringGenerator instanceof SedolStringGenerator)
+            return "Sedol";
+
+        if (stringGenerator instanceof IsinStringGenerator)
+            return "Isin";
+
+        return stringGenerator.toString();
     }
 }
