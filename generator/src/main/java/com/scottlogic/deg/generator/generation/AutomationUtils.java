13ac2e0b34f7b14e05667ac331efcbe2bee5e6ea
Olly Hayes <ohayes@scottlogic.co.uk>
2018-10-30 11:53:38 +0000

    Switch to non-recursive longest example finder

--- generator/src/main/java/com/scottlogic/deg/generator/generation/AutomationUtils.java
+++ generator/src/main/java/com/scottlogic/deg/generator/generation/AutomationUtils.java
@@ -4,14 +4,13 @@ import dk.brics.automaton.Automaton;
 import dk.brics.automaton.State;
 import dk.brics.automaton.Transition;
 
+import javax.swing.text.StyledEditorKit;
 import java.util.*;
 import java.util.stream.Collectors;
 
 class AutomationUtils {
 
     private static final char printableChar = ' ';
-
-
 
     // Returns a string of printable characters based on the supplied automaton.
     // This method ignores transitions that lead back to the same node so recursive states will only ever produce a
@@ -25,7 +24,34 @@ class AutomationUtils {
         // The start node always has one transition to start
         Transition start = automaton.getInitialState().getSortedTransitions(true).get(0);
 
-        visit(start, new Stack<>(), solution);
+        Stack<List<Transition>> transitionsToVisit = new Stack<>();
+        transitionsToVisit.add(Collections.singletonList(start));
+
+        while (!transitionsToVisit.empty()) {
+            List<Transition> currentTransitionPath = transitionsToVisit.pop();
+            Transition currentTransition = currentTransitionPath.get(currentTransitionPath.size() - 1);
+
+            State dest = currentTransition.getDest();
+
+            List<Transition> nonRecursiveTransitions = dest.getTransitions()
+                .stream()
+                .filter(x -> x.getDest() != dest)
+                .collect(Collectors.toList());
+
+            if (dest.isAccept() && nonRecursiveTransitions.isEmpty() && currentTransitionPath.size() > solution.size()) {
+
+                solution.clear();
+                solution.addAll(currentTransitionPath);
+            }
+
+            for (Transition nextTransition : nonRecursiveTransitions) {
+                List<Transition> nextTransitionPath = new ArrayList<>();
+                nextTransitionPath.addAll(currentTransitionPath);
+                nextTransitionPath.add(nextTransition);
+
+                transitionsToVisit.add(nextTransitionPath);
+            }
+        }
 
         StringBuilder sb = new StringBuilder();
 
@@ -34,31 +60,6 @@ class AutomationUtils {
         }
 
         return sb.toString();
-
-    }
-
-    private static void visit(Transition transition, Stack<Transition> ancestors, Stack<Transition> currentBest) {
-
-        ancestors.push(transition);
-
-        State dest = transition.getDest();
-
-        List<Transition> filteredTransitions = dest.getTransitions()
-            .stream()
-            .filter(x -> x.getDest() != dest)
-            .collect(Collectors.toList());
-
-        if (dest.isAccept() && filteredTransitions.isEmpty() && ancestors.size() > currentBest.size()) {
-
-            currentBest.clear();
-            ancestors.forEach(currentBest::add);
-        }
-
-        for (Transition next : filteredTransitions) {
-            visit(next, ancestors, currentBest);
-        }
-
-        ancestors.pop();
     }
 
     // Taken from Automaton but updated to return printable characters
