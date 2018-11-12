b00108e3731b1a66836f2a20b3e465264fc0719c
Paul Daulby <pdaulby@scottlogic.com>
2018-11-12 11:35:26 +0000

    Refactor constraint iterator to constraint and leaf constraint

--- generator/src/main/java/com/scottlogic/deg/generator/walker/ConstraintIterator.java
+++ generator/src/main/java/com/scottlogic/deg/generator/walker/ConstraintIterator.java
@@ -1,55 +1,34 @@
 package com.scottlogic.deg.generator.walker;
 
 import com.scottlogic.deg.generator.decisiontree.ConstraintNode;
-import com.scottlogic.deg.generator.decisiontree.DecisionNode;
-import com.scottlogic.deg.generator.decisiontree.DecisionTree;
 import com.scottlogic.deg.generator.walker.routes.RowSpecRoute;
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
-
-import javax.management.Notification;
-import java.util.Iterator;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.Queue;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-import java.util.stream.StreamSupport;
 
-//TODO re implement with end node as different class, and use a builder to construct
-public class ConstraintIterator implements Iterator<RowSpecRoute> {
+public class ConstraintIterator implements IConstraintIterator {
 
     private int decisionIndexFromParent;
-
     private IDecisionIterator decisions;
-    private boolean hasNext;
 
-    public ConstraintIterator(ConstraintNode constraintNode) { this(constraintNode, 0); }
-    public ConstraintIterator(ConstraintNode constraintNode, int decisionIndexFromParent){
+    public static IConstraintIterator build(ConstraintNode constraintNode, int decisionIndexFromParent){
+        if (constraintNode.getDecisions().isEmpty()){
+            return new LeafConstraintIterator(decisionIndexFromParent);
+        }
+        return new ConstraintIterator(constraintNode, decisionIndexFromParent);
+    }
+    public static IConstraintIterator build(ConstraintNode constraintNode){ return build(constraintNode, 0); }
+
+    private ConstraintIterator(ConstraintNode constraintNode, int decisionIndexFromParent){
         decisions = DecisionIterator.build(constraintNode.getDecisions());
-        hasNext = true;
         this.decisionIndexFromParent = decisionIndexFromParent;
     }
 
     @Override
     public boolean hasNext() {
-        if (decisions == null) {
-            return hasNext;
-        }
-
         return decisions.hasNext();
     }
 
     @Override
     public RowSpecRoute next() {
-
-        if(decisions == null) {
-            hasNext = false;
-            RowSpecRoute rowSpecRoute = new RowSpecRoute();
-            rowSpecRoute.decisionIndex = decisionIndexFromParent;
-            rowSpecRoute.subRoutes = new RowSpecRoute[]{};
-            return rowSpecRoute;
-        }
-
         RowSpecRoute rowSpecRoute = new RowSpecRoute();
         rowSpecRoute.decisionIndex = decisionIndexFromParent;
         List<RowSpecRoute> next = decisions.next();
@@ -58,8 +37,7 @@ public class ConstraintIterator implements Iterator<RowSpecRoute> {
         return rowSpecRoute;
     }
 
-    void reset(){
-        hasNext=true;
-        if(decisions!= null) decisions.reset();
+    public void reset(){
+        decisions.reset();
     }
 }
--- generator/src/main/java/com/scottlogic/deg/generator/walker/routes/ExhaustiveProducer.java
+++ generator/src/main/java/com/scottlogic/deg/generator/walker/routes/ExhaustiveProducer.java
@@ -2,7 +2,6 @@ package com.scottlogic.deg.generator.walker.routes;
 
 import com.scottlogic.deg.generator.decisiontree.DecisionTree;
 import com.scottlogic.deg.generator.walker.ConstraintIterator;
-import com.scottlogic.deg.generator.walker.DecisionIterator;
 
 import java.util.Iterator;
 import java.util.stream.Stream;
@@ -11,7 +10,7 @@ import java.util.stream.StreamSupport;
 public class ExhaustiveProducer implements RowSpecRouteProducer{
     @Override
     public Stream<RowSpecRoute> produceRoutes(DecisionTree tree) {
-        Iterator<RowSpecRoute> iterator = new ConstraintIterator(tree.getRootNode());
+        Iterator<RowSpecRoute> iterator = ConstraintIterator.build(tree.getRootNode());
         Iterable<RowSpecRoute> iterable = ()->iterator;
         return StreamSupport.stream(iterable.spliterator(), false);
     }
