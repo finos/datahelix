18869555ad296af2e5804cfacf17fb494eecebb6
Paul Daulby <pdaulby@scottlogic.com>
2018-11-12 11:14:23 +0000

    refactor decision iterator into decision, and end decision

---
+++ generator/src/main/java/com/scottlogic/deg/generator/walker/EndDecisionIterator.java
@@ -0,0 +1,47 @@
+package com.scottlogic.deg.generator.walker;
+
+import com.scottlogic.deg.generator.decisiontree.ConstraintNode;
+import com.scottlogic.deg.generator.decisiontree.DecisionNode;
+import com.scottlogic.deg.generator.walker.routes.RowSpecRoute;
+
+import java.util.*;
+
+public class EndDecisionIterator implements IDecisionIterator{
+    private List<ConstraintIterator> options = new ArrayList<>();
+    private int currentOption;
+    private RowSpecRoute currentOptionsSubroute;
+
+    public EndDecisionIterator(DecisionNode decisionNode){
+        int count = 0;
+        for (ConstraintNode constraintNode: decisionNode.getOptions()) {
+            options.add(new ConstraintIterator(constraintNode, count));
+            count++;
+        }
+    }
+
+    @Override
+    public boolean hasNext() {
+        return currentOption < options.size();
+    }
+
+    @Override
+    public List<RowSpecRoute> next() {
+            ConstraintIterator currentOptionIterator = options.get(currentOption);
+            currentOptionsSubroute = currentOptionIterator.next();
+            if (!currentOptionIterator.hasNext()){
+                currentOption++;
+            }
+            List<RowSpecRoute> r = new ArrayList<>();
+            r.add(0, currentOptionsSubroute);
+            return r;
+    }
+
+    @Override
+    public void reset(){
+        currentOption = 0;
+        currentOptionsSubroute = null;
+        for (ConstraintIterator option: options) {
+            option.reset();
+        }
+    }
+}
---
+++ generator/src/main/java/com/scottlogic/deg/generator/walker/IDecisionIterator.java
@@ -0,0 +1,16 @@
+package com.scottlogic.deg.generator.walker;
+
+import com.scottlogic.deg.generator.walker.routes.RowSpecRoute;
+
+import java.util.Iterator;
+import java.util.List;
+
+public interface IDecisionIterator extends Iterator<List<RowSpecRoute>> {
+    @Override
+    boolean hasNext();
+
+    @Override
+    List<RowSpecRoute> next();
+
+    void reset();
+}
