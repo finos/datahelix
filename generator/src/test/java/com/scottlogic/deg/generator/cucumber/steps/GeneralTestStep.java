ba089d28636c4e62748bb9add8707ea5d019f973
Simon Laing <slaing@scottlogic.com>
2018-11-13 12:50:02 +0000

    Introduced matches in-any-order matcher

--- generator/src/test/java/com/scottlogic/deg/generator/cucumber/steps/GeneralTestStep.java
+++ generator/src/test/java/com/scottlogic/deg/generator/cucumber/steps/GeneralTestStep.java
@@ -89,7 +89,7 @@ public class GeneralTestStep {
     @Then("^the following data should be generated:$")
     public void theFollowingDataShouldBeGenerated(List<Map<String, String>> expectedResultsTable) {
         GeneratedTestData data = getExpectedAndGeneratedData(expectedResultsTable);
-        Assert.assertThat(data.expectedData, new RowsPresentMatcher(data.generatedData));
+        Assert.assertThat(data.generatedData, new RowsMatchAnyOrderMatcher(data.expectedData));
     }
 
     @Then("^the following data should be generated in order:$")
@@ -133,6 +133,47 @@ public class GeneralTestStep {
         GeneratedTestData(List <List<Object>> expectedData, List <List<Object>> generatedData){
             this.expectedData = expectedData;
             this.generatedData = generatedData;
+        }
+    }
+
+    class RowsMatchAnyOrderMatcher extends BaseMatcher<List<List<Object>>>{
+        private final List<List<Object>> expectedRows;
+
+        public RowsMatchAnyOrderMatcher(List<List<Object>> expectedRows) {
+            if (expectedRows == null)
+                expectedRows = new ArrayList<>();
+            this.expectedRows = expectedRows;
+        }
+
+        @Override
+        public boolean matches(Object o) {
+            List<List<Object>> actualRows = (List<List<Object>>) o;
+
+            if (expectedRows.size() != actualRows.size())
+                return false;
+
+            //check that every expected row exists in the actual rows
+            Collection<RowMatcher> expectedMatchers = getExpectedMatchers();
+            for (List<Object> actualRow : actualRows){
+                if (!expectedMatchers.stream().anyMatch(matcher -> matcher.matches(actualRow))){
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        @Override
+        public void describeTo(Description description) {
+            Collection<RowMatcher> expectedMatchers = getExpectedMatchers();
+            description.appendText(Objects.toString(expectedMatchers));
+        }
+
+        private List<RowMatcher> getExpectedMatchers() {
+            return expectedRows
+                .stream()
+                .map(expectedRow -> new RowMatcher(expectedRow))
+                .collect(Collectors.toList());
         }
     }
 
