3c533778f723469a6d36d0e8d55f68e372196513
Simon Laing <slaing@scottlogic.com>
2018-11-14 12:00:06 +0000

    Updated profile to require dates in object format
    i.e. { "date": "yyyy-MM-ddTHH:mm:ss.SSS" } instead of string format.
    This resolves the issue whereby EqualTo constraints don't know that the value is a Date rather than a regular string

--- generator/src/test/java/com/scottlogic/deg/generator/cucumber/steps/DateValueStep.java
+++ generator/src/test/java/com/scottlogic/deg/generator/cucumber/steps/DateValueStep.java
@@ -1,8 +1,10 @@
 package com.scottlogic.deg.generator.cucumber.steps;
 
 import com.scottlogic.deg.generator.cucumber.utils.DegTestState;
-import com.scottlogic.deg.generator.cucumber.utils.GeneratorTestUtilities;
 import cucumber.api.java.en.When;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class DateValueStep {
 
@@ -15,11 +17,17 @@ public class DateValueStep {
 
     @When("{fieldVar} is {operator} {dateString}")
     public void whenFieldIsConstrainedByDateValue(String fieldName, String constraintName, String value) throws Exception {
-        this.state.addConstraint(fieldName, constraintName, value);
+        this.state.addConstraint(fieldName, constraintName, dateObject(value));
     }
 
     @When("{fieldVar} is anything but {operator} {dateString}")
     public void whenFieldIsNotConstrainedByDateValue(String fieldName, String constraintName, String value) throws Exception {
-        this.state.addNotConstraint(fieldName, constraintName, value);
+        this.state.addNotConstraint(fieldName, constraintName, dateObject(value));
+    }
+
+    private static Map dateObject(String dateValue) {
+        HashMap map = new HashMap();
+        map.put("date", dateValue);
+        return map;
     }
 }
