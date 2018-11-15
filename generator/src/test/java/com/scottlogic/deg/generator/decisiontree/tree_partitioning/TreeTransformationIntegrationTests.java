65329e00c1a1bf69395a8948860fc79a36bb8301
Yui-Wah (Clement) Lee <clee@scottlogic.co.uk>
2018-11-15 13:07:13 +0000

    Factor the code and rename to TreeTransformationIntegrationTests.java
    Note that the code has been changed to expect filenames of `expected.json` but not the old name (for partitioner tests) of `expected-partitioning.json`, but the physical files under `...\generator\resources\partitioning-tests\` have not been renamed.  I have coordinated with @steve-tennantsl that for now the @TestFactory for partitioner tests are disabled (using @Disabled).  Steve is working on some of the json files currently so prefer to rename those files at a later time.  He will also re-enable the partitioner tests at that point.

--- generator/src/test/java/com/scottlogic/deg/generator/decisiontree/tree_partitioning/TreeTransformationIntegrationTests.java
+++ generator/src/test/java/com/scottlogic/deg/generator/decisiontree/tree_partitioning/TreeTransformationIntegrationTests.java
@@ -5,12 +5,15 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.scottlogic.deg.generator.Profile;
 import com.scottlogic.deg.generator.decisiontree.DecisionTree;
 import com.scottlogic.deg.generator.decisiontree.DecisionTreeGenerator;
+import com.scottlogic.deg.generator.decisiontree.DecisionTreeOptimiser;
 import com.scottlogic.deg.generator.decisiontree.IDecisionTreeGenerator;
+import com.scottlogic.deg.generator.decisiontree.IDecisionTreeOptimiser;
 import com.scottlogic.deg.generator.decisiontree.tree_partitioning.test_utils.*;
 import com.scottlogic.deg.generator.decisiontree.tree_partitioning.test_utils.mapping.DecisionTreeMapper;
 import com.scottlogic.deg.generator.inputs.InvalidProfileException;
 import com.scottlogic.deg.generator.inputs.ProfileReader;
 import org.junit.Assert;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DynamicTest;
 import org.junit.jupiter.api.TestFactory;
 
@@ -21,37 +24,84 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+class TreeTransformationIntegrationTests {
+    private enum XUT { // Transformation Under Test
+        TREE_PARTITION ("partitioning-tests"),
+        TREE_OPTIMISE ("optimiser-tests");
+        
+        private String testsDirName;
 
-class DecisionTreePartitionerIntegrationTests {
-    private static final String treePartitionerTestsDirectory = "../generator/resources/partitioning-tests/";
+        public String getTestsDirName() {
+            return testsDirName;
+        }
+
+        private XUT(String testDirsName) {
+            this.testsDirName = testDirsName;
+        }
+    }
 
     private final IDecisionTreeGenerator decisionTreeGenerator = new DecisionTreeGenerator();
-    private final ITreePartitioner treePartitioner = new TreePartitioner();
     private final ObjectMapper jsonMapper = new ObjectMapper();
     private final DecisionTreeMapper decisionTreeMapper = new DecisionTreeMapper();
 
+    @Disabled
     @TestFactory
     Collection<DynamicTest> decisionTreePartitioner_givenProfileInputs_resultEqualsProfileOutputs() {
+    	return doTest(XUT.TREE_PARTITION);
+    }
+    
+    @TestFactory
+    Collection<DynamicTest> decisionTreeOptimiser_givenProfileInputs_resultEqualsProfileOutputs() {
+    	return doTest(XUT.TREE_OPTIMISE);
+    }
+    
+    private List<DecisionTree> transformTree(DecisionTree beforeTree, XUT xut) {
+        List<DecisionTree> actualTrees = null;
+        
+        switch (xut) {
+        case TREE_PARTITION:
+            ITreePartitioner treePartitioner = new TreePartitioner();
+            actualTrees = treePartitioner
+                .splitTreeIntoPartitions(beforeTree)
+                .collect(Collectors.toList());            
+            break;
+        case TREE_OPTIMISE:
+            IDecisionTreeOptimiser treeOptimiser = new DecisionTreeOptimiser();            
+            DecisionTree afterTree = treeOptimiser.optimiseTree(beforeTree);
+            actualTrees = Collections.singletonList(afterTree);
+            break;
+        default:
+            throw new UnsupportedOperationException("Illegal transformation under test: " 
+                        + xut.name());
+        }
+        
+        return actualTrees;
+    }
+    
+    private Collection<DynamicTest> doTest(XUT xut) {
+        final String FS = File.separator;
+        final String testsDirPathPrefix = ".." + FS + "generator" + FS + "resources" + FS;
+        String testsDirPathName = testsDirPathPrefix + FS + xut.getTestsDirName() + FS;
         TreeComparisonReporter reporter = new TreeComparisonReporter();
 
-        return getPartitioningTestsDirectory().map(directory -> {
+        return getTestFiles(testsDirPathName).map(directory -> {
             try {
-                File inputFile = new File(directory.getPath() + "/profile.json");
-                File outputFile = new File(directory.getPath() + "/expected-partitioning.json");
+                File inputFile = new File(directory.getPath() + FS + "profile.json");
+                File outputFile = new File(directory.getPath() + FS + "expected.json");
                 Profile inputProfile = getProfile(inputFile.toPath());
 
-                DecisionTree decisionTree = decisionTreeGenerator.analyse(inputProfile).getMergedTree();
-                final List<DecisionTree> actualPartitionedTrees = treePartitioner
-                    .splitTreeIntoPartitions(decisionTree)
-                    .collect(Collectors.toList());
-
+                DecisionTree beforeTree = decisionTreeGenerator.analyse(inputProfile).getMergedTree();
+                
+                final List<DecisionTree> actualTrees = transformTree(beforeTree, xut);
+                
                 List<DecisionTreeDto> expectedTreeDto = getMappedExpectedOutput(outputFile);
-                final List<DecisionTree> expectedPartitionedTrees = expectedTreeDto.stream()
+                final List<DecisionTree> expectedTrees = expectedTreeDto.stream()
                     .map(decisionTreeMapper::map)
                     .collect(Collectors.toList());
 
@@ -72,8 +122,8 @@ class DecisionTreePartitionerIntegrationTests {
                     );
 
                     boolean match = anyOrderComparer.equals(
-                        expectedPartitionedTrees,
-                        actualPartitionedTrees);
+                        expectedTrees,
+                        actualTrees);
 
                     if (!match) {
                         reporter.reportMessages(context);
@@ -88,9 +138,9 @@ class DecisionTreePartitionerIntegrationTests {
         }).collect(Collectors.toList());
     }
 
-    private Stream<File> getPartitioningTestsDirectory() {
+    private Stream<File> getTestFiles(String testsDirectory) {
         return
-            Stream.of(Objects.requireNonNull(Paths.get(treePartitionerTestsDirectory)
+            Stream.of(Objects.requireNonNull(Paths.get(testsDirectory)
                 .toFile()
                 .listFiles(File::isDirectory)));
     }
