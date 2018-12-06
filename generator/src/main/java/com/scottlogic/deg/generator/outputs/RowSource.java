7f3b30ecf5246f138098496e6d86a0edc218023a
Simon Laing <slaing@scottlogic.com>
2018-12-06 15:38:48 +0000

    Remove unused imports

--- generator/src/main/java/com/scottlogic/deg/generator/outputs/RowSource.java
+++ generator/src/main/java/com/scottlogic/deg/generator/outputs/RowSource.java
@@ -1,9 +1,6 @@
 package com.scottlogic.deg.generator.outputs;
 
-import com.scottlogic.deg.generator.constraints.IConstraint;
-
 import java.util.List;
-import java.util.Set;
 
 public class RowSource {
     public List<CellSource> columns;
