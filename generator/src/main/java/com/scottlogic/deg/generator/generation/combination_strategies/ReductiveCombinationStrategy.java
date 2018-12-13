7f62b6ccc8e88afe6f4c7622253fc7277c1a6154
Simon Laing <slaing@scottlogic.com>
2018-12-13 07:44:12 +0000

    Rename ICombinationStrategy -> CombinationStrategy

--- generator/src/main/java/com/scottlogic/deg/generator/generation/combination_strategies/ReductiveCombinationStrategy.java
+++ generator/src/main/java/com/scottlogic/deg/generator/generation/combination_strategies/ReductiveCombinationStrategy.java
@@ -6,7 +6,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-public class ReductiveCombinationStrategy implements ICombinationStrategy {
+public class ReductiveCombinationStrategy implements CombinationStrategy {
     @Override
     public Stream<DataBag> permute(Stream<Stream<DataBag>> dataBagSequences) {
 
