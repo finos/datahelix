9a745c0f8ca8ddd2e3ff0fcd46f1e8f509611e3a
Harry Bedford <hbedford@scottlogic.co.uk>
2019-01-07 16:35:15 +0000

    removed unecessary providers and adjusted bindings

--- generator/src/main/java/com/scottlogic/deg/generator/Guice/IoCContainer.java
+++ generator/src/main/java/com/scottlogic/deg/generator/Guice/IoCContainer.java
@@ -11,11 +11,14 @@ import com.scottlogic.deg.generator.decisiontree.DecisionTreeOptimiser;
 import com.scottlogic.deg.generator.decisiontree.tree_partitioning.TreePartitioner;
 import com.scottlogic.deg.generator.generation.*;
 import com.scottlogic.deg.generator.outputs.targets.FileOutputTarget;
+import com.scottlogic.deg.generator.walker.CartesianProductDecisionTreeWalker;
 import com.scottlogic.deg.generator.walker.DecisionTreeWalker;
 import com.scottlogic.deg.generator.walker.DecisionTreeWalkerFactory;
+import com.scottlogic.deg.generator.walker.ReductiveDecisionTreeWalker;
 import com.scottlogic.deg.generator.walker.reductive.IterationVisualiser;
 import com.scottlogic.deg.generator.walker.reductive.NoOpIterationVisualiser;
 import com.scottlogic.deg.generator.walker.reductive.field_selection_strategy.FixFieldStrategy;
+import com.scottlogic.deg.generator.walker.reductive.field_selection_strategy.HierarchicalDependencyFixFieldStrategy;
 
 public class IoCContainer extends AbstractModule {
     private final CommandLineBase commandLine;
@@ -30,14 +33,18 @@ public class IoCContainer extends AbstractModule {
         bindAllCommandLineTypes();
 
         // Bind providers - used to retrieve implementations based on user input
-        bind(TreePartitioner.class).toProvider(TreePartitioningProvider.class);
+        bind(DecisionTreeDataGenerator.class).toProvider(DataGeneratorProvider.class);
         bind(DecisionTreeOptimiser.class).toProvider(DecisionTreeOptimiserProvider.class);
         bind(GenerationConfig.class).toProvider(GenerationConfigProvider.class);
         bind(Profile.class).toProvider(ProfileProvider.class);
         bind(FixFieldStrategy.class).toProvider(FixFieldStrategyProvider.class);
         bind(DecisionTreeWalkerFactory.class).toProvider(DecisionTreeWalkerFactoryProvider.class);
         bind(FileOutputTarget.class).toProvider(FileOutputTargetProvider.class);
+        bind(GenerationConfig.class).toProvider(GenerationConfigProvider.class);
         bind(GenerationEngine.class).toProvider(GenerationEngineProvider.class);
+        bind(Profile.class).toProvider(ProfileProvider.class);
+        bind(TreePartitioner.class).toProvider(TreePartitioningProvider.class);
+        
         // Bind known implementations - no user input required
         bind(DataGeneratorMonitor.class).to(ReductiveDataGeneratorMonitor.class);
         bind(ReductiveDataGeneratorMonitor.class).to(NoopDataGeneratorMonitor.class);
@@ -45,6 +52,8 @@ public class IoCContainer extends AbstractModule {
         bind(DecisionTreeWalker.class).annotatedWith(Names.named("cartesian")).to(CartesianProductDecisionTreeWalker.class);
         bind(DecisionTreeWalker.class).annotatedWith(Names.named("reductive")).to(ReductiveDecisionTreeWalker.class);
         bind(DecisionTreeWalker.class).toProvider(DecisionTreeWalkerProvider.class);
+        bind(DecisionTreeWalker.class).annotatedWith(Names.named("reductive")).to(ReductiveDecisionTreeWalker.class);
+        bind(DecisionTreeWalker.class).annotatedWith(Names.named("cartesian")).to(CartesianProductDecisionTreeWalker.class);
     }
 
     private void bindAllCommandLineTypes() {
