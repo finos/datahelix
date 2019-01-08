36bda6199519af6634433a9622f143239ff961f7
Harry Bedford <hbedford@scottlogic.co.uk>
2019-01-08 09:53:15 +0000

    removed unecessary providers, changed to explicit bindings

--- generator/src/main/java/com/scottlogic/deg/generator/Guice/IoCContainer.java
+++ generator/src/main/java/com/scottlogic/deg/generator/Guice/IoCContainer.java
@@ -5,16 +5,19 @@ import com.google.inject.name.Names;
 import com.scottlogic.deg.generator.CommandLine.CanGenerate;
 import com.scottlogic.deg.generator.CommandLine.CommandLineBase;
 import com.scottlogic.deg.generator.CommandLine.GenerateCommandLine;
+import com.scottlogic.deg.generator.CommandLine.GenerateExecute;
 import com.scottlogic.deg.generator.GenerationEngine;
 import com.scottlogic.deg.generator.Profile;
 import com.scottlogic.deg.generator.decisiontree.DecisionTreeOptimiser;
 import com.scottlogic.deg.generator.decisiontree.tree_partitioning.TreePartitioner;
 import com.scottlogic.deg.generator.generation.*;
 import com.scottlogic.deg.generator.outputs.targets.FileOutputTarget;
-import com.scottlogic.deg.generator.walker.CartesianProductDecisionTreeWalker;
-import com.scottlogic.deg.generator.walker.DecisionTreeWalker;
-import com.scottlogic.deg.generator.walker.DecisionTreeWalkerFactory;
-import com.scottlogic.deg.generator.walker.ReductiveDecisionTreeWalker;
+import com.scottlogic.deg.generator.outputs.targets.OutputTarget;
+import com.scottlogic.deg.generator.reducer.ConstraintReducer;
+import com.scottlogic.deg.generator.restrictions.FieldSpecFactory;
+import com.scottlogic.deg.generator.restrictions.FieldSpecMerger;
+import com.scottlogic.deg.generator.restrictions.RowSpecMerger;
+import com.scottlogic.deg.generator.walker.*;
 import com.scottlogic.deg.generator.walker.reductive.IterationVisualiser;
 import com.scottlogic.deg.generator.walker.reductive.NoOpIterationVisualiser;
 import com.scottlogic.deg.generator.walker.reductive.field_selection_strategy.FixFieldStrategy;
@@ -33,15 +36,11 @@ public class IoCContainer extends AbstractModule {
         bindAllCommandLineTypes();
 
         // Bind providers - used to retrieve implementations based on user input
-        bind(DecisionTreeDataGenerator.class).toProvider(DataGeneratorProvider.class);
         bind(DecisionTreeOptimiser.class).toProvider(DecisionTreeOptimiserProvider.class);
         bind(GenerationConfig.class).toProvider(GenerationConfigProvider.class);
         bind(Profile.class).toProvider(ProfileProvider.class);
-        bind(FixFieldStrategy.class).toProvider(FixFieldStrategyProvider.class);
-        bind(DecisionTreeWalkerFactory.class).toProvider(DecisionTreeWalkerFactoryProvider.class);
         bind(FileOutputTarget.class).toProvider(FileOutputTargetProvider.class);
         bind(GenerationConfig.class).toProvider(GenerationConfigProvider.class);
-        bind(GenerationEngine.class).toProvider(GenerationEngineProvider.class);
         bind(Profile.class).toProvider(ProfileProvider.class);
         bind(TreePartitioner.class).toProvider(TreePartitioningProvider.class);
         
@@ -51,9 +50,11 @@ public class IoCContainer extends AbstractModule {
         bind(IterationVisualiser.class).to(NoOpIterationVisualiser.class);
         bind(DecisionTreeWalker.class).annotatedWith(Names.named("cartesian")).to(CartesianProductDecisionTreeWalker.class);
         bind(DecisionTreeWalker.class).annotatedWith(Names.named("reductive")).to(ReductiveDecisionTreeWalker.class);
+        bind(DecisionTreeWalkerFactory.class).to(RuntimeDecisionTreeWalkerFactory.class);
+        bind(OutputTarget.class).to(FileOutputTarget.class);
         bind(DecisionTreeWalker.class).toProvider(DecisionTreeWalkerProvider.class);
-        bind(DecisionTreeWalker.class).annotatedWith(Names.named("reductive")).to(ReductiveDecisionTreeWalker.class);
         bind(DecisionTreeWalker.class).annotatedWith(Names.named("cartesian")).to(CartesianProductDecisionTreeWalker.class);
+        bind(DecisionTreeWalker.class).annotatedWith(Names.named("reductive")).to(ReductiveDecisionTreeWalker.class);
     }
 
     private void bindAllCommandLineTypes() {
