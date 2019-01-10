b3d5f6bc0af01327d337507c919956220c9af5dd
Harry Bedford <hbedford@scottlogic.co.uk>
2019-01-10 12:22:49 +0000

    replaced canGenerate interface with already present GenerationConfigSource

--- generator/src/main/java/com/scottlogic/deg/generator/CommandLine/CanGenerate.java
+++
@@ -1,9 +0,0 @@
-package com.scottlogic.deg.generator.CommandLine;
-
-import com.scottlogic.deg.generator.generation.GenerationConfig;
-
-public interface CanGenerate{
-    GenerationConfig.DataGenerationType getGenerationType();
-    GenerationConfig.CombinationStrategyType getCombinationType();
-    GenerationConfig.TreeWalkerType getWalkerType();
-}
--- generator/src/main/java/com/scottlogic/deg/generator/Guice/GenerationConfigProvider.java
+++ generator/src/main/java/com/scottlogic/deg/generator/Guice/GenerationConfigProvider.java
@@ -2,23 +2,22 @@ package com.scottlogic.deg.generator.Guice;
 
 import com.google.inject.Inject;
 import com.google.inject.Provider;
-import com.scottlogic.deg.generator.CommandLine.CanGenerate;
 import com.scottlogic.deg.generator.generation.GenerationConfig;
+import com.scottlogic.deg.generator.generation.GenerationConfigSource;
 
 public class GenerationConfigProvider implements Provider<GenerationConfig> {
 
-    private CanGenerate commandLine;
+    private GenerationConfigSource commandLine;
 
     @Inject
-    public GenerationConfigProvider(CanGenerate commandLine) {
+    public GenerationConfigProvider(GenerationConfigSource commandLine) {
         this.commandLine = commandLine;
     }
 
     @Override
     public GenerationConfig get() {
-        return new GenerationConfig(
-            commandLine.getGenerationType(),
-            commandLine.getWalkerType(),
-            commandLine.getCombinationType());
+
+        return new GenerationConfig(commandLine);
+
     }
 }
