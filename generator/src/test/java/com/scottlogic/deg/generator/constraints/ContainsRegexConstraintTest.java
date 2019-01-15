daeec74c8d939d9482a2ab776079958824c64b55
Simon Laing <slaing@scottlogic.com>
2019-01-15 11:31:27 +0000

    Update tests to use consistent Test attribute

--- generator/src/test/java/com/scottlogic/deg/generator/constraints/ContainsRegexConstraintTest.java
+++ generator/src/test/java/com/scottlogic/deg/generator/constraints/ContainsRegexConstraintTest.java
@@ -5,7 +5,7 @@ import com.scottlogic.deg.generator.constraints.atomic.ContainsRegexConstraint;
 import com.scottlogic.deg.generator.inputs.RuleInformation;
 import com.scottlogic.deg.schemas.v3.RuleDTO;
 import org.junit.Assert;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import java.util.Collections;
 import java.util.Set;
