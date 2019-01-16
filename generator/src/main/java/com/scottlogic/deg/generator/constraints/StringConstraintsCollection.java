85438040f42cce1e7566e2df2122c824b79b95d5
Simon Laing <slaing@scottlogic.com>
2019-01-16 15:06:25 +0000

    Update to handle length contradictions correctly
    i.e. not(ofLength 6) is not contradictory with ofLength 7
    and not(ofLength 7) is not contradictory with ofLength 6

--- generator/src/main/java/com/scottlogic/deg/generator/constraints/StringConstraintsCollection.java
+++ generator/src/main/java/com/scottlogic/deg/generator/constraints/StringConstraintsCollection.java
@@ -1,8 +1,11 @@
 package com.scottlogic.deg.generator.constraints;
 
+import com.scottlogic.deg.generator.Field;
 import com.scottlogic.deg.generator.constraints.atomic.*;
+import com.scottlogic.deg.generator.inputs.RuleInformation;
 
 import java.util.Collections;
+import java.util.Objects;
 import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -81,6 +84,10 @@ public class StringConstraintsCollection {
                 return areLengthConstraintsContradictory((StringHasLengthConstraint)constraint, (StringHasLengthConstraint)otherConstraint);
             }
 
+            if (constraint instanceof StringHasDifferentLengthConstraint) {
+                return areLengthConstraintsContradictory((StringHasDifferentLengthConstraint)constraint, (StringHasDifferentLengthConstraint)otherConstraint);
+            }
+
             return false; //same type constraints cannot contradict
         }
 
@@ -96,7 +103,17 @@ public class StringConstraintsCollection {
             return isContradictoryToOfLengthConstraint(((StringHasLengthConstraint) constraint).referenceValue, otherConstraint);
         }
 
+        if (constraint instanceof StringHasDifferentLengthConstraint) {
+            return isContradictoryToDifferentLengthConstraint(((StringHasDifferentLengthConstraint) constraint).getLength(), otherConstraint);
+        }
+
         return false; //we cannot prove it is contradictory
+    }
+
+    private boolean areLengthConstraintsContradictory(
+        StringHasDifferentLengthConstraint constraint,
+        StringHasDifferentLengthConstraint otherConstraint) {
+        return constraint.getLength() != otherConstraint.getLength(); //TODO: should this be == or !=?
     }
 
     private boolean areLengthConstraintsContradictory(
@@ -146,6 +163,20 @@ public class StringConstraintsCollection {
             return false; //technically non-contradictory
         }
 
+        if (otherConstraint instanceof StringHasDifferentLengthConstraint){
+            int notOfLength = ((StringHasDifferentLengthConstraint) otherConstraint).getLength();
+            return notOfLength == requiredLength;
+        }
+
+        return false;
+    }
+
+    private boolean isContradictoryToDifferentLengthConstraint(int notOfLength, AtomicConstraint otherConstraint) {
+        if (otherConstraint instanceof StringHasLengthConstraint){
+            int requiredLength = ((StringHasLengthConstraint) otherConstraint).referenceValue;
+            return notOfLength == requiredLength;
+        }
+
         return false;
     }
 
@@ -169,14 +200,62 @@ public class StringConstraintsCollection {
         if (negatedConstraint instanceof StringHasLengthConstraint){
             StringHasLengthConstraint ofLength = (StringHasLengthConstraint) negatedConstraint;
 
-            //can use either longerThan <length> or shorterThan <length>
-            return new IsStringShorterThanConstraint(
-                ofLength.field,
-                ofLength.referenceValue,
-                ofLength.getRules()
-            );
+            return new StringHasDifferentLengthConstraint(ofLength);
         }
 
         return null;
+    }
+
+    private class StringHasDifferentLengthConstraint implements AtomicConstraint {
+        final StringHasLengthConstraint underlyingConstraint;
+
+        public StringHasDifferentLengthConstraint(StringHasLengthConstraint underlyingConstraint) {
+            this.underlyingConstraint = underlyingConstraint;
+        }
+
+        @Override
+        public String toDotLabel() {
+            return this.underlyingConstraint.toDotLabel();
+        }
+
+        @Override
+        public Field getField() {
+            return this.underlyingConstraint.getField();
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (!(o instanceof StringHasDifferentLengthConstraint)) {
+                return false;
+            }
+
+            StringHasDifferentLengthConstraint other = (StringHasDifferentLengthConstraint) o;
+            return this.equals(other);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(this.underlyingConstraint, StringHasDifferentLengthConstraint.class);
+        }
+
+        @Override
+        public String toString() {
+            return String.format("`%s` != %d", this.underlyingConstraint.getField().name, this.underlyingConstraint.referenceValue);
+        }
+
+        @Override
+        public Set<RuleInformation> getRules() {
+            return this.underlyingConstraint.getRules();
+        }
+
+        @Override
+        public AtomicConstraint withRules(Set<RuleInformation> rules) {
+            return new StringHasDifferentLengthConstraint(
+                (StringHasLengthConstraint) this.underlyingConstraint.withRules(rules));
+        }
+
+        public int getLength(){
+            return this.underlyingConstraint.referenceValue;
+        }
     }
 }
