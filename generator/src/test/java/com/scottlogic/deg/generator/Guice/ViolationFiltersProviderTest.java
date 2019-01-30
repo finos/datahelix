fc5d9384951b839f626601d6b8e534e5bfea0d74
Paul Daulby <pdaulby@scottlogic.com>
2019-01-30 13:32:15 +0000

    make constraintType field private, and acces via reflection in test

--- generator/src/test/java/com/scottlogic/deg/generator/Guice/ViolationFiltersProviderTest.java
+++ generator/src/test/java/com/scottlogic/deg/generator/Guice/ViolationFiltersProviderTest.java
@@ -7,6 +7,7 @@ import com.scottlogic.deg.generator.violations.filters.ViolationFilter;
 import com.scottlogic.deg.schemas.v3.AtomicConstraintType;
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Field;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
@@ -39,7 +40,7 @@ class ViolationFiltersProviderTest {
     }
 
     @Test
-    void hasLengthConstraintsToViolate_ReturnListWithOneHasLengthFilter() {
+    void hasLengthConstraintsToViolate_ReturnListWithOneHasLengthFilter() throws ClassNotFoundException, NoSuchFieldException, IllegalAccessException {
         GenerationConfigSource configSource = mock(GenerationConfigSource.class);
         when(configSource.getConstraintsToNotViolate()).thenReturn(Arrays.asList(AtomicConstraintType.HASLENGTH));
         ViolationFiltersProvider provider = new ViolationFiltersProvider(configSource, new AtomicConstraintTypeMapper());
@@ -47,8 +48,13 @@ class ViolationFiltersProviderTest {
         List<ViolationFilter> filters = provider.get();
         assertThat(filters, hasSize(1));
         assertThat(filters.get(0), instanceOf(ConstraintTypeViolationFilter.class));
+
+        Class reflection = Class.forName("com.scottlogic.deg.generator.violations.filters.ConstraintTypeViolationFilter");
+        Field field = reflection.getDeclaredField("constraintType");
+        field.setAccessible(true);
+
         ConstraintTypeViolationFilter filter = (ConstraintTypeViolationFilter) filters.get(0);
-        assertThat(filter.constraintType, equalTo(StringHasLengthConstraint.class));
+        assertThat(field.get(filter), equalTo(StringHasLengthConstraint.class));
     }
 
     @Test
