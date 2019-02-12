9a2d89acf6584971dd470683b277953ce5973f63
Andy Aspell-Clark <aaspellc@scottlogic.co.uk>
2019-02-12 10:53:07 +0000

    issue #566 - test for overwriting existing file - generation - non violating

--- generator/src/test/java/com/scottlogic/deg/generator/GenerateExecuteTests.java
+++ generator/src/test/java/com/scottlogic/deg/generator/GenerateExecuteTests.java
@@ -8,6 +8,10 @@ import com.scottlogic.deg.generator.generation.GenerationConfigSource;
 import com.scottlogic.deg.generator.inputs.InvalidProfileException;
 import com.scottlogic.deg.generator.inputs.JsonProfileReader;
 import com.scottlogic.deg.generator.outputs.targets.FileOutputTarget;
+import com.scottlogic.deg.generator.outputs.targets.OutputTarget;
+import com.scottlogic.deg.generator.validators.ErrorReporter;
+import com.scottlogic.deg.generator.validators.GenerationConfigValidator;
+import com.scottlogic.deg.generator.validators.ValidationResult;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
@@ -18,6 +22,7 @@ import static org.mockito.Mockito.*;
 
 public class GenerateExecuteTests {
 
+    private OutputTarget fileOutputTarget= mock(OutputTarget.class);
     private GenerationConfig config = mock(GenerationConfig.class);
     private JsonProfileReader profileReader = mock(JsonProfileReader.class);
     private StandardGenerationEngine generationEngine = mock(StandardGenerationEngine.class);
@@ -33,7 +38,7 @@ public class GenerateExecuteTests {
     @Test
     public void invalidConfigCallsCorrectMethods() throws IOException, InvalidProfileException {
         //Arrange
-        when(validator.validateCommandLine(config)).thenReturn(validationResult);
+        when(validator.validateCommandLine(config, outputTarget)).thenReturn(validationResult);
         when(validationResult.isValid()).thenReturn(false);
 
         //Act
@@ -48,7 +53,7 @@ public class GenerateExecuteTests {
     public void validConfigCallsCorrectMethods() throws IOException, InvalidProfileException {
         //Arrange
         File testFile = new File("TestFile");
-        when(validator.validateCommandLine(config)).thenReturn(validationResult);
+        when(validator.validateCommandLine(config, outputTarget)).thenReturn(validationResult);
         when(configSource.getProfileFile()).thenReturn(testFile);
         when(validationResult.isValid()).thenReturn(true);
 
