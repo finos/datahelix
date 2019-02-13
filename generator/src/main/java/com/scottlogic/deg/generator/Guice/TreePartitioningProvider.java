4c9e347f26a1d278eac71461318126e54bcda7b6
Simon Laing <slaing@scottlogic.com>
2019-02-13 14:07:41 +0000

    Prevent partitioning when generating RANDOM data

--- generator/src/main/java/com/scottlogic/deg/generator/Guice/TreePartitioningProvider.java
+++ generator/src/main/java/com/scottlogic/deg/generator/Guice/TreePartitioningProvider.java
@@ -5,6 +5,7 @@ import com.google.inject.Provider;
 import com.scottlogic.deg.generator.decisiontree.tree_partitioning.NoopTreePartitioner;
 import com.scottlogic.deg.generator.decisiontree.tree_partitioning.RelatedFieldTreePartitioner;
 import com.scottlogic.deg.generator.decisiontree.tree_partitioning.TreePartitioner;
+import com.scottlogic.deg.generator.generation.GenerationConfig;
 import com.scottlogic.deg.generator.generation.GenerationConfigSource;
 
 public class TreePartitioningProvider implements Provider<TreePartitioner> {
@@ -17,7 +18,10 @@ public class TreePartitioningProvider implements Provider<TreePartitioner> {
 
     @Override
     public TreePartitioner get() {
-        if (configSource.shouldDoPartitioning()){
+        boolean isRandom = configSource.getGenerationType() == GenerationConfig.DataGenerationType.RANDOM;
+        if (configSource.shouldDoPartitioning() && !isRandom){
+            //if we partition with random each partition will have random values, but will be static in relation to
+            //other partitions, therefore it looks like certain fields do not vary randomly
             return new RelatedFieldTreePartitioner();
         }
         return new NoopTreePartitioner();
