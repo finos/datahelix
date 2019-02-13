1a291a594b0a52ce5f292095f4494e4b9205b180
Andy Aspell-Clark <aaspellc@scottlogic.co.uk>
2019-02-13 14:31:04 +0000

    adding --overwrite to visualise command

--- generator/src/main/java/com/scottlogic/deg/generator/outputs/targets/FileOutputTarget.java
+++ generator/src/main/java/com/scottlogic/deg/generator/outputs/targets/FileOutputTarget.java
@@ -4,16 +4,16 @@ import com.google.inject.Inject;
 import com.google.inject.name.Named;
 import com.scottlogic.deg.generator.ProfileFields;
 import com.scottlogic.deg.generator.outputs.GeneratedObject;
-import com.scottlogic.deg.generator.outputs.TestCaseGenerationResult;
 import com.scottlogic.deg.generator.outputs.dataset_writers.DataSetWriter;
 
 import java.io.Closeable;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.stream.Stream;
 
-public class FileOutputTarget implements OutputTarget{
+public class FileOutputTarget implements OutputTarget {
     private final Path filePath;
     private final DataSetWriter dataSetWriter;
 
@@ -48,9 +48,17 @@ public class FileOutputTarget implements OutputTarget{
         }
     }
 
-    public FileOutputTarget withFilename(String filename){
+    public FileOutputTarget withFilename(String filename) {
         return new FileOutputTarget(
             filePath.resolve(dataSetWriter.getFileName(filename)),
             dataSetWriter);
+    }
+
+    public boolean exists() {
+        return Files.exists(filePath);
+    }
+
+    public boolean isDirectory() {
+        return Files.isDirectory(filePath);
     }
 }
