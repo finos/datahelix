31acb5e693b8d43deabe57c5060ad4960e828664
Mark <>
2019-02-15 18:59:09 +0000

    Make cucumber framework allow datetime offsets

--- generator/src/test/java/com/scottlogic/deg/generator/cucumber/steps/DateValueStep.java
+++ generator/src/test/java/com/scottlogic/deg/generator/cucumber/steps/DateValueStep.java
@@ -4,8 +4,9 @@ import com.scottlogic.deg.generator.cucumber.utils.CucumberTestState;
 import cucumber.api.java.en.When;
 
 public class DateValueStep {
-
-    public static final String DATE_REGEX = "((\\d{4})-(\\d{2})-(\\d{2}T(\\d{2}:\\d{2}:\\d{2}\\.\\d{3})))$";
+    // matches ISO-8601; could be more specific.
+    // we use a plain .* at the end of the time bit because if the earlier stuff is matched then we don't really need to be stringent about the rest of it
+    public static final String DATE_REGEX = "((\\d{4})-(\\d{2})-(\\d{2}T(\\d{2}:\\d{2}:\\d{2}\\.\\d{3}.*)))$";
     private final CucumberTestState state;
 
     public DateValueStep(CucumberTestState state){
