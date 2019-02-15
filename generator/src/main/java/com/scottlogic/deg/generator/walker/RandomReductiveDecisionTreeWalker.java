25b281824b95717d11743e7f9a8ce5e0d02504fb
Simon Laing <slaing@scottlogic.com>
2019-02-15 13:15:41 +0000

    Refactor from review

--- generator/src/main/java/com/scottlogic/deg/generator/walker/RandomReductiveDecisionTreeWalker.java
+++ generator/src/main/java/com/scottlogic/deg/generator/walker/RandomReductiveDecisionTreeWalker.java
@@ -1,41 +1,38 @@
 package com.scottlogic.deg.generator.walker;
 
 import com.google.inject.Inject;
-import com.scottlogic.deg.generator.FlatMappingSpliterator;
 import com.scottlogic.deg.generator.decisiontree.DecisionTree;
 import com.scottlogic.deg.generator.fieldspecs.RowSpec;
 
-import java.util.Iterator;
 import java.util.Optional;
-import java.util.Spliterator;
-import java.util.Spliterators;
-import java.util.function.Supplier;
 import java.util.stream.Stream;
-import java.util.stream.StreamSupport;
 
 public class RandomReductiveDecisionTreeWalker implements DecisionTreeWalker {
     private final ReductiveDecisionTreeWalker underlyingWalker;
 
     @Inject
-    public RandomReductiveDecisionTreeWalker(ReductiveDecisionTreeWalker underlyingWalker) {
+    RandomReductiveDecisionTreeWalker(ReductiveDecisionTreeWalker underlyingWalker) {
         this.underlyingWalker = underlyingWalker;
     }
 
     @Override
     public Stream<RowSpec> walk(DecisionTree tree) {
-        Supplier<Stream<RowSpec>> rowGenerator = () -> underlyingWalker.walk(tree);
-        Optional<RowSpec> firstRowSpec = rowGenerator.get().findFirst();
-        if (!firstRowSpec.isPresent()) {
+        Optional<RowSpec> firstRowSpecOpt = getFirstRowSpecFromRandomisingIteration(tree);
+        //noinspection OptionalIsPresent
+        if (!firstRowSpecOpt.isPresent()) {
             return Stream.empty();
         }
 
-        Stream<Stream<RowSpec>> infiniteRandomisingIterations = Stream.concat(
-            Stream.of(Stream.of(firstRowSpec.get())),
-            Stream.generate(rowGenerator));
+        return Stream.concat(
+            Stream.of(firstRowSpecOpt.get()),
+            Stream.generate(() ->
+                getFirstRowSpecFromRandomisingIteration(tree))
+                    .filter(Optional::isPresent)
+                    .map(Optional::get));
+    }
 
-        return FlatMappingSpliterator.flatMap(
-            infiniteRandomisingIterations,
-            streamOfRandomData -> streamOfRandomData.limit(1)
-        );
+    private Optional<RowSpec> getFirstRowSpecFromRandomisingIteration(DecisionTree tree){
+        return underlyingWalker.walk(tree)
+            .findFirst();
     }
 }
