5995a9ed09e6175cf6a76749ac258386c9122e9d
Andy Aspell-Clark <aaspellc@scottlogic.co.uk>
2019-03-15 10:49:41 +0000

    first test case profile json files
    added anyof and allof
    
    added some more error conditions
    
    fixed some regex's
    
    added checking temporal values (After, before etc.)
    
    moved test profiles into `test/resources` subdirectory
    
    added more test files
    
    fixed `if` constraint
    
    start documenting test files
    
    added some constraint nesting tests
    
    changed rules to allow an empty array
    
    updated test profile documentation
    added empty fields array profile
    
    added descriptions for more test files
    updated nesting test files to include allof
    
    add instructions on using the library to validate a profile
    
    updated README
    fixed regex error json file
    
    removed inset errors file
    
    added Medeia JSON schema validator
    
    fixed regex unit test and profile file
    
    fixed json structure

---
+++ schemas/src/test/java/com/scottlogic/deg/schemas/v0_1/ProfileValidatorTest.java
@@ -0,0 +1,161 @@
+package com.scottlogic.deg.schemas.v0_1;
+
+import com.scottlogic.deg.schemas.common.ValidationResult;
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+import java.io.InputStream;
+
+class ProfileValidatorTest {
+
+    private ProfileValidator profileValidator = new ProfileValidatorMedia();
+
+    private ValidationResult validate(String profile) {
+        InputStream testProfile = this.getClass().getResourceAsStream(profile);
+        return profileValidator.validateProfile(testProfile);
+    }
+
+    @Test
+    void validateProfile_notExists_fails() {
+        ValidationResult result = validate("non-existing-file");
+        Assert.assertFalse("Profile should not be valid", result.isValid());
+    }
+
+    @Test
+    void validateProfile_empty_fails() {
+        ValidationResult result = validate("/test-profiles/profile-test-empty.json");
+        Assert.assertFalse("Profile should not be valid", result.isValid());
+    }
+
+    @Test
+    void validateProfile_fields_array_empty_fails() {
+        ValidationResult result = validate("/test-profiles/profile-test-fields-array-empty.json");
+        Assert.assertFalse("Profile should not be valid", result.isValid());
+    }
+
+    @Test
+    void validateProfile_missing_constraint_fails() {
+        ValidationResult result = validate("/test-profiles/profile-test-missing-constraint.json");
+        Assert.assertFalse("Profile should not be valid", result.isValid());
+    }
+
+    @Test
+    void validateProfile_no_constraints_success() {
+        ValidationResult result = validate("/test-profiles/profile-test-no-constraints.json");
+        Assert.assertTrue("Profile should be valid", result.isValid());
+    }
+
+    @Test
+    void validateProfile_no_fields_fails() {
+        ValidationResult result = validate("/test-profiles/profile-test-no-fields.json");
+        Assert.assertFalse("Profile should not be valid", result.isValid());
+    }
+
+    @Test
+    void validateProfile_no_rules_fails() {
+        ValidationResult result = validate("/test-profiles/profile-test-no-rules.json");
+        Assert.assertFalse("Profile should not be valid", result.isValid());
+    }
+
+    @Test
+    void validateProfile_no_version_fails() {
+        ValidationResult result = validate("/test-profiles/profile-test-no-version.json");
+        Assert.assertFalse("Profile should not be valid", result.isValid());
+    }
+
+    @Test
+    void validateProfile_rule_array_empty_success() {
+        ValidationResult result = validate("/test-profiles/profile-test-rule-array-empty.json");
+        Assert.assertTrue("Profile should be valid", result.isValid());
+    }
+
+    @Test
+    void validateProfile_anyof_allof_success() {
+        ValidationResult result = validate("/test-profiles/profile-test-simple-anyof-allof.json");
+        Assert.assertTrue("Profile should be valid", result.isValid());
+    }
+
+    @Test
+    void validateProfile_anyof_allof_fails() {
+        ValidationResult result = validate("/test-profiles/profile-test-simple-anyof-allof-errors.json");
+        Assert.assertFalse("Profile should not be valid", result.isValid());
+    }
+
+    @Test
+    void validateProfile_data_constraints_success() {
+        ValidationResult result = validate("/test-profiles/profile-test-simple-data-constraints.json");
+        Assert.assertTrue("Profile should be valid", result.isValid());
+    }
+
+    @Test
+    void validateProfile_data_constraints_fails() {
+        ValidationResult result = validate("/test-profiles/profile-test-simple-data-constraints-errors.json");
+        Assert.assertFalse("Profile should not be valid", result.isValid());
+    }
+
+    @Test
+    void validateProfile_data_constraints_nested_1level_success() {
+        ValidationResult result = validate("/test-profiles/profile-test-simple-data-constraints-nested-1level.json");
+        Assert.assertTrue("Profile should be valid", result.isValid());
+    }
+
+    @Test
+    void validateProfile_data_constraints_nested_2level_success() {
+        ValidationResult result = validate("/test-profiles/profile-test-simple-data-constraints-nested-2level.json");
+        Assert.assertTrue("Profile should be valid", result.isValid());
+    }
+
+    @Test
+    void validateProfile_data_constraints_nested_7level_success() {
+        ValidationResult result = validate("/test-profiles/profile-test-simple-data-constraints-nested-7level.json");
+        Assert.assertTrue("Profile should be valid", result.isValid());
+    }
+
+    @Test
+    void validateProfile_if_success() {
+        ValidationResult result = validate("/test-profiles/profile-test-simple-if.json");
+        Assert.assertTrue("Profile should be valid", result.isValid());
+    }
+
+    @Test
+    void validateProfile_if_fails() {
+        ValidationResult result = validate("/test-profiles/profile-test-simple-if-errors.json");
+        Assert.assertFalse("Profile should not be valid", result.isValid());
+    }
+
+    @Test
+    void validateProfile_inset_success() {
+        ValidationResult result = validate("/test-profiles/profile-test-simple-inset.json");
+        Assert.assertTrue("Profile should be valid", result.isValid());
+    }
+
+    @Test
+    void validateProfile_not_success() {
+        ValidationResult result = validate("/test-profiles/profile-test-simple-not.json");
+        Assert.assertTrue("Profile should be valid", result.isValid());
+    }
+
+    @Test
+    void validateProfile_regex_success() {
+        ValidationResult result = validate("/test-profiles/profile-test-simple-regex.json");
+        Assert.assertTrue("Profile should be valid", result.isValid());
+    }
+
+    @Test
+    void validateProfile_regex_fails() {
+        ValidationResult result = validate("/test-profiles/profile-test-simple-regex-errors.json");
+        Assert.assertFalse("Profile should not be valid", result.isValid());
+    }
+
+    @Test
+    void validateProfile_temporal_success() {
+        ValidationResult result = validate("/test-profiles/profile-test-simple-temporals.json");
+        Assert.assertTrue("Profile should be valid", result.isValid());
+    }
+
+    @Test
+    void validateProfile_temporal_fails() {
+        ValidationResult result = validate("/test-profiles/profile-test-simple-temporals-errors.json");
+        Assert.assertFalse("Profile should not be valid", result.isValid());
+    }
+}
