1407849fe9b1ad4246029f11be89b7cccdecb27d
Andy Aspell-Clark <aaspellc@scottlogic.co.uk>
2019-03-20 12:10:11 +0000

    pluralised class name

--- schemas/src/test/java/com/scottlogic/deg/schemas/v0_1/ProfileValidatorTests.java
+++ schemas/src/test/java/com/scottlogic/deg/schemas/v0_1/ProfileValidatorTests.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 
 import java.io.InputStream;
 
-class ProfileValidatorTest {
+class ProfileValidatorTests {
 
     private ProfileValidator profileValidator = new ProfileValidatorMedia();
 
