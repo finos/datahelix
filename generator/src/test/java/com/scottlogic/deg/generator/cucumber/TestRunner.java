ec04f13d5af7ce6f1b81009e183bc9ebaa63c666
Mark <>
2019-03-22 14:27:22 +0000

    Rearrange cucumber tests into subdirectories

--- generator/src/test/java/com/scottlogic/deg/generator/cucumber/TestRunner.java
+++ generator/src/test/java/com/scottlogic/deg/generator/cucumber/TestRunner.java
@@ -2,13 +2,13 @@ package com.scottlogic.deg.generator.cucumber;
 
 import org.junit.runner.RunWith;
 import cucumber.api.CucumberOptions;
-import com.scottlogic.deg.generator.cucumber.utils.GeneratorCucumber;
+import com.scottlogic.deg.generator.cucumber.engine.utils.GeneratorCucumber;
 
 @RunWith(GeneratorCucumber.class)
 @CucumberOptions(
     plugin = {"pretty"},
     features = {"src/test/java/com/scottlogic/deg/generator/cucumber"},
-    glue={"com.scottlogic.deg.generator.cucumber.steps"},
+    glue={"com.scottlogic.deg.generator.cucumber.engine.steps"},
     monochrome = true,
     tags = "not @ignore"
 )
