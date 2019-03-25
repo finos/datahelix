ec6bf1a1dac095d4388e3db0b5db2ea6660162f2
Simon Laing <slaing@scottlogic.com>
2019-03-25 11:59:14 +0000

    TBC: changes to reintroduce tracing-output

--- generator/src/main/java/com/scottlogic/deg/generator/walker/reductive/fieldselectionstrategy/FieldValue.java
+++ generator/src/main/java/com/scottlogic/deg/generator/walker/reductive/fieldselectionstrategy/FieldValue.java
@@ -1,20 +1,34 @@
 package com.scottlogic.deg.generator.walker.reductive.fieldselectionstrategy;
 
 import com.scottlogic.deg.generator.Field;
+import com.scottlogic.deg.generator.fieldspecs.FieldSpec;
+import com.scottlogic.deg.generator.fieldspecs.FieldSpecSource;
 import com.scottlogic.deg.generator.restrictions.FormatRestrictions;
 
 public class FieldValue {
     private final Field field;
     private final Object value;
+    private final FieldSpecSource valueSource;
     private final FormatRestrictions formatRestrictions;
+    private final FieldSpecSource formatSource;
 
     public FieldValue(Field field, Object value){
-        this(field, value, null);
+        this(field, value, (FormatRestrictions) null);
     }
     public FieldValue(Field field, Object value, FormatRestrictions formatRestrictions){
         this.value = value;
         this.field = field;
         this.formatRestrictions = formatRestrictions;
+        this.valueSource = null;
+        this.formatSource = null;
+    }
+
+    public FieldValue(Field field, Object value, FieldSpec valueSource){
+        this.value = value;
+        this.field = field;
+        this.valueSource = valueSource.getFieldSpecSource();
+        this.formatRestrictions = valueSource.getFormatRestrictions();
+        this.formatSource = valueSource.getFieldSpecSource();
     }
 
     public Object getValue() {
@@ -33,5 +47,9 @@ public class FieldValue {
 
     public FormatRestrictions getFormatRestrictions() {
         return formatRestrictions;
+    }
+
+    public FieldSpecSource getFieldSpecSource() {
+        return valueSource;
     }
 }
