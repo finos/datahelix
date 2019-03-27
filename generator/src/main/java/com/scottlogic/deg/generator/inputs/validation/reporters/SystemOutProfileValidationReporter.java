7c735175e24113831412f179caff7ff9b016e425
Simon Laing <slaing@scottlogic.com>
2019-03-27 07:41:56 +0000

    Remove final error message, not relevant given other changes

--- generator/src/main/java/com/scottlogic/deg/generator/inputs/validation/reporters/SystemOutProfileValidationReporter.java
+++ generator/src/main/java/com/scottlogic/deg/generator/inputs/validation/reporters/SystemOutProfileValidationReporter.java
@@ -8,12 +8,7 @@ import java.util.Collection;
 public class SystemOutProfileValidationReporter implements ProfileValidationReporter {
     @Override
     public void output(Collection<ValidationAlert> alerts) {
-        boolean hasErrors = false;
         for (ValidationAlert alert : alerts) {
-            if (alert.getCriticality().equals(Criticality.ERROR)) {
-                hasErrors = true;
-            }
-
             if (alert.getField() != null) {
                 System.out.println(String.format("Field %s: %s during %s Validation: %s",
                     alert.getField().toString(),
@@ -26,10 +21,6 @@ public class SystemOutProfileValidationReporter implements ProfileValidationRepo
                     alert.getValidationType().toString(),
                     alert.getMessage().getVerboseMessage()));
             }
-        }
-
-        if (hasErrors) {
-            System.out.println("Encountered unrecoverable profile validation errors.");
         }
     }
 }
