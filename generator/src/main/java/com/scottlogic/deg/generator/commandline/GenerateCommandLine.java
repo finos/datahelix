246a8a64c09a12e4547d5b4f0b195477444f8939
Simon Laing <slaing@scottlogic.com>
2019-03-29 07:59:00 +0000

    Allow fields to be untyped via a CLI switch

--- generator/src/main/java/com/scottlogic/deg/generator/commandline/GenerateCommandLine.java
+++ generator/src/main/java/com/scottlogic/deg/generator/commandline/GenerateCommandLine.java
@@ -108,6 +108,12 @@ public class GenerateCommandLine extends CommandLineBase implements GenerationCo
         defaultValue = GenerationConfig.Constants.OutputFormats.DEFAULT)
     private GenerationConfig.OutputFormat outputFormat;
 
+    @CommandLine.Option(
+        names = {"--allow-untyped-fields"},
+        description = "Remove the need for each field to have at least one compliant typing constraint applied",
+        defaultValue = GenerationConfig.Constants.OutputFormats.DEFAULT)
+    private boolean allowUntypedFields = false;
+
     public boolean shouldDoPartitioning() {
         return !this.dontPartitionTrees;
     }
@@ -165,6 +171,11 @@ public class GenerateCommandLine extends CommandLineBase implements GenerationCo
     @Override
     public List<AtomicConstraintType> getConstraintsToNotViolate() {
         return constraintsToNotViolate;
+    }
+
+    @Override
+    public boolean requireFieldTyping(){
+        return !allowUntypedFields;
     }
 
     @Override
