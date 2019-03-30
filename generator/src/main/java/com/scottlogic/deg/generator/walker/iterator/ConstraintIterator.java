e6d20179fb8d7cb4fa1661578fd38466846e9cf8
Paul Daulby <pdaulby@scottlogic.com>
2019-03-30 08:32:58 +0000

    remove routes walker

--- generator/src/main/java/com/scottlogic/deg/generator/walker/iterator/ConstraintIterator.java
+++
@@ -1,9 +0,0 @@
-package com.scottlogic.deg.generator.walker.iterator;
-
-import com.scottlogic.deg.generator.walker.routes.RowSpecRoute;
-
-import java.util.Iterator;
-
-public interface ConstraintIterator extends Iterator<RowSpecRoute> {
-    void reset();
-}
