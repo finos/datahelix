06cf4e90f522e05e562bb3a847008e8cc47bb1c2
Matt Cline <mcline@scottlogic.com>
2019-04-01 18:47:16 +0100

    811 - Improve Justify schema error messages

--- schemas/src/main/java/com/scottlogic/deg/schemas/v0_1/ProfileSchemaValidatorLeadPony.java
+++ schemas/src/main/java/com/scottlogic/deg/schemas/v0_1/ProfileSchemaValidatorLeadPony.java
@@ -1,17 +1,20 @@
 package com.scottlogic.deg.schemas.v0_1;
 
 import com.scottlogic.deg.schemas.common.ValidationResult;
+import com.sun.xml.internal.ws.api.ha.StickyFeature;
 import org.leadpony.justify.api.JsonSchema;
 import org.leadpony.justify.api.JsonValidationService;
+import org.leadpony.justify.api.Problem;
 import org.leadpony.justify.api.ProblemHandler;
 
 import javax.json.stream.JsonParser;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.InputStream;
+import java.io.*;
+import java.nio.file.Files;
 import java.util.ArrayList;
+import java.util.BitSet;
+import java.util.Collection;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Used to validate a DataHelix Profile JSON file.
@@ -20,11 +23,14 @@ import java.util.List;
  */
 public class ProfileSchemaValidatorLeadPony implements ProfileSchemaValidator {
 
+    private List<String> profileJsonLines;
+
     @Override
     public ValidationResult validateProfile(File profileFile) {
         try {
+            profileJsonLines = Files.readAllLines(profileFile.toPath());
             return validateProfile(new FileInputStream(profileFile));
-        } catch (FileNotFoundException e) {
+        } catch (IOException e) {
             List<String> errMsgs = new ArrayList<>();
             errMsgs.add(e.getLocalizedMessage());
             return new ValidationResult(errMsgs);
@@ -58,7 +64,8 @@ public class ProfileSchemaValidatorLeadPony implements ProfileSchemaValidator {
             JsonSchema schema = service.readSchema(schemaStream);
 
             // Problem handler which will print problems found.
-            ProblemHandler handler = service.createProblemPrinter(s -> errorMessages.add(s));
+            List<Problem> problems = new ArrayList<>();
+            ProblemHandler handler = ProblemHandler.collectingTo(problems);
 
             // We have to step over the profile otherwise it is not checked against the schema.
             try (JsonParser parser = service.createParser(profileStream, schema, handler)) {
@@ -67,8 +74,31 @@ public class ProfileSchemaValidatorLeadPony implements ProfileSchemaValidator {
                     // Do something useful here
                 }
             }
+
+            //Add all of the problems as error messages
+            extractProblems(problems, errorMessages);
+        }
+
+        if(!errorMessages.isEmpty()) {
+            errorMessages.add(0, "Error(s) occurred during schema validation." +
+                "\nFor full details try opening the profile in a json schema-enabled IDE. See link");
         }
         return new ValidationResult(errorMessages);
     }
 
+    private void extractProblems(List<Problem> problems, List<String> outputList) {
+        for (Problem problem : problems) {
+            extractProblem(problem, outputList);
+        }
+    }
+
+    private void extractProblem(Problem problem, List<String> outputList) {
+        if (!problem.hasBranches()) {
+            String jsonSnippet = profileJsonLines.get((int)problem.getLocation().getLineNumber() -1).trim();
+            outputList.add("Problem found at line " + problem.getLocation().getLineNumber() + "\n... " + jsonSnippet + " ...\n"
+                + "Suggested fix:" + "\n- " + problem.getMessage() + "\n");
+            return;
+        }
+        extractProblems(problem.getBranch(0), outputList);
+    }
 }
