e53d9e8703deb8a6bd80cc19cdc01d95208002b8
Paul Daulby <pdaulby@scottlogic.com>
2019-04-02 11:23:35 +0100

    return DataBagValue instead of DataBag in FieldSpecValueGenerator

--- generator/src/main/java/com/scottlogic/deg/generator/generation/databags/StandardRowSpecDataBagSourceFactory.java
+++ generator/src/main/java/com/scottlogic/deg/generator/generation/databags/StandardRowSpecDataBagSourceFactory.java
@@ -1,11 +1,11 @@
 package com.scottlogic.deg.generator.generation.databags;
 
 import com.google.inject.Inject;
+import com.scottlogic.deg.generator.DataBagValue;
 import com.scottlogic.deg.generator.Field;
 import com.scottlogic.deg.generator.fieldspecs.FieldSpec;
 import com.scottlogic.deg.generator.fieldspecs.RowSpec;
 import com.scottlogic.deg.generator.generation.FieldSpecValueGenerator;
-import com.scottlogic.deg.generator.generation.GenerationConfig;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,29 +21,21 @@ public class StandardRowSpecDataBagSourceFactory implements RowSpecDataBagSource
     }
 
     public DataBagSource createDataBagSource(RowSpec rowSpec){
-
-        List<DataBagSource> fieldDataBagSources = new ArrayList<>(rowSpec.getFields().size());
+        List<Stream<GeneratedObject>> fieldDataBagSources = new ArrayList<>(rowSpec.getFields().size());
 
         for (Field field: rowSpec.getFields()) {
             FieldSpec fieldSpec = rowSpec.getSpecForField(field);
 
             fieldDataBagSources.add(
-                new StreamDataBagSource(generator.generate(field, fieldSpec)));
+                generator.generate(field, fieldSpec)
+                    .map(this::toGeneratedObject)
+            );
         }
 
-        return new MultiplexingDataBagSource(fieldDataBagSources.stream());
+        return new FieldCombiningDataBagSource(fieldDataBagSources);
     }
 
-    class StreamDataBagSource implements DataBagSource{
-        private final Stream<GeneratedObject> dataBags;
-
-        StreamDataBagSource(Stream<GeneratedObject> dataBags) {
-            this.dataBags = dataBags;
-        }
-
-        @Override
-        public Stream<GeneratedObject> generate(GenerationConfig generationConfig) {
-            return dataBags;
-        }
+    private GeneratedObject toGeneratedObject(DataBagValue dataBagValue) {
+        return GeneratedObject.startBuilding().set(dataBagValue.getField(), dataBagValue).build();
     }
 }
