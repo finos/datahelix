f87e531777e4f78d481a428adf9e6e0338acf502
Matt Cline <mcline@scottlogic.com>
2019-04-02 16:03:07 +0100

    811 - Group error messages by line number

--- schemas/src/main/java/com/scottlogic/deg/schemas/v0_1/ProfileSchemaValidatorLeadPony.java
+++ schemas/src/main/java/com/scottlogic/deg/schemas/v0_1/ProfileSchemaValidatorLeadPony.java
@@ -2,19 +2,20 @@ package com.scottlogic.deg.schemas.v0_1;
 
 import com.scottlogic.deg.schemas.common.ValidationResult;
 import com.sun.xml.internal.ws.api.ha.StickyFeature;
+import javafx.util.Pair;
 import org.leadpony.justify.api.JsonSchema;
 import org.leadpony.justify.api.JsonValidationService;
 import org.leadpony.justify.api.Problem;
 import org.leadpony.justify.api.ProblemHandler;
 
 import javax.json.stream.JsonParser;
+import javax.print.DocFlavor;
 import java.io.*;
 import java.nio.file.Files;
-import java.util.ArrayList;
-import java.util.BitSet;
-import java.util.Collection;
-import java.util.List;
+import java.nio.file.Path;
+import java.util.*;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * Used to validate a DataHelix Profile JSON file.
@@ -24,17 +25,31 @@ import java.util.stream.Collectors;
 public class ProfileSchemaValidatorLeadPony implements ProfileSchemaValidator {
 
     private List<String> profileJsonLines;
+    private Path profilePath;
 
     @Override
     public ValidationResult validateProfile(File profileFile) {
         try {
-            profileJsonLines = Files.readAllLines(profileFile.toPath());
-            return validateProfile(new FileInputStream(profileFile));
+            byte[] data = Files.readAllBytes(profilePath = profileFile.toPath());
+            profileJsonLines = readAllLines(data);
+            return validateProfile(new ByteArrayInputStream(data));
         } catch (IOException e) {
             List<String> errMsgs = new ArrayList<>();
             errMsgs.add(e.getLocalizedMessage());
             return new ValidationResult(errMsgs);
         }
+    }
+
+    private List<String> readAllLines(byte[] data) throws IOException {
+        final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(new ByteArrayInputStream(data)));
+        List<String> lines = new ArrayList<>();
+
+        String line;
+        while ((line = bufferedReader.readLine()) != null){
+            lines.add(line);
+        }
+
+        return lines;
     }
 
     /**
@@ -76,29 +91,56 @@ public class ProfileSchemaValidatorLeadPony implements ProfileSchemaValidator {
             }
 
             //Add all of the problems as error messages
-            extractProblems(problems, errorMessages);
+            if(!problems.isEmpty()) {
+                HashMap<Integer, String> problemDictionary = new HashMap<>();
+                extractProblems(problems, problemDictionary);
+                errorMessages.addAll(formatProblems(problemDictionary));
+            }
         }
 
         if(!errorMessages.isEmpty()) {
-            errorMessages.add(0, "Error(s) occurred during schema validation." +
-                "\nFor full details try opening the profile in a json schema-enabled IDE. See link");
+            errorMessages.add(0,
+                "Error(s) occurred during schema validation." +
+                "\nFile path: " + profilePath.toString() +
+                "\nFor full details try opening the profile in a json schema-enabled IDE." +
+                "\nSee https://github.com/ScottLogic/datahelix/blob/master/docs/ProfileDeveloperGuide.md#Microsoft-Visual-Studio-Code\n");
         }
         return new ValidationResult(errorMessages);
     }
 
-    private void extractProblems(List<Problem> problems, List<String> outputList) {
+    private void extractProblems(List<Problem> problems, HashMap<Integer, String> problemDictionary) {
         for (Problem problem : problems) {
-            extractProblem(problem, outputList);
+            extractProblem(problem, problemDictionary);
         }
     }
 
-    private void extractProblem(Problem problem, List<String> outputList) {
+    private void extractProblem(Problem problem, HashMap<Integer, String> problemDictionary) {
         if (!problem.hasBranches()) {
-            String jsonSnippet = profileJsonLines.get((int)problem.getLocation().getLineNumber() -1).trim();
-            outputList.add("Problem found at line " + problem.getLocation().getLineNumber() + "\n... " + jsonSnippet + " ...\n"
-                + "Suggested fix:" + "\n- " + problem.getMessage() + "\n");
+            int lineNumber = (int)problem.getLocation().getLineNumber();
+            String formattedMessage = "- " + problem.getMessage() + "\n";
+            problemDictionary.merge(lineNumber, formattedMessage, String::concat);
+
             return;
         }
-        extractProblems(problem.getBranch(0), outputList);
+        extractProblems(problem.getBranch(0), problemDictionary);
+    }
+
+    private List<String> formatProblems(HashMap<Integer, String> problemDictionary) {
+        List<String> outputList = new ArrayList<>();
+        String messageFormat = "Problem found at line %d\n... %s ...\nSuggested fix:\n%s";
+
+        problemDictionary.forEach(
+            (lineNo, messages)
+                -> outputList.add(
+                String.format(
+                    messageFormat,
+                    lineNo,
+                    profileJsonLines.get(lineNo - 1).trim(),
+                    messages
+                )
+            )
+        );
+
+        return outputList;
     }
 }
