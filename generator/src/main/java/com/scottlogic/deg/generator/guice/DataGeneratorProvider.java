d63feebac7146dc2257bd69d5bd1f0909e9be43c
Paul Daulby <pdaulby@scottlogic.com>
2019-04-02 16:31:26 +0100

    decorate DataGenerator's in the DataGeneratorProvider

--- generator/src/main/java/com/scottlogic/deg/generator/guice/DataGeneratorProvider.java
+++ generator/src/main/java/com/scottlogic/deg/generator/guice/DataGeneratorProvider.java
@@ -5,35 +5,30 @@ import com.google.inject.Provider;
 import com.scottlogic.deg.generator.decisiontree.DecisionTreeOptimiser;
 import com.scottlogic.deg.generator.decisiontree.treepartitioning.TreePartitioner;
 import com.scottlogic.deg.generator.generation.*;
-import com.scottlogic.deg.generator.walker.RandomReductiveDecisionTreeWalker;
+import com.scottlogic.deg.generator.walker.RestartingDataGeneratorDecorator;
 import com.scottlogic.deg.generator.walker.ReductiveDataGenerator;
 
 public class DataGeneratorProvider implements Provider<DataGenerator> {
 
     private final WalkingDataGenerator walkingDataGenerator;
     private final ReductiveDataGenerator reductiveDataGenerator;
-    private final RandomReductiveDecisionTreeWalker randomReductiveDecisionTreeWalker;
+
     private final GenerationConfigSource configSource;
 
     private final TreePartitioner treePartitioner;
     private final DecisionTreeOptimiser optimiser;
-    private final ReductiveDataGeneratorMonitor monitor;
 
     @Inject
     public DataGeneratorProvider(WalkingDataGenerator walkingDataGenerator,
                                  ReductiveDataGenerator reductiveDataGenerator,
-                                 RandomReductiveDecisionTreeWalker randomReductiveDecisionTreeWalker,
                                  GenerationConfigSource configSource,
                                  TreePartitioner treePartitioner,
-                                 DecisionTreeOptimiser optimiser,
-                                 ReductiveDataGeneratorMonitor monitor){
+                                 DecisionTreeOptimiser optimiser){
         this.walkingDataGenerator = walkingDataGenerator;
         this.reductiveDataGenerator = reductiveDataGenerator;
-        this.randomReductiveDecisionTreeWalker = randomReductiveDecisionTreeWalker;
         this.configSource = configSource;
         this.treePartitioner = treePartitioner;
         this.optimiser = optimiser;
-        this.monitor = monitor;
     }
 
     @Override
@@ -45,15 +40,23 @@ public class DataGeneratorProvider implements Provider<DataGenerator> {
             ? reductiveDataGenerator
             : walkingDataGenerator;
 
-        if (isRandom && isReductive){
-            return randomReductiveDecisionTreeWalker;
+        if (configSource.shouldDoPartitioning()){
+            underlying = decorateWithPartitioning(underlying);
         }
 
-        if (configSource.shouldDoPartitioning() && !isRandom){
-            //if we partition with random each partition will have random values, but will be static in relation to
-            //other partitions, therefore it looks like certain fields do not vary randomly
-            return new PartitioningDataGeneratorDecorator(underlying, treePartitioner, optimiser, monitor);
+        if (isRandom && isReductive){
+            //restarting should be the outermost step if used with partitioning.
+            underlying = decorateWithRestarting(underlying);
         }
+
         return underlying;
+    }
+
+    private DataGenerator decorateWithPartitioning(DataGenerator underlying) {
+        return new PartitioningDataGeneratorDecorator(underlying, treePartitioner, optimiser);
+    }
+
+    private DataGenerator decorateWithRestarting(DataGenerator underlying) {
+        return new RestartingDataGeneratorDecorator(underlying);
     }
 }
