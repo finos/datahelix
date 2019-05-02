1768bb1f6bda1edc854aad578cf9dce88bd3d712
Declan Lucas <dlucas@scottlogic.com>
2019-05-02 09:10:53 +0100

    Add a comment to RadomDateIterator to explain dateetime granularity trimming

--- generator/src/main/java/com/scottlogic/deg/generator/generation/fieldvaluesources/datetime/RandomDateIterator.java
+++ generator/src/main/java/com/scottlogic/deg/generator/generation/fieldvaluesources/datetime/RandomDateIterator.java
@@ -42,6 +42,10 @@ class RandomDateIterator implements Iterator<OffsetDateTime> {
     }
 
     private OffsetDateTime trimUnwantedGranularity(OffsetDateTime dateToTrim, Timescale granularity) {
+
+        // Remove unneeded granularity from the dateToTrim.
+        // For example: if a granularity of days is passed in; all smaller units of time will be set to the lowest possible value.
+        // (hours, minutes, seconds and milliseconds will all be set to 0)
         return granularity.getGranularityFunction().apply(dateToTrim);
     }
 }
