15830f828e24c36eb1bdc7ae3779c2aa7321661b
Declan Lucas <dlucas@scottlogic.com>
2019-05-02 10:49:40 +0100

    add javadoc for nextDate in SequentialDateIterator

--- generator/src/main/java/com/scottlogic/deg/generator/generation/fieldvaluesources/datetime/SequentialDateIterator.java
+++ generator/src/main/java/com/scottlogic/deg/generator/generation/fieldvaluesources/datetime/SequentialDateIterator.java
@@ -1,24 +1,19 @@
 package com.scottlogic.deg.generator.generation.fieldvaluesources.datetime;
 
-import com.sun.scenario.effect.Offset;
-
 import java.time.OffsetDateTime;
-import java.time.temporal.ChronoUnit;
-import java.time.temporal.TemporalUnit;
 import java.util.Iterator;
-import java.util.stream.Stream;
 
 class SequentialDateIterator implements Iterator<OffsetDateTime> {
     private final OffsetDateTime maxDate;
-    private final Timescale unit;
+    private final Timescale granularityUnit;
 
     private OffsetDateTime current;
     private boolean hasNext;
 
     SequentialDateIterator(OffsetDateTime inclusiveMinDate, OffsetDateTime exclusiveMaxDate, Timescale granularity) {
         maxDate = exclusiveMaxDate;
-        unit = granularity;
-        current = roundUpToGranularity(inclusiveMinDate, granularity);
+        granularityUnit = granularity;
+        current = roundUpToGranularity(inclusiveMinDate);
         hasNext = current.compareTo(exclusiveMaxDate) < 0;
     }
 
@@ -27,13 +22,12 @@ class SequentialDateIterator implements Iterator<OffsetDateTime> {
      * This can be used to ensure a datetime is not lower than a previously specified minimum after granularity is applied.
      * 10:00 -> HOURS => 10:00
      * 10:01 -> HOURS => 11:00
-     * @param initial initial datetime which will have granularity applied
-     * @param unit unit of granularity
+     * @param initial initial datetime which will have granularity applied     *
      * @return datetime that has had granularity applied
      */
-    private OffsetDateTime roundUpToGranularity(final OffsetDateTime initial, final Timescale unit) {
-        OffsetDateTime earlierOrEqual = unit.getGranularityFunction().apply(initial);
-        return earlierOrEqual.equals(initial) ? earlierOrEqual : unit.getNext().apply(earlierOrEqual);
+    private OffsetDateTime roundUpToGranularity(final OffsetDateTime initial) {
+        OffsetDateTime earlierOrEqual = granularityUnit.getGranularityFunction().apply(initial);
+        return earlierOrEqual.equals(initial) ? earlierOrEqual : granularityUnit.getNext().apply(earlierOrEqual);
     }
 
     @Override
@@ -46,7 +40,7 @@ class SequentialDateIterator implements Iterator<OffsetDateTime> {
 
         OffsetDateTime next = current;
 
-        current = nextDate(current, unit);
+        current = nextDate(current);
         if (current.isAfter(maxDate) || current.isEqual(maxDate)) {
             hasNext = false;
         }
@@ -54,8 +48,15 @@ class SequentialDateIterator implements Iterator<OffsetDateTime> {
         return next;
     }
 
-    private OffsetDateTime nextDate(OffsetDateTime previous, Timescale granularity) {
-        return granularity.getNext().apply(previous);
+    /**
+     * Get the next available date at a given granularity.
+     * 24/07/1990 -> days = 25/07/1990
+     * 01/07/1990 -> months = 01/08/1990
+     * @param previous input date
+     * @return date after previous at given granularity
+     */
+    private OffsetDateTime nextDate(OffsetDateTime previous) {
+        return granularityUnit.getNext().apply(previous);
     }
 }
 
