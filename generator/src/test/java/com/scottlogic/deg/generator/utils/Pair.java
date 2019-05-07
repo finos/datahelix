a45c74ec24abb7a809562d1952739248a941adbf
Ryan Stuart <rstuart@scottlogic.com>
2019-05-07 14:12:46 +0100

    Clean up pair (equals, hashcode, toString)

--- generator/src/test/java/com/scottlogic/deg/generator/utils/Pair.java
+++ generator/src/test/java/com/scottlogic/deg/generator/utils/Pair.java
@@ -1,5 +1,7 @@
 package com.scottlogic.deg.generator.utils;
 
+import java.util.Objects;
+
 public class Pair<T1, T2> {
 
     private final T1 key;
@@ -19,4 +21,25 @@ public class Pair<T1, T2> {
         return value;
     }
 
+    @Override
+    public String toString() {
+        return "Pair{" +
+            "key=" + key +
+            ", value=" + value +
+            '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Pair<?, ?> pair = (Pair<?, ?>) o;
+        return Objects.equals(key, pair.key) &&
+            Objects.equals(value, pair.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(key, value);
+    }
 }
