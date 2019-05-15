f83c9d6e72cc65cd4029b1d76cafcdf1467c0cbd
Paul Daulby <pdaulby@scottlogic.com>
2019-05-15 16:53:36 +0100

    Move cucumber tests to orchestrator

---
+++ orchestrator/src/test/java/com/scottlogic/deg/orchestrator/cucumber/testframework/utils/CucumberGenerationMode.java
@@ -0,0 +1,9 @@
+package com.scottlogic.deg.orchestrator.cucumber.testframework.utils;
+
+/**
+ * Enum to represent the possible generation modes in cucumber tests.
+ */
+public enum CucumberGenerationMode {
+    VIOLATING,
+    VALIDATING
+}
