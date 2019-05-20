4a57f58817334938bb3a8fcc8381af7548bce4b7
willsalt-sl <wsalt@scottlogic.com>
2019-05-20 13:29:30 +0100

    #939 Refactor common CUSIP and SEDOL code.

---
+++ generator/src/main/java/com/scottlogic/deg/generator/generation/ChecksummedCodeStringGenerator.java
@@ -0,0 +1,128 @@
+package com.scottlogic.deg.generator.generation;
+
+import com.scottlogic.deg.generator.utils.*;
+
+import java.util.Arrays;
+import java.util.Objects;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public abstract class ChecksummedCodeStringGenerator implements StringGenerator {
+    protected final RegexStringGenerator sansCheckDigitGenerator;
+    protected final boolean negate;
+
+    public ChecksummedCodeStringGenerator(String generationPatternSansCheck) {
+        negate = false;
+        sansCheckDigitGenerator = new RegexStringGenerator(generationPatternSansCheck, true);
+    }
+
+    public ChecksummedCodeStringGenerator(RegexStringGenerator generator, boolean negate) {
+        this.negate = negate;
+        sansCheckDigitGenerator = generator;
+    }
+
+    public abstract char calculateCheckDigit(String str);
+
+    @Override
+    public StringGenerator intersect(StringGenerator stringGenerator) {
+        return new NoStringsStringGenerator(
+            RegexStringGenerator.intersectRepresentation(stringGenerator.toString(), "<checksummed string>")
+        );
+    }
+
+    @Override
+    public abstract StringGenerator complement();
+
+    @Override
+    public boolean isFinite() {
+        return true;
+    }
+
+    @Override
+    public long getValueCount() {
+        return sansCheckDigitGenerator.getValueCount();
+    }
+
+    @Override
+    public abstract boolean match(String subject);
+
+    @Override
+    public Iterable<String> generateInterestingValues() {
+        if (negate) {
+            return new ConcatenatingIterable<>(
+                sansCheckDigitGenerator.complement().generateInterestingValues(),
+                generateInvalidCheckDigitStrings(sansCheckDigitGenerator::generateInterestingValues));
+        }
+        return new ProjectingIterable<>(sansCheckDigitGenerator.generateInterestingValues(),
+            sansCheckDigit -> sansCheckDigit + calculateCheckDigit(sansCheckDigit));
+    }
+
+    @Override
+    public Iterable<String> generateAllValues() {
+        if (negate) {
+            return new ConcatenatingIterable<>(
+                generateAllInvalidRegexStrings(),
+                generateAllInvalidCheckDigitStrings());
+        }
+        return new ProjectingIterable<>(sansCheckDigitGenerator.generateAllValues(),
+            sansCheckDigit -> sansCheckDigit + calculateCheckDigit(sansCheckDigit));
+    }
+
+    @Override
+    public Iterable<String> generateRandomValues(RandomNumberGenerator randomNumberGenerator) {
+        if (negate) {
+            return new RandomMergingIterable<>(
+                Arrays.asList(
+                    generateRandomInvalidRegexStrings(randomNumberGenerator),
+                    generateRandomInvalidCheckDigitStrings(randomNumberGenerator)),
+                randomNumberGenerator);
+        }
+        return new ProjectingIterable<>(sansCheckDigitGenerator.generateRandomValues(randomNumberGenerator),
+            sansCheckDigit -> sansCheckDigit + calculateCheckDigit(sansCheckDigit));
+    }
+
+    private Iterable<String> generateAllInvalidRegexStrings() {
+        return sansCheckDigitGenerator.complement().generateAllValues();
+    }
+
+    private Iterable<String> generateRandomInvalidRegexStrings(RandomNumberGenerator randomNumberGenerator) {
+        return sansCheckDigitGenerator.complement().generateRandomValues(randomNumberGenerator);
+    }
+
+    private Iterable<String> generateAllInvalidCheckDigitStrings() {
+        return generateInvalidCheckDigitStrings(sansCheckDigitGenerator::generateAllValues);
+    }
+
+    private Iterable<String> generateRandomInvalidCheckDigitStrings(RandomNumberGenerator randomNumberGenerator) {
+        return generateInvalidCheckDigitStrings(
+            () -> sansCheckDigitGenerator.generateRandomValues(randomNumberGenerator));
+    }
+
+    private Iterable<String> generateInvalidCheckDigitStrings(Supplier<Iterable<String>> sansCheckDigitSupplier) {
+        return new FlatteningIterable<>(
+            sansCheckDigitSupplier.get(),
+            sansCheckDigit -> {
+                final char checkDigit = calculateCheckDigit(sansCheckDigit);
+                return IntStream.range(0, 10).boxed()
+                    .map(digit -> Character.forDigit(digit, 10))
+                    .filter(digit -> digit != checkDigit)
+                    .map(digit -> sansCheckDigit + digit)
+                    .collect(Collectors.toList());
+            });
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        ChecksummedCodeStringGenerator that = (ChecksummedCodeStringGenerator) o;
+        return negate == that.negate;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(negate, getClass());
+    }
+}
--- generator/src/main/java/com/scottlogic/deg/generator/generation/CusipStringGenerator.java
+++ generator/src/main/java/com/scottlogic/deg/generator/generation/CusipStringGenerator.java
@@ -2,144 +2,35 @@ package com.scottlogic.deg.generator.generation;
 
 import com.scottlogic.deg.generator.utils.*;
 
-import java.util.Arrays;
-import java.util.Objects;
-import java.util.function.Supplier;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
-
-public class CusipStringGenerator implements StringGenerator {
+public class CusipStringGenerator extends ChecksummedCodeStringGenerator {
     public final static int CUSIP_LENGTH = 9;
     private final static String CUSIP_SANS_CHECK_DIGIT_REGEX = "[0-9]{3}[0-9A-Z]{5}";
 
-    private final RegexStringGenerator cusipSansCheckDigitGenerator;
-    private final boolean negate;
-
     public CusipStringGenerator() {
-        this.negate = false;
-        this.cusipSansCheckDigitGenerator =
-            new RegexStringGenerator(CUSIP_SANS_CHECK_DIGIT_REGEX, true);
+        super(CUSIP_SANS_CHECK_DIGIT_REGEX);
     }
 
     public CusipStringGenerator(String prefix) {
-        this.negate = false;
-        this.cusipSansCheckDigitGenerator =
-            new RegexStringGenerator(prefix + CUSIP_SANS_CHECK_DIGIT_REGEX, true);
+        super(prefix + CUSIP_SANS_CHECK_DIGIT_REGEX);
     }
 
     private CusipStringGenerator(RegexStringGenerator cusipSansCheckDigitGenerator, boolean negate) {
-        this.negate = negate;
-        this.cusipSansCheckDigitGenerator = cusipSansCheckDigitGenerator;
+        super(cusipSansCheckDigitGenerator, negate);
     }
 
     @Override
-    public StringGenerator intersect(StringGenerator stringGenerator) {
-        return new NoStringsStringGenerator(
-            RegexStringGenerator.intersectRepresentation(stringGenerator.toString(), "<CUSIP>")
-        );
+    public char calculateCheckDigit(String str) {
+        return IsinUtils.calculateCusipCheckDigit(str.substring(str.length() - (CUSIP_LENGTH - 1)));
     }
 
     @Override
     public StringGenerator complement() {
-        return new CusipStringGenerator(cusipSansCheckDigitGenerator, !negate);
-    }
-
-    @Override
-    public boolean isFinite() {
-        return true;
-    }
-
-    @Override
-    public long getValueCount() {
-        return cusipSansCheckDigitGenerator.getValueCount();
+        return new CusipStringGenerator(sansCheckDigitGenerator, !negate);
     }
 
     @Override
     public boolean match(String subject) {
         boolean matches = IsinUtils.isValidCusipNsin(subject);
         return matches != negate;
-    }
-
-    @Override
-    public Iterable<String> generateInterestingValues() {
-        if (negate) {
-            return new ConcatenatingIterable<>(
-                cusipSansCheckDigitGenerator.complement().generateInterestingValues(),
-                generateInvalidCheckDigitCusips(cusipSansCheckDigitGenerator::generateInterestingValues));
-        }
-        return new ProjectingIterable<>(cusipSansCheckDigitGenerator.generateInterestingValues(),
-            cusipSansCheckDigit -> cusipSansCheckDigit + IsinUtils.calculateCusipCheckDigit(
-                cusipSansCheckDigit.substring(cusipSansCheckDigit.length() - (CUSIP_LENGTH - 1))));
-    }
-
-    @Override
-    public Iterable<String> generateAllValues() {
-        if (negate) {
-            return new ConcatenatingIterable<>(
-                generateAllInvalidRegexCusips(),
-                generateAllInvalidCheckDigitCusips());
-        }
-        return new ProjectingIterable<>(cusipSansCheckDigitGenerator.generateAllValues(),
-            cusipSansCheckDigit -> cusipSansCheckDigit + IsinUtils.calculateCusipCheckDigit(
-                cusipSansCheckDigit.substring(cusipSansCheckDigit.length() - (CUSIP_LENGTH - 1))));
-    }
-
-    @Override
-    public Iterable<String> generateRandomValues(RandomNumberGenerator randomNumberGenerator) {
-        if (negate) {
-            return new RandomMergingIterable<>(
-                Arrays.asList(
-                    generateRandomInvalidRegexCusips(randomNumberGenerator),
-                    generateRandomInvalidCheckDigitCusips(randomNumberGenerator)),
-                randomNumberGenerator);
-        }
-        return new ProjectingIterable<>(cusipSansCheckDigitGenerator.generateRandomValues(randomNumberGenerator),
-            cusipSansCheckDigit -> cusipSansCheckDigit + IsinUtils.calculateCusipCheckDigit(
-                cusipSansCheckDigit.substring(cusipSansCheckDigit.length() - (CUSIP_LENGTH - 1))));
-    }
-
-    private Iterable<String> generateAllInvalidRegexCusips() {
-        return cusipSansCheckDigitGenerator.complement().generateAllValues();
-    }
-
-    private Iterable<String> generateRandomInvalidRegexCusips(RandomNumberGenerator randomNumberGenerator) {
-        return cusipSansCheckDigitGenerator.complement().generateRandomValues(randomNumberGenerator);
-    }
-
-    private Iterable<String> generateAllInvalidCheckDigitCusips() {
-        return generateInvalidCheckDigitCusips(cusipSansCheckDigitGenerator::generateAllValues);
-    }
-
-    private Iterable<String> generateRandomInvalidCheckDigitCusips(RandomNumberGenerator randomNumberGenerator) {
-        return generateInvalidCheckDigitCusips(
-            () -> cusipSansCheckDigitGenerator.generateRandomValues(randomNumberGenerator));
-    }
-
-    private Iterable<String> generateInvalidCheckDigitCusips(Supplier<Iterable<String>> cusipSansCheckDigitSupplier) {
-        return new FlatteningIterable<>(
-            cusipSansCheckDigitSupplier.get(),
-            cusipSansCheckDigit -> {
-                final char checkDigit = IsinUtils.calculateCusipCheckDigit(
-                    cusipSansCheckDigit.substring(cusipSansCheckDigit.length() - (CUSIP_LENGTH - 1)));
-                return IntStream.range(0, 10).boxed()
-                    .map(digit -> Character.forDigit(digit, 10))
-                    .filter(digit -> digit != checkDigit)
-                    .map(digit -> cusipSansCheckDigit + digit)
-                    .collect(Collectors.toList());
-            });
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        CusipStringGenerator that = (CusipStringGenerator) o;
-        return negate == that.negate;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(negate, getClass());
     }
 }
