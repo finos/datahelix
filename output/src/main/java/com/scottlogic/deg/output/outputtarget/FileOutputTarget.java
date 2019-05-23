dc46ed604136c03fff1fe68b022342f93a8455b7
Paul Daulby <pdaulby@scottlogic.com>
2019-05-23 16:07:19 +0100

    Make OutputTargetProvider know less

--- output/src/main/java/com/scottlogic/deg/output/outputtarget/FileOutputTarget.java
+++ output/src/main/java/com/scottlogic/deg/output/outputtarget/FileOutputTarget.java
@@ -1,5 +1,7 @@
 package com.scottlogic.deg.output.outputtarget;
 
+import com.google.inject.Inject;
+import com.google.inject.name.Named;
 import com.scottlogic.deg.common.profile.ProfileFields;
 import com.scottlogic.deg.output.FileUtils;
 import com.scottlogic.deg.output.writer.DataSetWriter;
@@ -13,18 +15,18 @@ import java.nio.file.Paths;
 
 public class FileOutputTarget implements SingleDatasetOutputTarget {
     private final Path filePath;
-    private final FileUtils fileUtils;
     private final boolean canOverwriteExistingFiles;
     private final OutputWriterFactory outputWriterFactory;
+    private final FileUtils fileUtils;
 
+    @Inject
     public FileOutputTarget(
-        Path filePath,
+        @Named("config:outputPath") Path filePath,
         OutputWriterFactory outputWriterFactory,
-        boolean canOverwriteExistingFiles,
-        FileUtils fileUtils) {
-        this.fileUtils = fileUtils;
-        this.canOverwriteExistingFiles = canOverwriteExistingFiles;
+        @Named("config:canOverwriteOutputFiles") boolean canOverwriteOutputFiles, FileUtils fileUtils) {
+        this.canOverwriteExistingFiles = canOverwriteOutputFiles;
         this.outputWriterFactory = outputWriterFactory;
+        this.fileUtils = fileUtils;
 
         Path directoryPath = filePath.getParent();
         if (directoryPath == null) {
@@ -44,7 +46,6 @@ public class FileOutputTarget implements SingleDatasetOutputTarget {
             return outputWriterFactory.createWriter(stream, fields);
         } catch (Exception e) {
             stream.close();
-
             throw e;
         }
     }
@@ -52,16 +53,15 @@ public class FileOutputTarget implements SingleDatasetOutputTarget {
     @Override
     public void validate() throws OutputTargetValidationException, IOException {
         if (fileUtils.isDirectory(filePath)) {
-            throw new OutputTargetValidationException(
-                "target is a directory, please use a different output filename");
-        } else if (!canOverwriteExistingFiles && fileUtils.exists(filePath)) {
-            throw new OutputTargetValidationException(
-                "file already exists, please use a different output filename or use the --replace option");
-        } else if (!fileUtils.exists(filePath)) {
+            throw new OutputTargetValidationException("target is a directory, please use a different output filename");
+        }
+        else if (!canOverwriteExistingFiles && fileUtils.exists(filePath)) {
+            throw new OutputTargetValidationException("file already exists, please use a different output filename or use the --replace option");
+        }
+        else if (!fileUtils.exists(filePath)) {
             Path parent = filePath.toAbsolutePath().getParent();
             if (!fileUtils.createDirectories(parent)) {
-                throw new OutputTargetValidationException(
-                    "parent directory of output file already exists but is not a directory, please use a different output filename");
+                throw new OutputTargetValidationException("parent directory of output file already exists but is not a directory, please use a different output filename");
             }
         }
     }
---
+++ output/src/main/java/com/scottlogic/deg/output/outputtarget/TraceFileOutputTarget.java
@@ -0,0 +1,25 @@
+package com.scottlogic.deg.output.outputtarget;
+
+import com.google.inject.Inject;
+import com.google.inject.name.Named;
+import com.scottlogic.deg.output.FileUtils;
+import com.scottlogic.deg.output.writer.trace.TraceOutputWriterFactory;
+
+import java.nio.file.Path;
+
+public class TraceFileOutputTarget extends FileOutputTarget {
+
+    @Inject
+    public TraceFileOutputTarget(
+        @Named("config:outputPath") Path filePath,
+        TraceOutputWriterFactory outputWriterFactory,
+        @Named("config:canOverwriteOutputFiles") boolean canOverwriteOutputFiles,
+        FileUtils fileUtils)
+    {
+        super(getTraceFilePath(filePath), outputWriterFactory, canOverwriteOutputFiles, fileUtils);
+    }
+
+    private static Path getTraceFilePath(Path filePath) {
+        return FileUtils.addFilenameSuffix(FileUtils.replaceExtension(filePath, "json"), "-trace");
+    }
+}
