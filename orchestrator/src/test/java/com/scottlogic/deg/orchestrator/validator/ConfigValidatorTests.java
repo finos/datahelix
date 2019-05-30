2cf61eb633b99f83d5eac56481ba92d75b539067
Dave <dwithers@scottlogic.com>
2019-05-30 10:45:44 +0100

    Add more ConfigValidator tests

--- orchestrator/src/test/java/com/scottlogic/deg/orchestrator/validator/ConfigValidatorTests.java
+++ orchestrator/src/test/java/com/scottlogic/deg/orchestrator/validator/ConfigValidatorTests.java
@@ -1,5 +1,6 @@
 package com.scottlogic.deg.orchestrator.validator;
 
+import com.scottlogic.deg.common.ValidationException;
 import com.scottlogic.deg.generator.config.detail.DataGenerationType;
 import com.scottlogic.deg.generator.outputs.targets.OutputTargetValidationException;
 import com.scottlogic.deg.generator.utils.FileUtils;
@@ -55,11 +56,10 @@ public class ConfigValidatorTests {
         when(mockFileUtils.getTraceFile(any())).thenReturn(mock(File.class));
 
         assertDoesNotThrow(()->configValidator.preProfileChecks(mockConfigSource),"Expected no exception, but one was thrown.");
-
     }
 
     @Test
-    public void validateCommandLineOptions_traceConstraintsOutputFileAlreadyExistsNoOverwrite_returnsCorrectErrorMessage() {
+    public void validateCommandLineOptions_traceConstraintsOutputFileAlreadyExistsNoOverwrite_throwsException() {
         ConfigValidator configValidator = new ConfigValidator(mockFileUtils);
         when(mockConfigSource.getProfileFile()).thenReturn(mockProfileFile);
         when(mockFileUtils.containsInvalidChars(mockProfileFile)).thenReturn(false);
@@ -72,4 +72,62 @@ public class ConfigValidatorTests {
 
         assertThrows(OutputTargetValidationException.class, ()->configValidator.preProfileChecks(mockConfigSource),"Expected OutputTargetValidationException to throw, but didn't");
    }
+
+    @Test
+    public void validateCommandLineOptions_traceConstraintsOutputFileAlreadyExistsOverwrite_returnsNoErrorMessages() {
+        ConfigValidator configValidator = new ConfigValidator(mockFileUtils);
+        when(mockConfigSource.getProfileFile()).thenReturn(mockProfileFile);
+        when(mockFileUtils.containsInvalidChars(mockProfileFile)).thenReturn(false);
+        when(mockProfileFile.exists()).thenReturn(true);
+        when(mockProfileFile.isDirectory()).thenReturn(false);
+        when(mockFileUtils.isFileEmpty(mockProfileFile)).thenReturn(false);
+        when(mockConfigSource.isEnableTracing()).thenReturn(true);
+        when(mockFileUtils.getTraceFile(any())).thenReturn(mock(File.class));
+        when(mockFileUtils.getTraceFile(any()).exists()).thenReturn(true);
+        when(mockConfigSource.overwriteOutputFiles()).thenReturn(true);
+
+        assertDoesNotThrow(()->configValidator.preProfileChecks(mockConfigSource),"Expected no exception, but one was thrown.");
+    }
+
+    @Test
+    public void preProfileChecks_profileFilePathContainsInvalidChars_throwsException() {
+        ConfigValidator configValidator = new ConfigValidator(mockFileUtils);
+        when(mockConfigSource.getProfileFile()).thenReturn(mockProfileFile);
+        when(mockFileUtils.containsInvalidChars(mockProfileFile)).thenReturn(true);
+
+        assertThrows(ValidationException.class, ()->configValidator.preProfileChecks(mockConfigSource),"Expected ValidationException to throw, but didn't");
+    }
+
+    @Test
+    public void preProfileChecks_profileFileDoesNotExist_throwsException() {
+        ConfigValidator configValidator = new ConfigValidator(mockFileUtils);
+        when(mockConfigSource.getProfileFile()).thenReturn(mockProfileFile);
+        when(mockFileUtils.containsInvalidChars(mockProfileFile)).thenReturn(false);
+        when(mockProfileFile.exists()).thenReturn(false);
+
+        assertThrows(ValidationException.class, ()->configValidator.preProfileChecks(mockConfigSource),"Expected ValidationException to throw, but didn't");
+    }
+
+    @Test
+    public void preProfileChecks_profileFileIsDir_throwsException() {
+        ConfigValidator configValidator = new ConfigValidator(mockFileUtils);
+        when(mockConfigSource.getProfileFile()).thenReturn(mockProfileFile);
+        when(mockFileUtils.containsInvalidChars(mockProfileFile)).thenReturn(false);
+        when(mockProfileFile.exists()).thenReturn(true);
+        when(mockProfileFile.isDirectory()).thenReturn(true);
+
+        assertThrows(ValidationException.class, ()->configValidator.preProfileChecks(mockConfigSource),"Expected ValidationException to throw, but didn't");
+    }
+
+    @Test
+    public void preProfileChecks_profileFileIsEmpty_throwsException() {
+        ConfigValidator configValidator = new ConfigValidator(mockFileUtils);
+        when(mockConfigSource.getProfileFile()).thenReturn(mockProfileFile);
+        when(mockFileUtils.containsInvalidChars(mockProfileFile)).thenReturn(false);
+        when(mockProfileFile.exists()).thenReturn(true);
+        when(mockProfileFile.isDirectory()).thenReturn(false);
+        when(mockFileUtils.isFileEmpty(mockProfileFile)).thenReturn(true);
+
+        assertThrows(ValidationException.class, ()->configValidator.preProfileChecks(mockConfigSource),"Expected ValidationException to throw, but didn't");
+    }
 }
