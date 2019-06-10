2973b573a1e025a72aee3604972561f3960c4509
Ryan Stuart <rstuart@scottlogic.com>
2019-06-10 14:36:41 +0100

    Add output path for revert

--- output/src/main/java/com/scottlogic/deg/output/outputtarget/TraceFileOutputTarget.java
+++ output/src/main/java/com/scottlogic/deg/output/outputtarget/TraceFileOutputTarget.java
@@ -4,6 +4,7 @@ import com.google.inject.Inject;
 import com.google.inject.name.Named;
 import com.scottlogic.deg.output.FileUtils;
 import com.scottlogic.deg.output.FileUtilsImpl;
+import com.scottlogic.deg.output.OutputPath;
 import com.scottlogic.deg.output.writer.trace.TraceOutputWriterFactory;
 
 import java.nio.file.Path;
@@ -17,7 +18,7 @@ public class TraceFileOutputTarget extends FileOutputTarget {
         FileUtils fileUtils)
     {
         super(
-            fileUtils.getTraceFilePath(filePath),
+            new OutputPath(fileUtils.getTraceFilePath(filePath)),
             outputWriterFactory,
             canOverwriteOutputFiles,
             fileUtils
