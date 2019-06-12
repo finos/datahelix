df56526fbe44df629e24a325e75afffee8600681
Dave <dwithers@scottlogic.com>
2019-06-12 14:26:50 +0100

    Add basic Jar/E2E test
    This test simply runs the generator from the jar and checks that a single line from a simple profile is generated correctly

---
+++ orchestrator/src/test/java/com/scottlogic/deg/orchestrator/endtoend/JarExecuteTests.java
@@ -0,0 +1,28 @@
+package com.scottlogic.deg.orchestrator.endtoend;
+
+import org.junit.jupiter.api.Test;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class JarExecuteTests {
+
+    @Test
+    void GenerateSuccessfullyFromJar() throws Exception {
+            ProcessBuilder pb = new ProcessBuilder("java", "-jar", "\"build\\libs\\generator.jar\"", "generate","-p=\"src\\test\\java\\com\\scottlogic\\deg\\orchestrator\\endtoend\\testprofile.profile.json\"","--max-rows=1");
+
+            Process p = pb.start();
+            BufferedReader BufferedSTDOUTReader = new BufferedReader(new InputStreamReader(p.getInputStream()));
+            List<String> STDOUT = new ArrayList<String>();
+            String line;
+            while ((line = BufferedSTDOUTReader.readLine()) != null) {
+                STDOUT.add(line);
+            }
+            p.waitFor();
+            p.destroy();
+
+            assertEquals("\"Generation successful\"",STDOUT.get(1));
+    }
+    }
+
