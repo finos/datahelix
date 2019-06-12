2d46885c50b9da64945bb9b51ad4ebf3d3532038
Dave <dwithers@scottlogic.com>
2019-06-12 14:41:43 +0100

    Add debug logging

--- orchestrator/src/test/java/com/scottlogic/deg/orchestrator/endtoend/JarExecuteTests.java
+++ orchestrator/src/test/java/com/scottlogic/deg/orchestrator/endtoend/JarExecuteTests.java
@@ -11,13 +11,14 @@ public class JarExecuteTests {
     @Test
     void GenerateSuccessfullyFromJar() throws Exception {
             ProcessBuilder pb = new ProcessBuilder("java", "-jar", "\"build\\libs\\generator.jar\"", "generate","-p=\"src\\test\\java\\com\\scottlogic\\deg\\orchestrator\\endtoend\\testprofile.profile.json\"","--max-rows=1");
-
+            pb.redirectErrorStream(true);
             Process p = pb.start();
             BufferedReader BufferedSTDOUTReader = new BufferedReader(new InputStreamReader(p.getInputStream()));
             List<String> STDOUT = new ArrayList<String>();
             String line;
             while ((line = BufferedSTDOUTReader.readLine()) != null) {
                 STDOUT.add(line);
+                System.out.println(line);
             }
             p.waitFor();
             p.destroy();
