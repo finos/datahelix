b92869432a4b88c08b9bf14a0cbe9345f115e244
Ryan Stuart <rstuart@scottlogic.com>
2019-06-21 14:06:42 +0100

    Fix and tidy integration tests

--- orchestrator/src/test/java/com/scottlogic/deg/orchestrator/endtoend/JarExecuteTests.java
+++ orchestrator/src/test/java/com/scottlogic/deg/orchestrator/endtoend/JarExecuteTests.java
@@ -1,46 +1,58 @@
 package com.scottlogic.deg.orchestrator.endtoend;
 
 import org.junit.jupiter.api.Test;
+
 import java.io.*;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class JarExecuteTests {
 
     @Test
-    void GenerateSuccessfullyFromJar() throws Exception {
-            ProcessBuilder pb = new ProcessBuilder("java", "-jar", "build/libs/generator.jar", "generate", "-p=src/test/java/com/scottlogic/deg/orchestrator/endtoend/testprofile.profile.json", "--max-rows=1", "--quiet");
-            pb.redirectErrorStream(true);
-            Process p = pb.start();
-            BufferedReader BufferedSTDOUTReader = new BufferedReader(new InputStreamReader(p.getInputStream()));
-            List<String> collectedOutput = new ArrayList<String>();
-            String line;
-            while ((line = BufferedSTDOUTReader.readLine()) != null) {
-                collectedOutput.add(line);
-            }
-            p.waitFor();
-            p.destroy();
+    void generateSuccessfullyFromJar() throws Exception {
+        Process p = setupProcess("-p=src/test/java/com/scottlogic/deg/orchestrator/endtoend/testprofile.profile.json");
 
-            assertEquals(Arrays.asList("foo", "\"Generation successful\""), collectedOutput);
+        List<String> collectedOutput = collectOutputAndCloseProcess(p);
+
+        assertEquals(Arrays.asList("foo", "\"Generation successful\""), collectedOutput);
     }
 
     @Test
-    void GenerateSuccessfullyFromJarAndLoadFile() throws Exception {
-        ProcessBuilder pb = new ProcessBuilder("java", "-jar", "build/libs/generator.jar", "generate", "-p=src/test/java/com/scottlogic/deg/orchestrator/endtoend/loadfiletest.profile.json", "--max-rows=1", "--quiet");
-        pb.redirectErrorStream(true);
-        Process p = pb.start();
-        BufferedReader BufferedSTDOUTReader = new BufferedReader(new InputStreamReader(p.getInputStream()));
-        List<String> collectedOutput = new ArrayList<String>();
-        String line;
-        while ((line = BufferedSTDOUTReader.readLine()) != null) {
-            collectedOutput.add(line);
-        }
-        p.waitFor();
-        p.destroy();
+    void generateSuccessfullyFromJarAndLoadFile() throws Exception {
+        Process p = setupProcess("-p=src/test/java/com/scottlogic/deg/orchestrator/endtoend/loadfiletest.profile.json");
+
+        List<String> collectedOutput = collectOutputAndCloseProcess(p);
 
         assertEquals(Arrays.asList("foo", "\"Generated successfully from file\""), collectedOutput);
+    }
+
+    private Process setupProcess(final String profile) throws IOException {
+        ProcessBuilder pb = new ProcessBuilder(
+            "java",
+            "-jar",
+            "build/libs/generator.jar",
+            "generate",
+            profile,
+            "--max-rows=1",
+            "--quiet");
+        pb.redirectErrorStream(true);
+        return pb.start();
+    }
+
+    private List<String> collectOutputAndCloseProcess(Process process) throws IOException, InterruptedException {
+        BufferedReader BufferedStdOutReader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+        List<String> collectedOutput = new ArrayList<>();
+        String line;
+        while ((line = BufferedStdOutReader.readLine()) != null) {
+            collectedOutput.add(line);
+        }
+        process.waitFor();
+        process.destroy();
+
+        return collectedOutput;
     }
 }
 
