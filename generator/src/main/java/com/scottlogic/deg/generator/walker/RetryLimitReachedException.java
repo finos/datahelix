ea2f6db31fa2396b01f2aad691f6f7aa4f8e55dc
Matthew <masmith@scottlogic.co.uk>
2019-07-23 13:53:57 +0100

    Added error handling for when the retry limit is hit in full sequential mode.

--- generator/src/main/java/com/scottlogic/deg/generator/walker/RetryLimitReachedException.java
+++ generator/src/main/java/com/scottlogic/deg/generator/walker/RetryLimitReachedException.java
@@ -2,7 +2,7 @@ package com.scottlogic.deg.generator.walker;
 
 public class RetryLimitReachedException extends RuntimeException {
     // This exception must be a RuntimeException to be able to break out of a stream evaluation.
-    RetryLimitReachedException() {
+    public RetryLimitReachedException() {
         super();
     }
 }
