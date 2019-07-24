a519665597c0d3346a3f1f006e0a34e623181b72
Paul Daulby <pdaulby@scottlogic.com>
2019-07-24 13:47:00 +0100

    Add Decision Based tree walker to config

---
+++ generator/src/main/java/com/scottlogic/deg/generator/guice/RowSpecTreeSolverProvider.java
@@ -0,0 +1,37 @@
+package com.scottlogic.deg.generator.guice;
+
+import com.google.inject.Inject;
+import com.google.inject.Provider;
+import com.scottlogic.deg.common.ValidationException;
+import com.scottlogic.deg.generator.generation.GenerationConfigSource;
+import com.scottlogic.deg.generator.walker.decisionbased.DecisionBasedSolver;
+import com.scottlogic.deg.generator.walker.rowspec.CartesianProductRowSpecTreeSolver;
+import com.scottlogic.deg.generator.walker.rowspec.RowSpecTreeSolver;
+
+public class RowSpecTreeSolverProvider implements Provider<RowSpecTreeSolver> {
+
+    private final GenerationConfigSource config;
+    private final CartesianProductRowSpecTreeSolver cartesianProductRowSpecTreeSolver;
+    private final DecisionBasedSolver decisionBasedSolver;
+
+    @Inject
+    public RowSpecTreeSolverProvider(GenerationConfigSource config, CartesianProductRowSpecTreeSolver cartesianProductRowSpecTreeSolver, DecisionBasedSolver decisionBasedSolver) {
+        this.config = config;
+        this.cartesianProductRowSpecTreeSolver = cartesianProductRowSpecTreeSolver;
+        this.decisionBasedSolver = decisionBasedSolver;
+    }
+
+    @Override
+    public RowSpecTreeSolver get() {
+        switch (config.getWalkerType()) {
+            case DECISION_BASED:
+                return decisionBasedSolver;
+            case CARTESIAN_PRODUCT:
+                return cartesianProductRowSpecTreeSolver;
+            case REDUCTIVE:
+                return decisionBasedSolver;
+            default:
+                throw new ValidationException("no WalkerType selected");
+        }
+    }
+}
