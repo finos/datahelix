87c28da0787ddf0162b60de3c7143b18b26ad5a8
Ryan Stuart <rstuart@scottlogic.com>
2019-07-26 16:29:35 +0100

    Added explanation of plan for field spec related handling

--- common/src/main/java/com/scottlogic/deg/common/profile/constraints/delayed/DelayedAtomicConstraint.java
+++ common/src/main/java/com/scottlogic/deg/common/profile/constraints/delayed/DelayedAtomicConstraint.java
@@ -1,8 +1,13 @@
 package com.scottlogic.deg.common.profile.constraints.delayed;
 
+import com.scottlogic.deg.common.profile.Field;
 import com.scottlogic.deg.common.profile.constraints.Constraint;
+import com.scottlogic.deg.common.profile.constraints.atomic.AtomicConstraint;
 
 public interface DelayedAtomicConstraint extends Constraint {
 
+    AtomicConstraint underlyingConstraint();
+
+    Field field();
 
 }
--- generator/src/main/java/com/scottlogic/deg/generator/fieldspecs/FieldSpecRelations.java
+++ generator/src/main/java/com/scottlogic/deg/generator/fieldspecs/FieldSpecRelations.java
@@ -1,4 +1,26 @@
 package com.scottlogic.deg.generator.fieldspecs;
 
+import com.scottlogic.deg.common.profile.Field;
+
 public interface FieldSpecRelations {
+
+    /**
+     * Produce a field spec similar to how atomic constraints create fieldspecs.
+     * This fieldspec should contain a barebones set of restrictions, with only the
+     * implementation specific bound included.
+     *
+     * An example would be for (a:Int > b:Int), where a and b have many restrictions across different types.
+     * The resultant fieldspec would take the fieldspec of a, and extract ONLY the RELEVANT Int information:
+     *
+     * a:Int maximum value, which is the maximum bound of a minus a unit (for ints, 1).
+     * This ensures that the resultant fieldspec, when merged with b, will guarantee that a can choose a value
+     * that respects the inputted relational constraint.
+     *
+     * @param otherValue
+     * @return
+     */
+    FieldSpec reduceToRelatedFieldSpec(FieldSpec otherValue);
+
+    Field other();
+
 }
