600d4e9d190f993c420b1b1e62b081c8f5364f0d
afroggattsl <afroggatt@scottlogic.com>
2019-07-31 13:36:25 +0100

    docs(#1153): Added alpha feature print statements to command files

--- orchestrator/src/main/java/com/scottlogic/deg/orchestrator/generate/GenerateCommandLine.java
+++ orchestrator/src/main/java/com/scottlogic/deg/orchestrator/generate/GenerateCommandLine.java
@@ -19,19 +19,21 @@ package com.scottlogic.deg.orchestrator.generate;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 import com.google.inject.Module;
-import com.scottlogic.deg.generator.config.detail.*;
+import com.scottlogic.deg.generator.config.detail.CombinationStrategyType;
+import com.scottlogic.deg.generator.config.detail.DataGenerationType;
+import com.scottlogic.deg.generator.config.detail.MonitorType;
+import com.scottlogic.deg.generator.config.detail.TreeWalkerType;
 import com.scottlogic.deg.orchestrator.guice.AllConfigSource;
 import com.scottlogic.deg.orchestrator.guice.AllModule;
 import com.scottlogic.deg.output.guice.OutputFormat;
-import com.scottlogic.deg.profile.v0_1.AtomicConstraintType;
 import picocli.CommandLine;
 
 import java.io.File;
 import java.nio.file.Path;
 import java.util.concurrent.Callable;
 
-import static com.scottlogic.deg.generator.config.detail.CombinationStrategyType.MINIMAL;
 import static com.scottlogic.deg.common.util.Defaults.DEFAULT_MAX_ROWS;
+import static com.scottlogic.deg.generator.config.detail.CombinationStrategyType.MINIMAL;
 import static com.scottlogic.deg.generator.config.detail.DataGenerationType.RANDOM;
 import static com.scottlogic.deg.generator.config.detail.TreeWalkerType.DECISION_BASED;
 import static com.scottlogic.deg.output.guice.OutputFormat.CSV;
@@ -50,11 +52,18 @@ public class GenerateCommandLine implements AllConfigSource, Callable<Integer> {
 
     @Override
     public Integer call() throws Exception {
+        checkForAlphaGenerationDataTypes();
         Module container = new AllModule(this);
         Injector injector = Guice.createInjector(container);
 
         injector.getInstance(GenerateExecute.class).execute();
         return 0;
+    }
+
+    private void checkForAlphaGenerationDataTypes() {
+        if (generationType.equals(DataGenerationType.INTERESTING)) {
+            printAlphaFeatureWarning("Generation Type of INTERESTING");
+        }
     }
 
     @CommandLine.Option(
@@ -231,5 +240,9 @@ public class GenerateCommandLine implements AllConfigSource, Callable<Integer> {
     @Override
     public String fromFilePath() {
         return fromFilePath;
+    }
+
+    protected static void printAlphaFeatureWarning(String feature) {
+        System.err.println(feature + " is an ALPHA FEATURE. Please do not rely on it. If you find any issues with it, please report them at https://github.com/finos/datahelix/issues.");
     }
 }
