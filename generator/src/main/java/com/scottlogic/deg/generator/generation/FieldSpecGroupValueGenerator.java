809978e4021371248a6d8c0df34593b0bc67b35c
Ryan Stuart <rstuart@scottlogic.com>
2019-08-01 10:43:39 +0100

    Add group reduce method

--- generator/src/main/java/com/scottlogic/deg/generator/generation/FieldSpecGroupValueGenerator.java
+++ generator/src/main/java/com/scottlogic/deg/generator/generation/FieldSpecGroupValueGenerator.java
@@ -32,6 +32,7 @@ public class FieldSpecGroupValueGenerator {
         Stream<DataBag> firstDataBagValues = underlyingGenerator.generate(firstSpec)
             .map(value -> toDataBag(first, value));
 
+        FieldSpecGroup groupWithoutFirstField = reduceGroup(first, groupRespectingFirstField);
         return createRemainingDataBags(firstDataBagValues, groupRespectingFirstField);
 
         // recursively operate on stream, adding
@@ -44,6 +45,10 @@ public class FieldSpecGroupValueGenerator {
     }
 
     private FieldSpecGroup adjustBounds(Field field, FieldSpecGroup group) {
+        throw new UnsupportedOperationException("Not implemented!");
+    }
+
+    private FieldSpecGroup reduceGroup(Field toRemove, FieldSpecGroup group) {
         throw new UnsupportedOperationException("Not implemented!");
     }
 
