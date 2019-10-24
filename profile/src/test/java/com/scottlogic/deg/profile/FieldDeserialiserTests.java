7d1cfe672046532730c7d15d5e8af6d36eda19d0
Tom Gilbert <tgilbert@scottlogic.com>
2019-10-24 10:42:07 +0100

    test(#1415): add Grammatical Constraint and Field Deserialiser unit tests.
    Further work required to incorporate null changes.

--- profile/src/test/java/com/scottlogic/deg/profile/FieldDeserialiserTests.java
+++ profile/src/test/java/com/scottlogic/deg/profile/FieldDeserialiserTests.java
@@ -16,76 +16,164 @@
 
 package com.scottlogic.deg.profile;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.exc.InvalidFormatException;
+import com.fasterxml.jackson.databind.exc.UnrecognizedPropertyException;
+import com.scottlogic.deg.common.profile.SpecificFieldType;
+import com.scottlogic.deg.profile.dtos.FieldDTO;
+import com.scottlogic.deg.profile.dtos.constraints.ConstraintDTO;
+import com.scottlogic.deg.profile.dtos.constraints.NullConstraintDTO;
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 
+import static com.shazam.shazamcrest.MatcherAssert.assertThat;
+import static com.shazam.shazamcrest.matcher.Matchers.sameBeanAs;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class FieldDeserialiserTests {
     @Test
-    public void shouldDeserialiseFieldsWithoutException() throws IOException {
+    public void shouldDeserialiseIntegerTypeWithoutException() throws IOException {
         // Arrange
-        final String json =
-            "  \"rules\": [" +
-            "    { \"field\": \"id\", \"ofType\": \"integer\" }" +
-            "    { \"field\": \"country\", \"ofType\": \"string\" }" +
-            "    { \"field\": \"tariff\", \"ofType\": \"decimal\" }" +
-            "    { \"field\": \"time\", \"ofType\": \"datetime\" }," +
-            "    { \"field\": \"first_name\", \"ofType\": \"firstname\" }," +
-            "    { \"field\": \"last_name\", \"ofType\": \"lastname\" }," +
-            "    { \"field\": \"full_name\", \"ofType\": \"fullname\" }," +
-            "  ]";
+        final String json ="{ \"name\": \"id\", \"type\": \"integer\" }";
 
         // Act
-        //ToDo final ProfileDTO profile = new ProfileDeserialiser().deserialise(json);
+        FieldDTO actual = deserialiseJsonString(json);
 
         // Assert
-       //ToDo  assertEquals("0.1", profile.schemaVersion);
+        FieldDTO expected = new FieldDTO();
+        expected.name = "id";
+        expected.type = SpecificFieldType.INTEGER;
+
+        assertThat(actual, sameBeanAs(expected));
     }
 
     @Test
-    public void shouldDeserialiseFieldAndThrowInvalidTypeException() throws IOException {
+    public void shouldDeserialiseStringTypeWithoutException() throws IOException {
         // Arrange
-        final String json =
-            "  \"rules\": [" +
-            "    { \"field\": \"id\", \"ofTye\": \"integer\" }," +
-            "  ],";
+        final String json ="{ \"name\": \"country\", \"type\": \"string\" }\"";
 
         // Act
-        //ToDo final ProfileDTO profile = new ProfileDeserialiser().deserialise(json);
+        FieldDTO actual = deserialiseJsonString(json);
 
         // Assert
-        //ToDo  assertEquals("0.1", profile.schemaVersion);
+        FieldDTO expected = new FieldDTO();
+        expected.name = "country";
+        expected.type = SpecificFieldType.STRING;
+
+        assertThat(actual, sameBeanAs(expected));
     }
 
     @Test
-    public void shouldDeserialiseFieldAndThrowInvalidValueException() throws IOException {
+    public void shouldDeserialiseDecimalTypeWithoutException() throws IOException {
         // Arrange
-        final String json =
-            "  \"rules\": [" +
-            "    { \"field\": \"id\", \"ofType\": \"invalidType\" }," +
-            "]";
+        final String json ="{ \"name\": \"tariff\", \"type\": \"decimal\" }\"";
 
         // Act
-        //ToDo final ProfileDTO profile = new ProfileDeserialiser().deserialise(json);
+        FieldDTO actual = deserialiseJsonString(json);
 
         // Assert
-        //ToDo  assertEquals("0.1", profile.schemaVersion);
+        FieldDTO expected = new FieldDTO();
+        expected.name = "tariff";
+        expected.type = SpecificFieldType.DECIMAL;
+
+        assertThat(actual, sameBeanAs(expected));
     }
 
     @Test
-    public void shouldDeserialiseFieldAndThrowBlankValueException() throws IOException {
+    public void shouldDeserialiseDateTimeTypeWithoutException() throws IOException {
         // Arrange
-        final String json =
-            "  \"rules\": [" +
-            "    { \"field\": \"id\", \"ofType\": \"\" }," +
-            "]";
+        final String json ="{ \"name\": \"time\", \"type\": \"datetime\" }\"";
 
         // Act
-        //ToDo final ProfileDTO profile = new ProfileDeserialiser().deserialise(json);
+        FieldDTO actual = deserialiseJsonString(json);
 
         // Assert
-        //ToDo  assertEquals("0.1", profile.schemaVersion);
+        FieldDTO expected = new FieldDTO();
+        expected.name = "time";
+        expected.type = SpecificFieldType.DATETIME;
+
+        assertThat(actual, sameBeanAs(expected));
+    }
+
+    @Test
+    public void shouldDeserialiseFirstNameTypeWithoutException() throws IOException {
+        // Arrange
+        final String json ="{ \"name\": \"first_name\", \"type\": \"firstname\" }\"";
+
+        // Act
+        FieldDTO actual = deserialiseJsonString(json);
+
+        // Assert
+        FieldDTO expected = new FieldDTO();
+        expected.name = "first_name";
+        expected.type = SpecificFieldType.FIRST_NAME;
+
+        assertThat(actual, sameBeanAs(expected));
+    }
+
+    @Test
+    public void shouldDeserialiseLastNameTypeWithoutException() throws IOException {
+        // Arrange
+        final String json ="{ \"name\": \"last_name\", \"type\": \"lastname\" }\"";
+
+        // Act
+        FieldDTO actual = deserialiseJsonString(json);
+
+        // Assert
+        FieldDTO expected = new FieldDTO();
+        expected.name = "last_name";
+        expected.type = SpecificFieldType.LAST_NAME;
+
+        assertThat(actual, sameBeanAs(expected));
+    }
+
+    @Test
+    public void shouldDeserialiseFullNameTypeWithoutException() throws IOException {
+        // Arrange
+        final String json ="{ \"name\": \"full_name\", \"type\": \"fullname\" }\"";
+
+        // Act
+        FieldDTO actual = deserialiseJsonString(json);
+
+        // Assert
+        FieldDTO expected = new FieldDTO();
+        expected.name = "full_name";
+        expected.type = SpecificFieldType.FULL_NAME;
+
+        assertThat(actual, sameBeanAs(expected));
+    }
+
+    @Test
+    public void shouldDeserialiseFieldAndThrowInvalidFieldNameException() throws IOException {
+        // Arrange
+        final String json = "{ \"name\": \"id\", \"tpe\": \"integer\" }";
+
+        try {
+            deserialiseJsonString(json);
+            Assert.fail("should have thrown an exception");
+        } catch (UnrecognizedPropertyException e) {
+            String expectedMessage = "Unrecognized field \"tpe\" (class com.scottlogic.deg.profile.dtos.FieldDTO), not marked as ignorable (5 known properties: \"unique\", \"type\", \"name\", \"formatting\", \"nullable\"])\n at [Source: (String)\"{ \"name\": \"id\", \"tpe\": \"integer\" }\"; line: 1, column: 25] (through reference chain: com.scottlogic.deg.profile.dtos.FieldDTO[\"tpe\"])";
+            assertThat(e.getMessage(), sameBeanAs(expectedMessage));
+        }
+    }
+
+    @Test
+    public void shouldDeserialiseFieldAndThrowInvalidTypeValueException() throws IOException {
+        // Arrange
+        final String json = "{ \"name\": \"id\", \"type\": \"intger\" }";
+
+        try {
+            deserialiseJsonString(json);
+            Assert.fail("should have thrown an exception");
+        } catch (InvalidFormatException e) {
+            String expectedMessage = "Cannot deserialize value of type `com.scottlogic.deg.common.profile.SpecificFieldType` from String \"intger\": value not one of declared Enum instance names: [string, CUSIP, fullname, datetime, RIC, integer, SEDOL, decimal, firstname, ISIN, lastname]\n at [Source: (String)\"{ \"name\": \"id\", \"type\": \"intger\" }\"; line: 1, column: 25] (through reference chain: com.scottlogic.deg.profile.dtos.FieldDTO[\"type\"])";
+            assertThat(e.getMessage(), sameBeanAs(expectedMessage));
+        }
+    }
+
+    private FieldDTO deserialiseJsonString(String json) throws IOException {
+        return new ObjectMapper().readerFor(FieldDTO.class).readValue(json);
     }
 }
--- profile/src/test/java/com/scottlogic/deg/profile/GrammaticalConstraintDeserialiserTests.java
+++ profile/src/test/java/com/scottlogic/deg/profile/GrammaticalConstraintDeserialiserTests.java
@@ -16,9 +16,20 @@
 
 package com.scottlogic.deg.profile;
 
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.exc.UnrecognizedPropertyException;
+import com.scottlogic.deg.profile.dtos.constraints.*;
+import com.scottlogic.deg.profile.reader.InvalidProfileException;
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
+import java.util.Arrays;
+
+import static com.shazam.shazamcrest.MatcherAssert.assertThat;
+import static com.shazam.shazamcrest.matcher.Matchers.sameBeanAs;
 
 public class GrammaticalConstraintDeserialiserTests {
     @Test
@@ -27,15 +38,24 @@ public class GrammaticalConstraintDeserialiserTests {
         final String json =
             "{ \"anyOf\": [" +
             "    { \"field\": \"foo\", \"equalTo\": \"0\" }," +
-            "    { \"field\": \"foo\", \"isNull\": \"true\" }" +
-            "  ]," +
+            "    { \"null\": \"foo\" }" +
+            "  ]" +
             "}";
 
         // Act
-        //ToDo final ProfileDTO profile = new ProfileDeserialiser().deserialise(json);
+        ConstraintDTO actual = deserialiseJsonString(json);
 
         // Assert
-       //ToDo  assertEquals("0.1", profile.schemaVersion);
+        EqualToConstraintDTO expectedEqualsTo = new EqualToConstraintDTO();
+        expectedEqualsTo.field = "foo";
+        expectedEqualsTo.value = "0";
+        NullConstraintDTO expectedNull = new NullConstraintDTO();
+        expectedNull.field = "foo";
+
+        AnyOfConstraintDTO expected = new AnyOfConstraintDTO();
+        expected.constraints = Arrays.asList(expectedEqualsTo, expectedNull);
+
+        assertThat(actual, sameBeanAs(expected));
     }
 
     @Test
@@ -43,16 +63,18 @@ public class GrammaticalConstraintDeserialiserTests {
         // Arrange
         final String json =
             "{ \"anyOf\": [" +
-            "    { \"field\": \"\", \"equalTo\": \"0\" }," +
-            "    { \"field\": \"\", \"isNull\": \"true\" }" +
-            "  ]," +
+            "    { \"fild\": \"foo\", \"equalTo\": \"0\" }," +
+            "    {\"null\": \"foo\"}" +
+            "  ]" +
             "}";
 
-        // Act
-        //ToDo final ProfileDTO profile = new ProfileDeserialiser().deserialise(json);
-
-        // Assert
-        //ToDo  assertEquals("0.1", profile.schemaVersion);
+        try {
+            deserialiseJsonString(json);
+            Assert.fail("should have thrown an exception");
+        } catch (UnrecognizedPropertyException e) {
+            String expectedMessage = "Unrecognized field \"fild\" (class com.scottlogic.deg.profile.dtos.constraints.EqualToConstraintDTO), not marked as ignorable (2 known properties: \"field\", \"equalTo\"])\n at [Source: UNKNOWN; line: -1, column: -1] (through reference chain: com.scottlogic.deg.profile.dtos.constraints.AnyOfConstraintDTO[\"anyOf\"]->java.util.ArrayList[0]->com.scottlogic.deg.profile.dtos.constraints.EqualToConstraintDTO[\"fild\"])";
+            assertThat(e.getMessage(), sameBeanAs(expectedMessage));
+        }
     }
 
     @Test
@@ -61,15 +83,17 @@ public class GrammaticalConstraintDeserialiserTests {
         final String json =
             "{ \"ayOf\": [" +
             "    { \"field\": \"foo\", \"equalTo\": \"0\" }," +
-            "    { \"field\": \"foo\", \"isNull\": \"true\" }" +
-            "  ]," +
+            "    {\"null\": \"foo\"}" +
+            "  ]" +
             "}";
 
-        // Act
-        //ToDo final ProfileDTO profile = new ProfileDeserialiser().deserialise(json);
-
-        // Assert
-        //ToDo  assertEquals("0.1", profile.schemaVersion);
+        try {
+            deserialiseJsonString(json);
+            Assert.fail("should have thrown an exception");
+        } catch (InvalidProfileException e) {
+            String expectedMessage = "The constraint json object node doesn't contain any of the expected keywords as properties: {\"ayOf\":[{\"field\":\"foo\",\"equalTo\":\"0\"},{\"null\":\"foo\"}]}";
+            assertThat(e.getMessage(), sameBeanAs(expectedMessage));
+        }
     }
 
     @Test
@@ -78,15 +102,17 @@ public class GrammaticalConstraintDeserialiserTests {
         final String json =
             "{ \"anyOf\": [" +
             "    { \"field\": \"foo\", \"equalTo\": \"0\" }," +
-            "    { \"field\": \"foo\", \"isNull\": \"tue\" }" +
-            "  ]," +
+            "    {\"nll\": \"foo\"}" +
+            "  ]" +
             "}";
 
-        // Act
-        //ToDo final ProfileDTO profile = new ProfileDeserialiser().deserialise(json);
-
-        // Assert
-        //ToDo  assertEquals("0.1", profile.schemaVersion);
+        try {
+            deserialiseJsonString(json);
+            Assert.fail("should have thrown an exception");
+        } catch (JsonMappingException e) {
+            String expectedMessage = "The constraint json object node doesn't contain any of the expected keywords as properties: {\"nll\":\"foo\"} (through reference chain: com.scottlogic.deg.profile.dtos.constraints.AnyOfConstraintDTO[\"anyOf\"]->java.util.ArrayList[1])";
+            assertThat(e.getMessage(), sameBeanAs(expectedMessage));
+        }
     }
 
     @Test
@@ -95,31 +121,94 @@ public class GrammaticalConstraintDeserialiserTests {
         final String json =
             "{ \"allOf\": [" +
             "    { \"field\": \"foo\", \"greaterThan\": \"0\" }," +
-            "    { \"field\": \"foo\", \"lessThan\": \"100\" } }" +
-            "  ]," +
+            "    { \"field\": \"foo\", \"lessThan\": \"100\" }" +
+            "  ]" +
             "}";
 
         // Act
-        //ToDo final ProfileDTO profile = new ProfileDeserialiser().deserialise(json);
+        ConstraintDTO actual = deserialiseJsonString(json);
 
         // Assert
-        //ToDo  assertEquals("0.1", profile.schemaVersion);
+        GreaterThanConstraintDTO expectedGreaterThan = new GreaterThanConstraintDTO();
+        expectedGreaterThan.field = "foo";
+        expectedGreaterThan.value = 0;
+        LessThanConstraintDTO expectedLessThan = new LessThanConstraintDTO();
+        expectedLessThan.field = "foo";
+        expectedLessThan.value = 100;
+
+        AllOfConstraintDTO expected = new AllOfConstraintDTO();
+        expected.constraints = Arrays.asList(expectedGreaterThan, expectedLessThan);
+
+        assertThat(actual, sameBeanAs(expected));
+    }
+
+    @Test
+    public void shouldDeserialiseNotWithoutException() throws IOException {
+        // Arrange
+        final String json = "{ \"not\": { \"null\": \"foo\" } }";
+
+        // Act
+       ConstraintDTO actual = deserialiseJsonString(json);
+
+        // Assert
+        NullConstraintDTO expectedNull = new NullConstraintDTO();
+        expectedNull.field = "foo";
+
+        NotConstraintDTO expected = new NotConstraintDTO();
+        expected.constraint = expectedNull;
+
+        assertThat(actual, sameBeanAs(expected));
     }
 
     @Test
     public void shouldDeserialiseIfWithoutException() throws IOException {
         // Arrange
         final String json =
-            "  \"rules\": [" +
-            " \"if\"{ \"field\": \"foo\", \"lessThan\": \"100\" }," +
-            " \"then\"{ \"field\": \"bar\", \"greaterThan\": \"0\" }," +
-            " \"else\"{ \"field\": \"bar\", \"equalTo\": \"N/A\" } }" +
-            "  ]";
+            "{ \"if\":{ \"field\": \"foo\", \"lessThan\": \"100\" }," +
+            " \"then\":{ \"field\": \"bar\", \"greaterThan\": \"0\" }," +
+            " \"else\":{ \"field\": \"bar\", \"equalTo\": \"500\" }}";
 
         // Act
-        //ToDo final ProfileDTO profile = new ProfileDeserialiser().deserialise(json);
+        ConstraintDTO actual = deserialiseJsonString(json);
 
         // Assert
-        //ToDo  assertEquals("0.1", profile.schemaVersion);
+        LessThanConstraintDTO expectedLessThan = new LessThanConstraintDTO();
+        expectedLessThan.field = "foo";
+        expectedLessThan.value = 100;
+        GreaterThanConstraintDTO expectedGreaterThan = new GreaterThanConstraintDTO();
+        expectedGreaterThan.field = "bar";
+        expectedGreaterThan.value = 0;
+        EqualToConstraintDTO expectedEqualTo = new EqualToConstraintDTO();
+        expectedEqualTo.field = "bar";
+        expectedEqualTo.value = "500";
+
+        IfConstraintDTO expected= new IfConstraintDTO();
+
+        expected.ifConstraint = expectedLessThan;
+        expected.thenConstraint = expectedGreaterThan;
+        expected.elseConstraint = expectedEqualTo;
+
+        assertThat(actual, sameBeanAs(expected));
+    }
+
+    @Test
+    public void shouldDeserialiseIfAndThrowMissingColonException() throws IOException {
+        // Arrange
+        final String json =
+            "{ \"if\"{ \"field\": \"foo\", \"lessThan\": \"100\" }," +
+                " \"then\"{ \"field\": \"bar\", \"greaterThan\": \"0\" }," +
+                " \"else\"{ \"field\": \"bar\", \"equalTo\": \"500\" }}";
+
+        try {
+            deserialiseJsonString(json);
+            Assert.fail("should have thrown an exception");
+        } catch (JsonParseException e) {
+            String expectedMessage = "Unexpected character ('{' (code 123)): was expecting a colon to separate field name and value\n at [Source: (String)\"{ \"if\"{ \"field\": \"foo\", \"lessThan\": \"100\" }, \"then\"{ \"field\": \"bar\", \"greaterThan\": \"0\" }, \"else\"{ \"field\": \"bar\", \"equalTo\": \"500\" }}\"; line: 1, column: 8]";
+            assertThat(e.getMessage(), sameBeanAs(expectedMessage));
+        }
+    }
+
+    private ConstraintDTO deserialiseJsonString(String json) throws IOException {
+        return new ObjectMapper().readerFor(ConstraintDTO.class).readValue(json);
     }
 }
