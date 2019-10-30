68c4d7c3ec07f6013bd9e19bdd6a6294478ffb2c
Charlton Dowding <cdowding@scottlogic.com>
2019-10-30 14:44:45 +0000

    Added CreateNonNullableRuleHandler

--- profile/src/main/java/com/scottlogic/deg/profile/handlers/CreateNonNullableRuleHandler.java
+++ profile/src/main/java/com/scottlogic/deg/profile/handlers/CreateNonNullableRuleHandler.java
@@ -2,12 +2,19 @@ package com.scottlogic.deg.profile.handlers;
 
 import com.scottlogic.deg.common.commands.CommandHandler;
 import com.scottlogic.deg.common.commands.CommandResult;
+import com.scottlogic.deg.common.profile.Field;
+import com.scottlogic.deg.common.profile.Fields;
 import com.scottlogic.deg.common.validators.Validator;
 import com.scottlogic.deg.generator.profile.Rule;
+import com.scottlogic.deg.generator.profile.RuleInformation;
+import com.scottlogic.deg.generator.profile.constraints.Constraint;
 import com.scottlogic.deg.profile.commands.CreateNonNullableRule;
+import com.scottlogic.deg.profile.dtos.constraints.NullConstraintDTO;
 import com.scottlogic.deg.profile.reader.ConstraintReader;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class CreateNonNullableRuleHandler extends CommandHandler<CreateNonNullableRule, Optional<Rule>>
 {
@@ -22,6 +29,20 @@ public class CreateNonNullableRuleHandler extends CommandHandler<CreateNonNullab
     @Override
     protected CommandResult<Optional<Rule>> handleCommand(CreateNonNullableRule command)
     {
-        return null;
+        List<Constraint> constraints = command.fields.stream()
+            .filter(field -> !field.isNullable())
+            .map(field -> createNullConstraint(field, command.fields))
+            .collect(Collectors.toList());
+
+        return constraints.isEmpty()
+            ? CommandResult.success(Optional.empty())
+            : CommandResult.success(Optional.of(new Rule(new RuleInformation("non-nullable"), constraints)));
+    }
+
+    private Constraint createNullConstraint(Field field, Fields fields)
+    {
+        NullConstraintDTO nullConstraintDTO = new NullConstraintDTO();
+        nullConstraintDTO.field = field.name;
+        return constraintReader.read(nullConstraintDTO, fields);
     }
 }
--- profile/src/main/java/com/scottlogic/deg/profile/handlers/CreateSpecificTypesRuleHandler.java
+++ profile/src/main/java/com/scottlogic/deg/profile/handlers/CreateSpecificTypesRuleHandler.java
@@ -2,11 +2,19 @@ package com.scottlogic.deg.profile.handlers;
 
 import com.scottlogic.deg.common.commands.CommandHandler;
 import com.scottlogic.deg.common.commands.CommandResult;
+import com.scottlogic.deg.common.profile.Field;
+import com.scottlogic.deg.common.profile.Fields;
 import com.scottlogic.deg.common.validators.Validator;
 import com.scottlogic.deg.generator.profile.Rule;
+import com.scottlogic.deg.generator.profile.RuleInformation;
+import com.scottlogic.deg.generator.profile.constraints.Constraint;
 import com.scottlogic.deg.profile.commands.CreateSpecificTypesRule;
+import com.scottlogic.deg.profile.dtos.constraints.NullConstraintDTO;
+import com.scottlogic.deg.profile.reader.atomic.FieldReader;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class CreateSpecificTypesRuleHandler extends CommandHandler<CreateSpecificTypesRule, Optional<Rule>>
 {
@@ -18,6 +26,21 @@ public class CreateSpecificTypesRuleHandler extends CommandHandler<CreateSpecifi
     @Override
     protected CommandResult<Optional<Rule>> handleCommand(CreateSpecificTypesRule command)
     {
-     return null;
+
+        List<Constraint> constraints = command.fields.stream()
+            .map(field -> FieldReader.read(field, field.type))
+            .filter(Optional::isPresent)
+            .map(Optional::get)
+            .collect(Collectors.toList());
+        return constraints.isEmpty()
+            ? CommandResult.success(Optional.empty())
+            : CommandResult.success(Optional.of(new Rule(new RuleInformation("non-nullable"), constraints)));
+    }
+
+    private Constraint createNullConstraint(Field field, Fields fields)
+    {
+        NullConstraintDTO nullConstraintDTO = new NullConstraintDTO();
+        nullConstraintDTO.field = field.name;
+        return constraintReader.read(nullConstraintDTO, fields);
     }
 }
