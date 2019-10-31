3a96fb3e0adec7ece5b7a2ddfcbda6cb37705303
Tom <thayden@scottlogic.com>
2019-10-31 17:06:00 +0000

    Add skeleton time constraint classes

---
+++ generator/src/main/java/com/scottlogic/deg/generator/profile/constraints/atomic/IsAfterOrEqualToConstantTimeConstraint.java
@@ -0,0 +1,21 @@
+package com.scottlogic.deg.generator.profile.constraints.atomic;
+
+import com.scottlogic.deg.common.profile.Field;
+import com.scottlogic.deg.generator.fieldspecs.FieldSpec;
+
+public class IsAfterOrEqualToConstantTimeConstraint implements AtomicConstraint{
+    @Override
+    public Field getField() {
+        return null;
+    }
+
+    @Override
+    public AtomicConstraint negate() {
+        return null;
+    }
+
+    @Override
+    public FieldSpec toFieldSpec() {
+        return null;
+    }
+}
