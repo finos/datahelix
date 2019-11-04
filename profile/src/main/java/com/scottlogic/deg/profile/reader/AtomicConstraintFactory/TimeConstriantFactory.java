30d12a56f5558d4030632abb1681b5e9cf9c3a21
Tom <thayden@scottlogic.com>
2019-11-04 10:18:02 +0000

    Add TimeConstraintFactory

---
+++ profile/src/main/java/com/scottlogic/deg/profile/reader/AtomicConstraintFactory/TimeConstriantFactory.java
@@ -0,0 +1,41 @@
+package com.scottlogic.deg.profile.reader.AtomicConstraintFactory;
+
+import com.scottlogic.deg.common.profile.*;
+import com.scottlogic.deg.generator.profile.constraints.Constraint;
+import com.scottlogic.deg.generator.profile.constraints.atomic.*;
+import com.scottlogic.deg.profile.dtos.constraints.*;
+import com.scottlogic.deg.profile.reader.FileReader;
+
+public class TimeConstriantFactory extends AtomicConstraintFactory {
+
+    TimeConstriantFactory(FileReader fileReader) {
+        super(fileReader);
+    }
+
+    @Override
+    Constraint getAfterOrAtConstraint(AfterOrAtConstraintDTO dto, Field field) {
+        return new IsAfterOrEqualToConstantTimeConstraint(field, HelixTime.create(dto.value));
+    }
+
+    @Override
+    Constraint getAfterConstraint(AfterConstraintDTO dto, Field field) {
+        return new IsAfterConstantTimeConstraint(field, HelixTime.create(dto.value));
+    }
+
+    @Override
+    Constraint getBeforeOrAtConstraint(BeforeOrAtConstraintDTO dto, Field field) {
+        return new IsBeforeOrEqualToConstantTimeConstraint(field, HelixTime.create(dto.value));
+    }
+
+    @Override
+    Constraint getBeforeConstraint(BeforeConstraintDTO dto, Field field) {
+        return new IsBeforeConstantTimeConstraint(field, HelixTime.create(dto.value));
+    }
+
+    @Override
+    Constraint getGranularToConstraint(GranularToConstraintDTO dto, Field field) {
+        return new IsGranularToTimeConstraint(field, TimeGranularity.create((String) dto.value));
+    }
+
+
+}
