34db302f4e24f0ed74e88084f90142b4007c902f
Tom <thayden@scottlogic.com>
2019-11-04 09:14:32 +0000

    Add fields and constructors to IsGranularToTimeConstraint

--- generator/src/main/java/com/scottlogic/deg/generator/profile/constraints/atomic/IsGranularToTimeConstraint.java
+++ generator/src/main/java/com/scottlogic/deg/generator/profile/constraints/atomic/IsGranularToTimeConstraint.java
@@ -1,9 +1,24 @@
 package com.scottlogic.deg.generator.profile.constraints.atomic;
 
+import com.scottlogic.deg.common.ValidationException;
 import com.scottlogic.deg.common.profile.Field;
+import com.scottlogic.deg.common.profile.TimeGranularity;
 import com.scottlogic.deg.generator.fieldspecs.FieldSpec;
 
 public class IsGranularToTimeConstraint implements AtomicConstraint {
+    public final TimeGranularity timeGranularity;
+    public final Field field;
+
+    public IsGranularToTimeConstraint(Field field, TimeGranularity timeGranularity) {
+        if(field == null)
+            throw new IllegalArgumentException("field must not be null");
+        if(timeGranularity == null)
+            throw new IllegalArgumentException("granularity must not be null");
+
+        this.timeGranularity = timeGranularity;
+        this.field = field;
+    }
+
     @Override
     public Field getField() {
         return null;
@@ -11,7 +26,7 @@ public class IsGranularToTimeConstraint implements AtomicConstraint {
 
     @Override
     public AtomicConstraint negate() {
-        return null;
+        throw new ValidationException("Time Granularity cannot be negated or used in if statements");
     }
 
     @Override
