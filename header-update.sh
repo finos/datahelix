b27ff419c36a7ef0331f8b12b6b733c9c02a6f4e
Ryan Stuart <rstuart@scottlogic.com>
2019-11-12 13:27:02 +0000

    chore(#669): Add cucumber license header scripts
    Scripts were previously verifying that the license headers had been
    added to all java sources. This commit adds the license headers to
    cucumber sources files also, and verifies that they are in place.

--- header-update.sh
+++ header-update.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 
-files=`find . -type f -name '*.java' | xargs grep -L "Licensed under the Apache License"`
+java_files=`find . -type f -name '*.java' | xargs grep -L "Licensed under the Apache License"`
+cucumber_files=`find . -type f -name '*.feature' | xargs grep -L "Licensed under the Apache License"`
 
-for i in ${files}; do
-	cat header.txt ${i} > temp && mv temp ${i}
+for i in ${java_files}; do
+	cat header_java.txt ${i} > temp && mv temp ${i}
+done
+
+for i in ${cucumber_files}; do
+	cat header_cucumber.txt ${i} > temp && mv temp ${i}
 done
