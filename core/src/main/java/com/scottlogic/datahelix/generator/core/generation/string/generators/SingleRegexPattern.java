58e7f3d51e82b37db66ce82af774434ed65694a8
Simon Laing <slaing@scottlogic.com>
2020-01-13 11:01:52 +0000

    chore(#0000): Introduce RegexPattern to more accurately represent regex used to build automaton

---
+++ core/src/main/java/com/scottlogic/datahelix/generator/core/generation/string/generators/SingleRegexPattern.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2019 Scott Logic Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.scottlogic.datahelix.generator.core.generation.string.generators;
+
+import java.util.function.Predicate;
+import java.util.regex.Pattern;
+
+public class SingleRegexPattern implements RegexPattern {
+    private final String regex;
+    private final boolean matchFullString;
+    private final Predicate<String> predicate;
+
+    public SingleRegexPattern(String regex, boolean matchFullString) {
+        this.regex = getPattern(regex);
+        this.matchFullString = matchFullString;
+        this.predicate = Pattern.compile(this.regex).asPredicate();
+    }
+
+    @Override
+    public String getRepresentation() {
+        if (matchFullString) {
+            return String.format("/%s/", regex);
+        }
+
+        return String.format("*/%s/*", regex);
+    }
+
+    @Override
+    public boolean matches(String input) {
+        return predicate.test(input);
+    }
+
+    private static String getPattern(String regex){
+        String firstStripped = regex.charAt(0) == '/'
+            ? regex.substring(1)
+            : regex;
+        return firstStripped.charAt(firstStripped.length() - 1) == '/'
+            ? firstStripped.substring(0, firstStripped.length() - 1)
+            : firstStripped;
+    }
+
+    @Override
+    public RegexPattern complement() {
+        return new NegatedRegexPattern(this);
+    }
+}
