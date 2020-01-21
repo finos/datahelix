d80e7934517642d8ae45f59b98fdd7c46c8e2746
Andy Aspell-Clark <aaspell-clark@scottlogic.com>
2020-01-21 16:28:02 +0000

    chore(#1641): Fixing path to jar to fit in with the LFS
    Fixing path to jar to fit in with the LFS
    
    footer:

--- bin/generator.sh
+++ bin/generator.sh
@@ -2,7 +2,7 @@
 
 command -v java >/dev/null 2>&1 || { echo >&2 "Java executable not found. Aborting."; exit 1; }
 
-DATAHELIX_GENERATOR_HOME=/var/lib/generator
+DATAHELIX_GENERATOR_HOME=/usr/lib/generator
 DATAHELIX_GENERATOR_JAR=generator.jar
 
 java -jar $DATAHELIX_GENERATOR_HOME/$DATAHELIX_GENERATOR_JAR "$@"
