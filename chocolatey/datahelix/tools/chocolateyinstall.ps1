0854d669cfeb703e5512f321fa23d9a6d3741393
Simon Laing <slaing@scottlogic.com>
2020-01-23 08:39:47 +0000

    chore(#1639): Highlight the need to run auto-update script

--- chocolatey/datahelix/tools/chocolateyinstall.ps1
+++ chocolatey/datahelix/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $version    = $env:ChocolateyPackageVersion
 $url32      = "https://github.com/finos/datahelix/releases/download/v$($version)/datahelix.zip"
 $url64      = $url32
-$checksum32 = ''
-$checksum64 = ''
+$checksum32 = '<run ./update.ps1 to set this>'
+$checksum64 = '<run ./update.ps1 to set this>'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
