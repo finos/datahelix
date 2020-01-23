816fb4cc3cf656dc65a78a317780fbabf7ec8d99
Simon Laing <slaing@scottlogic.com>
2020-01-23 14:56:05 +0000

    chore(#1639): Fix installation paths and shim generation

--- chocolatey/datahelix/tools/chocolateyinstall.ps1
+++ chocolatey/datahelix/tools/chocolateyinstall.ps1
@@ -1,10 +1,11 @@
 ﻿$ErrorActionPreference = 'Stop';
-$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)\..\..\"
 $version    = $env:ChocolateyPackageVersion
 $url32      = "https://github.com/finos/datahelix/releases/download/v$($version)/datahelix.zip"
 $url64      = $url32
 $checksum32 = '<run ./update.ps1 to set this>'
 $checksum64 = '<run ./update.ps1 to set this>'
+$shimPath   = "$($env:ChocolateyInstall)\lib\datahelix\bin\datahelix.bat"
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
@@ -19,7 +20,6 @@ $packageArgs = @{
   checksumType64= 'sha256'
 }
 
-Write-Host "Installing Data Helix: $($version)"
 Install-ChocolateyZipPackage @packageArgs
 
-Install-BinFile -name "datahelix" -path "$env:ChocolateyInstall\datahelix\bin\datahelix.bat"
+Install-BinFile -name "datahelix" -path $shimPath
