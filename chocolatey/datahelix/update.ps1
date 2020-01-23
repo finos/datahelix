8ffd138e373b0dd79b58d967ddf526039d09a653
Simon Laing <slaing@scottlogic.com>
2020-01-23 07:34:36 +0000

    chore(#1639): Fixes from PR review

--- chocolatey/datahelix/update.ps1
+++ chocolatey/datahelix/update.ps1
@@ -4,7 +4,7 @@ function global:au_GetLatest() {
      $releases = "https://github.com/finos/datahelix/releases/latest"
 
      $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing #1
-     $regex   = '.jar$'
+     $regex   = '.zip$'
      $url     = $download_page.links | ? href -match $regex | select -First 1 -expand href #2
      $version = [System.Text.RegularExpressions.Regex]::Match($url, "v(\d+\.\d+\.\d+)").Groups[1].Value
 
