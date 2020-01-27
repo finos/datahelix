e8795b2d11dad84c3762326953be80b241ce5f1d
Simon Laing <slaing@scottlogic.com>
2020-01-27 21:19:17 +0000

    chore(#1639): Fix uninstall path

--- chocolatey/datahelix/tools/chocolateyuninstall.ps1
+++ chocolatey/datahelix/tools/chocolateyuninstall.ps1
@@ -3,5 +3,6 @@ $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   softwareName  = 'datahelix*'
 }
+$shimPath   = "$($env:ChocolateyInstall)\lib\datahelix\bin\datahelix.bat"
 
-Uninstall-BinFile -name "datahelix" -path "$env:ChocolateyInstall\datahelix\bin\datahelix.bat"
+Uninstall-BinFile -name "datahelix" -path $shimPath
