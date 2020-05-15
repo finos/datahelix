e27f30310ff58017a3e8f899096f7c1f5174348c
Simon Laing <slaing@scottlogic.com>
2020-05-15 10:50:39 +0100

    fix(#841): Add tests for OneToManyRange

--- core/src/main/java/com/scottlogic/datahelix/generator/core/generation/relationships/OneToManyRange.java
+++ core/src/main/java/com/scottlogic/datahelix/generator/core/generation/relationships/OneToManyRange.java
@@ -50,7 +50,7 @@ public class OneToManyRange {
     }
 
     public boolean isEmpty() {
-        return this.max != null && this.min > this.max;
+        return this.max != null && (this.min > this.max || (this.min == 0 && this.max == 0));
     }
 
     @Override
